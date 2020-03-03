266854d3d0f38004301073f58b86c1860e84e529
jillr <jillr@redhat.com>
2020-03-03 19:43:21 +0000

    migration test cleanup
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/13b104b912784bb31a0bff23eed4c27b0f5e0283

--- plugins/modules/ec2_vpc_nat_gateway_info.py
+++ plugins/modules/ec2_vpc_nat_gateway_info.py
@@ -88,8 +88,14 @@ except ImportError:
     pass  # will be detected by imported HAS_BOTO3
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.ansible.amazon.plugins.module_utils.ec2 import (ec2_argument_spec, get_aws_connection_info, boto3_conn,
-                                      camel_dict_to_snake_dict, ansible_dict_to_boto3_filter_list, boto3_tag_list_to_ansible_dict, HAS_BOTO3)
+from ansible_collections.ansible.amazon.plugins.module_utils.ec2 import (ec2_argument_spec,
+                                                                         get_aws_connection_info,
+                                                                         boto3_conn,
+                                                                         camel_dict_to_snake_dict,
+                                                                         ansible_dict_to_boto3_filter_list,
+                                                                         boto3_tag_list_to_ansible_dict,
+                                                                         HAS_BOTO3,
+                                                                         )
 
 
 def date_handler(obj):
