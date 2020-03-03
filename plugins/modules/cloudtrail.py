0b6d3ff571633e479447cac7941a628a7ac29d28
jillr <jillr@redhat.com>
2020-03-03 19:43:21 +0000

    migration test cleanup
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/13b104b912784bb31a0bff23eed4c27b0f5e0283

--- plugins/modules/cloudtrail.py
+++ plugins/modules/cloudtrail.py
@@ -260,7 +260,9 @@ except ImportError:
 
 from ansible_collections.ansible.amazon.plugins.module_utils.aws.core import AnsibleAWSModule
 from ansible_collections.ansible.amazon.plugins.module_utils.ec2 import (camel_dict_to_snake_dict,
-                                      ansible_dict_to_boto3_tag_list, boto3_tag_list_to_ansible_dict)
+                                                                         ansible_dict_to_boto3_tag_list,
+                                                                         boto3_tag_list_to_ansible_dict,
+                                                                         )
 
 
 def create_trail(module, client, ct_params):
