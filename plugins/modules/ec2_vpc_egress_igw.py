7fa6f90da4aad4286b2e6816f737c13542c2ee2a
Jill R <4121322+jillr@users.noreply.github.com>
2020-03-25 15:39:40 -0700

    Rename collection (#12)
    * Rename core collection
    
    Rename references to ansible.amazon to amazon.aws.
    
    * Rename community.amazon to community.aws
    Fix pep8 line lengths for rewritten amazon.aws imports
    
    * Missed a path in shippable.sh
    * Dependency repos moved
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/235c5db571cc45db5839476c94356c9b91e1f228

--- plugins/modules/ec2_vpc_egress_igw.py
+++ plugins/modules/ec2_vpc_egress_igw.py
@@ -30,8 +30,8 @@ options:
     choices: [ 'present', 'absent' ]
     type: str
 extends_documentation_fragment:
-- ansible.amazon.aws
-- ansible.amazon.ec2
+- amazon.aws.aws
+- amazon.aws.ec2
 
 '''
 
@@ -61,8 +61,8 @@ vpc_id:
 '''
 
 
-from ansible_collections.ansible.amazon.plugins.module_utils.aws.core import AnsibleAWSModule
-from ansible_collections.ansible.amazon.plugins.module_utils.ec2 import camel_dict_to_snake_dict
+from ansible_collections.amazon.aws.plugins.module_utils.aws.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
 
 try:
     import botocore
