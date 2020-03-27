176e3720b3ce48cac85bba280834dee5ae6f3af2
Jill R <4121322+jillr@users.noreply.github.com>
2020-03-27 14:52:07 -0700

    Fixes to collection dependencies in shippable (#14)
    * Shippable fixes
    
    Re-enable change detection, fixed in ansible-test in ansible/ansible#68352
    Fix smoketest logic; duplicate fix from ansible/ansible/pull/67683
    
    * Install netcommon from galaxy now
    
    * Fixes to collection dependencies in shippable
    
    Fix paths in collection_path for tests
    Install community.crypto that aws_acm tests depend on

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -77,24 +77,23 @@ pip install setuptools==44.1.0
 pip install https://github.com/ansible/ansible/archive/devel.tar.gz --disable-pip-version-check
 
 #ansible-galaxy collection install community.general
-mkdir -p "${HOME}/.ansible/ansible_collections/community"
-mkdir -p "${HOME}/.ansible/ansible_collections/google"
-mkdir -p "${HOME}/.ansible/ansible_collections/openstack"
-mkdir -p "${HOME}/.ansible/ansible_collections/amazon"
+mkdir -p "${HOME}/.ansible/collections/ansible_collections/community"
+mkdir -p "${HOME}/.ansible/collections/ansible_collections/google"
+mkdir -p "${HOME}/.ansible/collections/ansible_collections/openstack"
 cwd=$(pwd)
-cd "${HOME}/.ansible/ansible_collections/"
+cd "${HOME}/.ansible/collections/ansible_collections/"
 git clone https://github.com/ansible-collections/community.general community/general
-git clone https://github.com/ansible-collections/amazon.aws amazon/aws
 # community.general requires a lot of things we need to manual pull in
 # once community.general is published this will be handled by galaxy cli
 git clone https://github.com/ansible-collections/ansible_collections_google google/cloud
 git clone https://opendev.org/openstack/ansible-collections-openstack openstack/cloud
-git clone https://github.com/ansible-collections/netcommon ansible/netcommon
-#ansible-galaxy collection install ansible.netcommon
+ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install ansible.netcommon
+ansible-galaxy collection install community.crypto
 cd "${cwd}"
 
 export ANSIBLE_COLLECTIONS_PATHS="${HOME}/.ansible/"
-TEST_DIR="${HOME}/.ansible/ansible_collections/community/aws/"
+TEST_DIR="${HOME}/.ansible/collections/ansible_collections/community/aws/"
 mkdir -p "${TEST_DIR}"
 cp -aT "${SHIPPABLE_BUILD_DIR}" "${TEST_DIR}"
 cd "${TEST_DIR}"
