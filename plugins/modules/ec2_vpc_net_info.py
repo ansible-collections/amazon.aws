c7acbde340df8bb64d1da897f204cf3440f77bbd
Mark Chappell <mchappel@redhat.com>
2020-04-26 16:49:13 +0200

    Pull camel_dict_to_snake_dict from its canonical source

--- plugins/modules/ec2_vpc_net_info.py
+++ plugins/modules/ec2_vpc_net_info.py
@@ -154,11 +154,11 @@ vpcs:
 import traceback
 
 from ansible.module_utils._text import to_native
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 from ansible_collections.amazon.aws.plugins.module_utils.aws.core import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
 
 try:
     import botocore
