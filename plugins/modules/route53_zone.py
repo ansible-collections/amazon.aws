8b584914a4067260b827717c4327f482ccc22253
Jill R <4121322+jillr@users.noreply.github.com>
2020-05-19 16:06:12 -0700

    Remove METADATA and cleanup galaxy.yml (#70)
    * Remove ANSIBLE_METADATA entirely, see ansible/ansible/pull/69454.
    Remove `license` field from galaxy.yml, in favor of `license_file`.
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/05672a64e2362cc2d865b5af6a57da6bc3cd08e3

--- plugins/modules/route53_zone.py
+++ plugins/modules/route53_zone.py
@@ -6,11 +6,6 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['stableinterface'],
-                    'supported_by': 'community'}
-
-
 DOCUMENTATION = '''
 module: route53_zone
 short_description: add or delete Route53 zones
