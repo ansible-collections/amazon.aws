9b2886aa732157f72c3505c36585115dcfb6d8c6
flowerysong <junk+github@flowerysong.com>
2020-06-16 19:30:00 -0400

    Update module_utils paths to remove aws subdir (#23)
    Co-authored-by: Ezekiel Hendrickson <ezekielh@umich.edu>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/10853d9441a586ba177006dd889325cfb24a3dd6

--- plugins/modules/iam_role_info.py
+++ plugins/modules/iam_role_info.py
@@ -151,7 +151,7 @@ try:
 except ImportError:
     pass  # caught by AnsibleAWSModule
 
-from ansible_collections.amazon.aws.plugins.module_utils.aws.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict, camel_dict_to_snake_dict, AWSRetry
 
 
