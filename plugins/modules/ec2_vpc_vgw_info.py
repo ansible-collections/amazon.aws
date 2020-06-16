96b86198d3cebfaaa71288b32ae7be1fe1619226
Abhijeet Kasurde <akasurde@redhat.com>
2020-06-17 01:24:54 +0530

    Update Examples with FQCN (#67)
    Updated module examples with FQCN
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/98173aefbbceed7fc0d9db62687b73f96a55a999

--- plugins/modules/ec2_vpc_vgw_info.py
+++ plugins/modules/ec2_vpc_vgw_info.py
@@ -36,13 +36,13 @@ EXAMPLES = '''
 # # Note: These examples do not set authentication details, see the AWS Guide for details.
 
 - name: Gather information about all virtual gateways for an account or profile
-  ec2_vpc_vgw_info:
+  community.aws.ec2_vpc_vgw_info:
     region: ap-southeast-2
     profile: production
   register: vgw_info
 
 - name: Gather information about a filtered list of Virtual Gateways
-  ec2_vpc_vgw_info:
+  community.aws.ec2_vpc_vgw_info:
     region: ap-southeast-2
     profile: production
     filters:
@@ -50,7 +50,7 @@ EXAMPLES = '''
   register: vgw_info
 
 - name: Gather information about a specific virtual gateway by VpnGatewayIds
-  ec2_vpc_vgw_info:
+  community.aws.ec2_vpc_vgw_info:
     region: ap-southeast-2
     profile: production
     vpn_gateway_ids: vgw-c432f6a7
