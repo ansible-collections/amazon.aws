9890a45011f27ee282dbca86f10049556a0de362
Jill R <4121322+jillr@users.noreply.github.com>
2020-06-16 11:23:52 -0700

    Collections related fixes for CI (#96)
    * Update module deprecations
    
    Switch version to `removed_at_date`
    
    * Don't install amazon.aws from galaxy
    
    We've been using galaxy to install amazon.aws in shippable, but that
    doesn't really work if we aren't publising faster. Get that collection
    from git so it is most up to date.
    
    * We need to declare python test deps now
    
    * missed a python dep
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/7cd211e9383db26bc2aa4cc06e657cf60ed0acc0

--- plugins/modules/ec2_vpc_vgw_info.py
+++ plugins/modules/ec2_vpc_vgw_info.py
@@ -143,7 +143,7 @@ def main():
 
     module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
     if module._name == 'ec2_vpc_vgw_facts':
-        module.deprecate("The 'ec2_vpc_vgw_facts' module has been renamed to 'ec2_vpc_vgw_info'", version='2.13')
+        module.deprecate("The 'ec2_vpc_vgw_facts' module has been renamed to 'ec2_vpc_vgw_info'", date='2021-12-01', collection_name='community.aws')
 
     # Validate Requirements
     if not HAS_BOTO3:
