ec892ee10183f992a032c502662fbe04e6a1a60a
flowerysong <junk+github@flowerysong.com>
2020-06-16 19:30:00 -0400

    Update module_utils paths to remove aws subdir (#23)
    Co-authored-by: Ezekiel Hendrickson <ezekielh@umich.edu>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/10853d9441a586ba177006dd889325cfb24a3dd6

--- plugins/modules/rds_instance_info.py
+++ plugins/modules/rds_instance_info.py
@@ -343,7 +343,7 @@ instances:
           sample: sg-abcd1234
 '''
 
-from ansible_collections.amazon.aws.plugins.module_utils.aws.core import AnsibleAWSModule, is_boto3_error_code
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule, is_boto3_error_code
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import (ansible_dict_to_boto3_filter_list,
                                                                      boto3_tag_list_to_ansible_dict,
                                                                      AWSRetry,
