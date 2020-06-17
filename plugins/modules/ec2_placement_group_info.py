8a7bdca654b63f4a538c2193d6f5b7b54a4e65c4
Jill R <4121322+jillr@users.noreply.github.com>
2020-06-17 09:31:32 -0700

    Update docs (#99)
    * Update docs
    
    Remove .git from repo url so links in readme will generate correctly
    Add required ansible version
    Run latest version of add_docs.py
    Add version_added string to modules
    
    * galaxy.yml was missing authors
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/96ee268e5267f5b12c3d59892bc1279f75aa3135

--- plugins/modules/ec2_placement_group_info.py
+++ plugins/modules/ec2_placement_group_info.py
@@ -9,6 +9,7 @@ __metaclass__ = type
 DOCUMENTATION = '''
 ---
 module: ec2_placement_group_info
+version_added: 1.0.0
 short_description: List EC2 Placement Group(s) details
 description:
     - List details of EC2 Placement Group(s).
