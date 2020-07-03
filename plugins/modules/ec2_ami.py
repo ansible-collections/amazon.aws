2e062f7ac241933fa626f29c0f1475c1deba8e3f
Mark Chappell <mchappel@redhat.com>
2020-07-03 20:31:50 +0200

    ec2_ami - Ensure suboptions in docs match what's parsed and validated

--- plugins/modules/ec2_ami.py
+++ plugins/modules/ec2_ami.py
@@ -78,16 +78,24 @@ options:
     suboptions:
         device_name:
           type: str
-          description: The device name. For example C(/dev/sda).
+          description:
+          - The device name. For example C(/dev/sda).
+          required: yes
+        virtual_name:
+          type: str
+          description:
+          - The virtual name for the device.
+          - See the AWS documentation for more detail U(https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_BlockDeviceMapping.html).
+        no_device:
+          type: bool
+          description:
+          - Suppresses the specified device included in the block device mapping of the AMI.
         volume_type:
           type: str
           description: The volume type.  Defaults to C(gp2) when not set.
         delete_on_termination:
           type: bool
           description: Whether the device should be automatically deleted when the Instance is terminated.
-        no_device:
-          type: bool
-          description: Suppresses the specified device included in the block device mapping of the AMI.
         snapshot_id:
           type: str
           description: The ID of the Snapshot.
@@ -446,8 +454,6 @@ def create_image(module, connection):
             block_device_mapping = []
             for device in device_mapping:
                 device['Ebs'] = {}
-                if 'device_name' not in device:
-                    module.fail_json(msg="Error - Device name must be set for volume.")
                 device = rename_item_if_exists(device, 'device_name', 'DeviceName')
                 device = rename_item_if_exists(device, 'virtual_name', 'VirtualName')
                 device = rename_item_if_exists(device, 'no_device', 'NoDevice')
@@ -674,6 +680,17 @@ def rename_item_if_exists(dict_object, attribute, new_attribute, child_node=None
 
 
 def main():
+    mapping_options = dict(
+        device_name=dict(type='str', aliases=['DeviceName'], required=True),
+        virtual_name=dict(type='str'),
+        no_device=dict(type='bool'),
+        volume_type=dict(type='str'),
+        delete_on_termination=dict(type='bool'),
+        snapshot_id=dict(type='str'),
+        iops=dict(type='int'),
+        encrypted=dict(type='bool'),
+        volume_size=dict(type='int', aliases=['size']),
+    )
     argument_spec = dict(
         instance_id=dict(),
         image_id=dict(),
@@ -688,7 +705,7 @@ def main():
         description=dict(default=''),
         no_reboot=dict(default=False, type='bool'),
         state=dict(default='present', choices=['present', 'absent']),
-        device_mapping=dict(type='list', elements='dict',),
+        device_mapping=dict(type='list', elements='dict', options=mapping_options),
         tags=dict(type='dict'),
         launch_permissions=dict(type='dict'),
         image_location=dict(),
