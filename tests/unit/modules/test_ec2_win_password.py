1b9fde2a928519677f75ce7dc11775cbf3693f6a
Philipp Hoffmann <phoffmann@spirit21.com>
2020-07-24 20:13:13 +0200

    Added comments to help with future test extensions

--- tests/unit/modules/test_ec2_win_password.py
+++ tests/unit/modules/test_ec2_win_password.py
@@ -2,6 +2,21 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
+'''
+Commands to encrypt a message that can be decrypted:
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
+import base64
+
+path = '/path/to/rsa_public_key.pem'
+with open(path, 'r') as f:
+    rsa_public_key_pem = to_text(f.read())
+load_pem_public_key(rsa_public_key_pem = , default_backend())
+base64_cipher = public_key.encrypt('Ansible_AWS_EC2_Win_Password', PKCS1v15())
+string_cipher = base64.b64encode(base64_cipher)
+'''
+
 from ansible.module_utils._text import to_bytes, to_text
 from ansible_collections.community.aws.plugins.modules.ec2_win_password import setup_module_object, ec2_win_password
 from ansible_collections.community.aws.tests.unit.compat.mock import patch
