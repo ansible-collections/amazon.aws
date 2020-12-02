de0cf476dde0f07933632733a337efcc0f506743
jillr <jillr@redhat.com>
2020-12-02 17:14:44 +0000

    Trap cleanup so it always runs Add a wait_for_connection to test playbook Some minor cleanup

--- tests/integration/targets/ec2_metadata_facts/runme.sh
+++ tests/integration/targets/ec2_metadata_facts/runme.sh
@@ -4,11 +4,18 @@ set -eux
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null'
 
+CMD_ARGS=("$@")
+
+# Destroy Environment
+cleanup() {
+    # teardown
+    ansible-playbook playbooks/teardown.yml -i inventory -c local "${CMD_ARGS[@]}"
+}
+trap "cleanup" EXIT
+
 # create test resources and inventory
 ansible-playbook playbooks/setup.yml -c local "$@"
 
 # test ec2_instance_metadata
-ansible-playbook playbooks/test_initial_metadata.yml -i inventory "$@"
+ansible-playbook playbooks/test_metadata.yml -i inventory "$@"
 
-# teardown
-ansible-playbook playbooks/teardown.yml -i inventory -c local "$@"
