8917a74ea5e5de8bbac3b917ac7d6dcf5efb18c1
Jill R <4121322+jillr@users.noreply.github.com>
2020-12-04 11:44:29 -0700

    Create initial test suite for ec2_metadata_facts (#212)
    * Create initial test suite for ec2_metadata_facts
    
    Create an instance, run metadata on the remote instance, and teardown.
    
    * sanity fix
    
    * Trap cleanup so it always runs
    Add a wait_for_connection to test playbook
    Some minor cleanup
    
    * set tmp dirs for ec2 instance
    general cleanup
    
    * wait longer
    
    * run non_local; disabled coverage

---
+++ tests/integration/targets/ec2_metadata_facts/runme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eux
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null'
+
+CMD_ARGS=("$@")
+
+# Destroy Environment
+cleanup() {
+    ansible-playbook playbooks/teardown.yml -i inventory -c local "${CMD_ARGS[@]}"
+}
+trap "cleanup" EXIT
+
+# create test resources and inventory
+ansible-playbook playbooks/setup.yml -c local "$@"
+
+# test ec2_instance_metadata
+ansible-playbook playbooks/test_metadata.yml -i inventory \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    "$@"
