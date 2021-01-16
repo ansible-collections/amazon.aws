c6a4c60a7c83ac068eb69c4b2c14c0b35c49f478
Mark Chappell <mchappel@redhat.com>
2021-01-16 10:50:49 +0100

    Bulk import cleanup (#360)
    * Split imports and reorder
    * Import camel_dict_to_snake_dict and snake_dict_to_camel_dict direct from ansible.module_utils.common.dict_transformations
    * Remove unused imports
    * Route53 Info was migrated to Boto3 drop the HAS_BOTO check and import
    * changelog
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/130cf3cc5980014020632f19fdab79c9bcf28add

--- plugins/modules/iam_user_info.py
+++ plugins/modules/iam_user_info.py
@@ -101,14 +101,15 @@ iam_users:
             sample: "test_user"
 '''
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict, AWSRetry
-
 try:
-    import botocore
     from botocore.exceptions import BotoCoreError, ClientError
 except ImportError:
     pass  # caught by AnsibleAWSModule
+
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
 
 
 @AWSRetry.exponential_backoff()
