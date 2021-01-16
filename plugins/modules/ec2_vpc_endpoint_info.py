1ce2ab4daabc0bf9ba4c5b22d3ec983fe25cf7a0
Mark Chappell <mchappel@redhat.com>
2021-01-16 10:50:49 +0100

    Bulk import cleanup (#360)
    * Split imports and reorder
    * Import camel_dict_to_snake_dict and snake_dict_to_camel_dict direct from ansible.module_utils.common.dict_transformations
    * Remove unused imports
    * Route53 Info was migrated to Boto3 drop the HAS_BOTO check and import
    * changelog

--- plugins/modules/ec2_vpc_endpoint_info.py
+++ plugins/modules/ec2_vpc_endpoint_info.py
@@ -113,11 +113,11 @@ try:
 except ImportError:
     pass  # Handled by AnsibleAWSModule
 
-from ansible.module_utils._text import to_native
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+
 from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
 
 
 def date_handler(obj):
