887d62460252fd4555c641bd0b34ab8d06f2e80f
Mark Chappell <mchappel@redhat.com>
2021-03-10 21:07:39 +0100

    Avoid per-test virtual environments where possible (#467)
    * Move ec2_launch_template to standard role-type test
    
    * Run elb_application_lb in our standard environment
    
    * Move elb_target_info to the standard role structure
    
    * add remote_tmp_dir dependency to ec2_launch_template
    
    * move test_multiple_actions_fail.yml inside full_test so we don't need to run the prep work twice
    
    * Rename the ALBs: "my-alb" isn't helpful and resource prefix is likely to change on us
    
    * Ensure ALBs are deleted if "failed" tests created something

--- tests/integration/targets/elb_target_info/runme.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-# Run full test suite
-source virtualenv.sh
-pip install 'botocore' 'boto3>=1.16.57'
-ansible-playbook -i ../../inventory -v playbooks/full_test.yml "$@"
