67bfcd88b6137b447ae116da212ea63dbe6ea00b
Mark Chappell <mchappel@redhat.com>
2021-07-09 10:22:26 +0200

    Reorder imports based on PEP8 import recommendations
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/fe960aceacd6b0d20f23f3d350ab66964422f1fe

--- plugins/modules/ec2_vpc_peering_info.py
+++ plugins/modules/ec2_vpc_peering_info.py
@@ -208,8 +208,8 @@ except ImportError:
 from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.core import normalize_boto3_result
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
 
 
