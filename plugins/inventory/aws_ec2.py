19ea763cd4719b239c0603bac88f73e9742a4051
Mandar Kulkarni <mandar242@gmail.com>
2021-08-24 03:58:50 -0700

    Adding example of using groups in aws_ec2.py module (#458)
    Adding example of using groups in aws_ec2.py module
    
    SUMMARY
    Adding a example of using groups option to add items from a specific inventory configuration to groups.
    Example shows use of groups to group hosts based on vpc id.
    Resolves #412
    
    ISSUE TYPE
    
    Docs Pull Request
    
    COMPONENT NAME
    
    aws_ec2.py
    
    Reviewed-by: Gonéri Le Bouder <goneri@lebouder.net>
    Reviewed-by: Alina Buzachis <None>
    Reviewed-by: Abhijeet Kasurde <None>
    Reviewed-by: None <None>

--- plugins/inventory/aws_ec2.py
+++ plugins/inventory/aws_ec2.py
@@ -188,6 +188,23 @@ include_filters:
 exclude_filters:
 - tag:Name:
   - 'my_first_tag'
+
+# Example using groups to assign the running hosts to a group based on vpc_id
+plugin: aws_ec2
+boto_profile: aws_profile
+# Populate inventory with instances in these regions
+regions:
+  - us-east-2
+filters:
+  # All instances with their state as `running`
+  instance-state-name: running
+keyed_groups:
+  - prefix: tag
+    key: tags
+compose:
+  ansible_host: public_dns_name
+groups:
+  libvpc: vpc_id == 'vpc-####'
 '''
 
 import re
