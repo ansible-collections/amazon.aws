c0995cbc15bd8d6c823d2bd1267b0fbee16a9822
Ananda Bhattacharya <abhattacharya@ns1.com>
2021-08-25 14:13:57 -0700

    Verify ipv4 and ipv6 addresses with cidr
    Verify ipv4 and ipv6 addresses with matching cidr

--- plugins/lookup/aws_service_ip_ranges.py
+++ plugins/lookup/aws_service_ip_ranges.py
@@ -43,6 +43,7 @@ _raw:
 """
 
 import json
+import netaddr
 
 from ansible.errors import AnsibleError
 from ansible.module_utils.six.moves.urllib.error import HTTPError
@@ -52,6 +53,20 @@ from ansible.module_utils.urls import ConnectionError
 from ansible.module_utils.urls import open_url
 from ansible.module_utils.urls import SSLValidationError
 from ansible.plugins.lookup import LookupBase
+
+def valid_cidr(Ip):
+    """
+    Validate IP address
+    """
+    try:
+        netaddr.IPNetwork(Ip)
+    except netaddr.core.AddrFormatError as e:
+        raise AnsibleError("Not a valid IP address" %e)
+        return False
+    cidr = Ip.split('/')
+    if (len(cidr) <= 1 or cidr[1] == ''):
+        return False
+    return True
 
 
 class LookupModule(LookupBase):
