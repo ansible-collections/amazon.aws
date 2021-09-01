80064947123ff310b561c591a1b98baf9cd592a8
Mandar Kulkarni <mandar242@gmail.com>
2021-09-01 16:00:22 -0700

    Sanity fixes

--- plugins/modules/ec2_spot_instance_info.py
+++ plugins/modules/ec2_spot_instance_info.py
@@ -2,7 +2,9 @@
 # This file is part of Ansible
 # GNU General Public License v3.0+ (see COPYING or https://wwww.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
 __metaclass__ = type
+
 
 DOCUMENTATION = '''
 ---
@@ -10,34 +12,34 @@ module: ec2_spot_instance_info
 version_added: 2.0.0
 short_description: Gather information about ec2 spot instance requests
 description:
-	- Describes the specified Spot Instance requests.
+  - Describes the specified Spot Instance requests.
 options:
-	filters:
-		description:
-			- A list of filters to apply.
-			- Each list item is a dict consisting of a filter key and a filter value.
-			- See U(https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSpotInstanceRequests.html) for possible filters.
-		type: list
-	dry_run:
-		description:
-			- A boolean to check if you have the required permissions for the action.
-			- Does not make the request.
-			- If required permissions are not present then returns an error response UnauthorizedOperation else DryRunOperation.
-		type: bool
-	spot_instance_request_ids:
-		description:
-			- One or more Spot Instance request IDs.
-		type: str
-	next_token:
-		description:
-			- A token to request the next set of results.
-			- Value is null when no more results to return
-		type: str
-	max_results:
-		description:
-			- Maximum number of results to be returned in a single call.
-			- Value can be between 5 and 1000.
-		type: int
+  filters:
+    description:
+      - A list of filters to apply.
+      - Each list item is a dict consisting of a filter key and a filter value.
+      - See U(https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSpotInstanceRequests.html) for possible filters.
+    type: list
+  dry_run:
+    description:
+      - A boolean to check if you have the required permissions for the action.
+      - Does not make the request.
+      - If required permissions are not present then returns an error response UnauthorizedOperation else DryRunOperation.
+    type: bool
+  spot_instance_request_ids:
+    description:
+      - One or more Spot Instance request IDs.
+    type: str
+  next_token:
+    description:
+      - A token to request the next set of results.
+      - Value is null when no more results to return
+    type: str
+  max_results:
+    description:
+      - Maximum number of results to be returned in a single call.
+      - Value can be between 5 and 1000.
+    type: int
 
 extends_documentation_fragment:
 - amazon.aws.aws
@@ -48,15 +50,21 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
+# Note: These examples do not set authentication details, see the AWS Guide for details.
+
+- name: describe the Spot Instance requests based on request IDs
+  amazon.aws.ec2_spot_instance_info:
+    spot_instance_request_ids:
+      - sir-c3cp9jsk
 '''
 
 import time
 import datetime
 
 try:
-  import botocore
+    import botocore
 except ImportError:
-  pass # Handled by AnsibleAWSModule
+    pass  # Handled by AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
@@ -65,6 +73,7 @@ from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict
 from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 from ansible_collections.amazon.aws.plugins.module_utils.core import is_boto3_error_code
+
 
 def describe_spot_instance_requests(connection, module):
 
@@ -95,16 +104,17 @@ def describe_spot_instance_requests(connection, module):
 
     describe_spot_instance_requests_response['Tags'] = boto3_tag_list_to_ansible_dict(describe_spot_instance_requests_response.get('Tags', []))
     describe_spot_requests = camel_dict_to_snake_dict(describe_spot_instance_requests_response, ignore_list=['Tags'])
-    module.exit_json(describe_spot_requests=describe_spot_requests, changed=changed)
+    module.exit_json(spot_requests=describe_spot_requests, changed=changed)
+
 
 def main():
 
-    argument_spec=dict(
+    argument_spec = dict(
         filters=dict(default=[], type='list', elements='dict'),
-        dry_run = dict(default=False, type='bool', choices=[True, False]),
+        dry_run=dict(default=False, type='bool', choices=[True, False]),
         spot_instance_request_ids=dict(default=[], type='list', elements='str'),
-        next_token = dict(default=None, type='str'),
-        max_results = dict(type='int')
+        next_token=dict(default=None, type='str'),
+        max_results=dict(type='int')
     )
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
@@ -116,6 +126,7 @@ def main():
         module.fail_json_aws(e, msg='Failed to connect to AWS')
 
     describe_spot_instance_requests(connection, module)
+
 
 if __name__ == '__main__':
     main()
