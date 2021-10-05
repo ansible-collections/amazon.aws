f51e365d74e224c70c23ee5b7beb1da2b39accff
Mark Chappell <mchappel@redhat.com>
2021-10-05 10:07:13 +0200

    Add helper for generating TagSpecification lists

--- plugins/module_utils/tagging.py
+++ plugins/module_utils/tagging.py
@@ -31,6 +31,7 @@ __metaclass__ = type
 
 from ansible.module_utils._text import to_native
 from ansible.module_utils._text import to_text
+from ansible.module_utils.six import string_types
 
 
 def boto3_tag_list_to_ansible_dict(tags_list, tag_name_key_name=None, tag_value_key_name=None):
@@ -98,6 +99,48 @@ def ansible_dict_to_boto3_tag_list(tags_dict, tag_name_key_name='Key', tag_value
         tags_list.append({tag_name_key_name: k, tag_value_key_name: to_native(v)})
 
     return tags_list
+
+
+def boto3_tag_specifications(tags_dict, types=None):
+    """ Converts a list of resource types and a flat dictionary of key:value pairs representing AWS
+    resource tags to a TagSpecification object.
+
+    https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_TagSpecification.html
+
+    Args:
+        tags_dict (dict): Dict representing AWS resource tags.
+        types (list) A list of resource types to be tagged.
+    Basic Usage:
+        >>> tags_dict = {'MyTagKey': 'MyTagValue'}
+        >>> boto3_tag_specifications(tags_dict, ['instance'])
+        [
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'MyTagKey',
+                        'Value': 'MyTagValue'
+                    }
+                ]
+            }
+        ]
+    Returns:
+        List: List of dictionaries representing an AWS Tag Specification
+    """
+    specifications = list()
+    tag_list = ansible_dict_to_boto3_tag_list(tags_dict)
+
+    if not types:
+        specifications.append(dict(Tags=tag_list))
+        return specifications
+
+    if isinstance(types, string_types):
+        types = [types]
+
+    for type_name in types:
+        specifications.append(dict(ResourceType=type_name, Tags=tag_list))
+
+    return specifications
 
 
 def compare_aws_tags(current_tags_dict, new_tags_dict, purge_tags=True):
