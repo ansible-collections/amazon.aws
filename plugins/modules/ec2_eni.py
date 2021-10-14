03394074e2397e663f7119e8dc07d79658f91115
Mandar Kulkarni <mandar242@gmail.com>
2021-10-14 02:12:39 -0700

    Minor sanity fix

--- plugins/modules/ec2_eni.py
+++ plugins/modules/ec2_eni.py
@@ -679,7 +679,7 @@ def delete_eni(connection, module):
         module.exit_json(changed=False)
 
     if module.check_mode:
-            module.exit_json(changed=True, msg="Would have deleted eni if not in check mode.")
+        module.exit_json(changed=True, msg="Would have deleted eni if not in check mode.")
 
     eni_id = eni["NetworkInterfaceId"]
     force_detach = module.params.get("force_detach")
