fa610432150c498b54f4aa7e870f7fb3dc437442
Jill R <4121322+jillr@users.noreply.github.com>
2021-12-02 02:58:06 -0700

    Remove deprecated "facts" aliases (#814)
    Remove deprecated "facts" aliases
    
    SUMMARY
    Modules named "facts.py" that do not return ansible_facts were renamed to "info.py" in 2.9. Remove these aliases now that the deprecation period is over.
    This PR should be included in 3.0.0 of the collection.
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    *_facts.py
    
    Reviewed-by: Mark Chappell <None>
    Reviewed-by: Jill R <None>
    Reviewed-by: None <None>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/68aaa7057be46a3ab36f572fd0013d64653af909

--- plugins/modules/ec2_vpc_nacl_info.py
+++ plugins/modules/ec2_vpc_nacl_info.py
@@ -12,7 +12,6 @@ version_added: 1.0.0
 short_description: Gather information about Network ACLs in an AWS VPC
 description:
     - Gather information about Network ACLs in an AWS VPC
-    - This module was called C(ec2_vpc_nacl_facts) before Ansible 2.9. The usage did not change.
 author: "Brad Davidson (@brandond)"
 options:
   nacl_ids:
@@ -207,8 +206,6 @@ def main():
         filters=dict(default={}, type='dict'))
 
     module = AnsibleAWSModule(argument_spec=argument_spec, supports_check_mode=True)
-    if module._name == 'ec2_vpc_nacl_facts':
-        module.deprecate("The 'ec2_vpc_nacl_facts' module has been renamed to 'ec2_vpc_nacl_info'", date='2021-12-01', collection_name='community.aws')
 
     connection = module.client('ec2', retry_decorator=AWSRetry.jittered_backoff())
 
