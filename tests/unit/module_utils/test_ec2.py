202a3542bf1031658331a6538059fcae591e78e2
Mandar Kulkarni <mandar242@gmail.com>
2022-01-13 06:19:34 -0800

    Remove redundant unit test

--- tests/unit/module_utils/test_ec2.py
+++ tests/unit/module_utils/test_ec2.py
@@ -7,8 +7,6 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import unittest
-from parametrize import parametrize
-from plugins.module_utils.ec2 import is_outposts_arn
 
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import map_complex_type
@@ -78,19 +76,3 @@ class Ec2Utils(unittest.TestCase):
 
         converted_filters_int = ansible_dict_to_boto3_filter_list(filters)
         self.assertEqual(converted_filters_int, filter_list_integer)
-
-    # ========================================================
-    #   ec2.is_outposts_arn
-    # ========================================================
-    @parametrize(
-        "outpost_arn, result",
-        [
-            ("arn:aws:outposts:us-east-1:123456789012:outpost/op-1234567890abcdef0", True),
-            ("arn:aws:outposts:us-east-1:123456789012:outpost/op-1234567890abcdef0123", False),
-            ("arn:aws:outpost:us-east-1: 123456789012:outpost/ op-1234567890abcdef0", False),
-            ("ars:aws:outposts:us-east-1: 123456789012:outpost/ op-1234567890abcdef0", False),
-            ("arn:was:outposts:us-east-1: 123456789012:outpost/ op-1234567890abcdef0", False),
-        ]
-    )
-    def test_is_outposts_arn(self, outpost_arn, result):
-        self.assertEqual(is_outposts_arn(outpost_arn), result)
