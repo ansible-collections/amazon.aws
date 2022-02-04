76e69b668abaf759e2c2a7610220eba0c2321fd8
Mandar Kulkarni <mandar242@gmail.com>
2022-02-04 11:33:23 -0800

    ec2_placement_group: Add partition strategy and partition count (#872)
    ec2_placement_group: Add partition strategy and partition count
    
    SUMMARY
    
    Add partition as a strategy and an option, partition_count to choose the actual number of partitions for the community.aws.ec2_placement_group module.
    
    Fixes #808
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    ec2_placement_group
    ADDITIONAL INFO
    Tested locally with
    - name: Create a partition placement group with partition count 4.
        ec2_placement_group:
          name: my-cluster
          state: present
          strategy: partition
          partition_count: 4
    
    Reviewed-by: Alina Buzachis <None>
    Reviewed-by: Mandar Kulkarni <mandar242@gmail.com>
    Reviewed-by: Mark Woolley <mw@marknet15.com>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/716ae77f71cfa7b6300a057892fea768c81819f9

--- plugins/modules/ec2_placement_group.py
+++ plugins/modules/ec2_placement_group.py
@@ -23,6 +23,13 @@ options:
       - The name for the placement group.
     required: true
     type: str
+  partition_count:
+    description:
+      - The number of partitions.
+      - Valid only when I(Strategy) is set to C(partition).
+      - Must be a value between C(1) and C(7).
+    type: int
+    version_added: 3.1.0
   state:
     description:
       - Create or delete placement group.
@@ -35,7 +42,7 @@ options:
         low-latency group in a single Availability Zone, while Spread spreads
         instances across underlying hardware.
     default: cluster
-    choices: [ 'cluster', 'spread' ]
+    choices: [ 'cluster', 'spread', 'partition' ]
     type: str
 extends_documentation_fragment:
 - amazon.aws.aws
@@ -57,6 +64,13 @@ EXAMPLES = '''
     name: my-cluster
     state: present
     strategy: spread
+
+- name: Create a Partition strategy placement group.
+  community.aws.ec2_placement_group:
+    name: my-cluster
+    state: present
+    strategy: partition
+    partition_count: 3
 
 - name: Delete a placement group.
   community.aws.ec2_placement_group:
@@ -126,10 +140,21 @@ def get_placement_group_details(connection, module):
 def create_placement_group(connection, module):
     name = module.params.get("name")
     strategy = module.params.get("strategy")
+    partition_count = module.params.get("partition_count")
+
+    if strategy != 'partition' and partition_count:
+        module.fail_json(
+            msg="'partition_count' can only be set when strategy is set to 'partition'.")
+
+    params = {}
+    params['GroupName'] = name
+    params['Strategy'] = strategy
+    if partition_count:
+        params['PartitionCount'] = partition_count
+    params['DryRun'] = module.check_mode
 
     try:
-        connection.create_placement_group(
-            GroupName=name, Strategy=strategy, DryRun=module.check_mode)
+        connection.create_placement_group(**params)
     except is_boto3_error_code('DryRunOperation'):
         module.exit_json(changed=True, placement_group={
             "name": name,
@@ -165,8 +190,9 @@ def delete_placement_group(connection, module):
 def main():
     argument_spec = dict(
         name=dict(required=True, type='str'),
+        partition_count=dict(type='int'),
         state=dict(default='present', choices=['present', 'absent']),
-        strategy=dict(default='cluster', choices=['cluster', 'spread'])
+        strategy=dict(default='cluster', choices=['cluster', 'spread', 'partition'])
     )
 
     module = AnsibleAWSModule(
