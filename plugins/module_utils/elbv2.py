b14e392a65945aead32acb78c7e5b6b852048346
Joseph Torcasso <87090265+jatorcasso@users.noreply.github.com>
2022-03-12 05:15:11 -0500

    elbv2 module_util - support alb attributes and add compare method for check_mode (#696)
    elbv2 module_util - support alb attributes and add compare method for check_mode
    
    SUMMARY
    
    Add support for modifying alb specific attributes for ansible-collections/community.aws#963
    Add compare_elb_attributes method to support check_mode
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    community.aws.elb_application_lb
    amazon/aws/plugins/module_utils/elbv2.py
    
    Reviewed-by: Mandar Kulkarni <mandar242@gmail.com>
    Reviewed-by: Jill R <None>
    Reviewed-by: Joseph Torcasso <None>

--- plugins/module_utils/elbv2.py
+++ plugins/module_utils/elbv2.py
@@ -289,6 +289,11 @@ class ApplicationLoadBalancer(ElasticLoadBalancerV2):
         self.access_logs_s3_prefix = module.params.get("access_logs_s3_prefix")
         self.idle_timeout = module.params.get("idle_timeout")
         self.http2 = module.params.get("http2")
+        self.http_desync_mitigation_mode = module.params.get("http_desync_mitigation_mode")
+        self.http_drop_invalid_header_fields = module.params.get("http_drop_invalid_header_fields")
+        self.http_x_amzn_tls_version_and_cipher_suite = module.params.get("http_x_amzn_tls_version_and_cipher_suite")
+        self.http_xff_client_port = module.params.get("http_xff_client_port")
+        self.waf_fail_open = module.params.get("waf_fail_open")
 
         if self.elb is not None and self.elb['Type'] != 'application':
             self.module.fail_json(msg="The load balancer type you are trying to manage is not application. Try elb_network_lb module instead.")
@@ -325,6 +330,47 @@ class ApplicationLoadBalancer(ElasticLoadBalancerV2):
         if self.wait:
             self.wait_for_status(self.elb['LoadBalancerArn'])
 
+    def compare_elb_attributes(self):
+        """
+        Compare user attributes with current ELB attributes
+        :return: bool True if they match otherwise False
+        """
+
+        update_attributes = []
+        if self.access_logs_enabled is not None and str(self.access_logs_enabled).lower() != self.elb_attributes['access_logs_s3_enabled']:
+            update_attributes.append({'Key': 'access_logs.s3.enabled', 'Value': str(self.access_logs_enabled).lower()})
+        if self.access_logs_s3_bucket is not None and self.access_logs_s3_bucket != self.elb_attributes['access_logs_s3_bucket']:
+            update_attributes.append({'Key': 'access_logs.s3.bucket', 'Value': self.access_logs_s3_bucket})
+        if self.access_logs_s3_prefix is not None and self.access_logs_s3_prefix != self.elb_attributes['access_logs_s3_prefix']:
+            update_attributes.append({'Key': 'access_logs.s3.prefix', 'Value': self.access_logs_s3_prefix})
+        if self.deletion_protection is not None and str(self.deletion_protection).lower() != self.elb_attributes['deletion_protection_enabled']:
+            update_attributes.append({'Key': 'deletion_protection.enabled', 'Value': str(self.deletion_protection).lower()})
+        if self.idle_timeout is not None and str(self.idle_timeout) != self.elb_attributes['idle_timeout_timeout_seconds']:
+            update_attributes.append({'Key': 'idle_timeout.timeout_seconds', 'Value': str(self.idle_timeout)})
+        if self.http2 is not None and str(self.http2).lower() != self.elb_attributes['routing_http2_enabled']:
+            update_attributes.append({'Key': 'routing.http2.enabled', 'Value': str(self.http2).lower()})
+        if self.http_desync_mitigation_mode is not None and str(self.http_desync_mitigation_mode).lower() != \
+                self.elb_attributes['routing_http_desync_mitigation_mode']:
+            update_attributes.append({'Key': 'routing.http.desync_mitigation_mode', 'Value': str(self.http_desync_mitigation_mode).lower()})
+        if self.http_drop_invalid_header_fields is not None and str(self.http_drop_invalid_header_fields).lower() != \
+                self.elb_attributes['routing_http_drop_invalid_header_fields_enabled']:
+            update_attributes.append({'Key': 'routing.http.drop_invalid_header_fields.enabled', 'Value': str(self.http_drop_invalid_header_fields).lower()})
+        if self.http_x_amzn_tls_version_and_cipher_suite is not None and str(self.http_x_amzn_tls_version_and_cipher_suite).lower() != \
+                self.elb_attributes['routing_http_x_amzn_tls_version_and_cipher_suite_enabled']:
+            update_attributes.append({'Key': 'routing.http.x_amzn_tls_version_and_cipher_suite.enabled',
+                                      'Value': str(self.http_x_amzn_tls_version_and_cipher_suite).lower()})
+        if self.http_xff_client_port is not None and str(self.http_xff_client_port).lower() != \
+                self.elb_attributes['routing_http_xff_client_port_enabled']:
+            update_attributes.append({'Key': 'routing.http.xff_client_port.enabled', 'Value': str(self.http_xff_client_port).lower()})
+        if self.waf_fail_open is not None and str(self.waf_fail_open).lower() != \
+                self.elb_attributes['waf_fail_open_enabled']:
+            update_attributes.append({'Key': 'waf.fail_open.enabled', 'Value': str(self.waf_fail_open).lower()})
+
+        if update_attributes:
+            return False
+        else:
+            return True
+
     def modify_elb_attributes(self):
         """
         Update Application ELB attributes if required
@@ -346,6 +392,22 @@ class ApplicationLoadBalancer(ElasticLoadBalancerV2):
             update_attributes.append({'Key': 'idle_timeout.timeout_seconds', 'Value': str(self.idle_timeout)})
         if self.http2 is not None and str(self.http2).lower() != self.elb_attributes['routing_http2_enabled']:
             update_attributes.append({'Key': 'routing.http2.enabled', 'Value': str(self.http2).lower()})
+        if self.http_desync_mitigation_mode is not None and str(self.http_desync_mitigation_mode).lower() != \
+                self.elb_attributes['routing_http_desync_mitigation_mode']:
+            update_attributes.append({'Key': 'routing.http.desync_mitigation_mode', 'Value': str(self.http_desync_mitigation_mode).lower()})
+        if self.http_drop_invalid_header_fields is not None and str(self.http_drop_invalid_header_fields).lower() != \
+                self.elb_attributes['routing_http_drop_invalid_header_fields_enabled']:
+            update_attributes.append({'Key': 'routing.http.drop_invalid_header_fields.enabled', 'Value': str(self.http_drop_invalid_header_fields).lower()})
+        if self.http_x_amzn_tls_version_and_cipher_suite is not None and str(self.http_x_amzn_tls_version_and_cipher_suite).lower() != \
+                self.elb_attributes['routing_http_x_amzn_tls_version_and_cipher_suite_enabled']:
+            update_attributes.append({'Key': 'routing.http.x_amzn_tls_version_and_cipher_suite.enabled',
+                                      'Value': str(self.http_x_amzn_tls_version_and_cipher_suite).lower()})
+        if self.http_xff_client_port is not None and str(self.http_xff_client_port).lower() != \
+                self.elb_attributes['routing_http_xff_client_port_enabled']:
+            update_attributes.append({'Key': 'routing.http.xff_client_port.enabled', 'Value': str(self.http_xff_client_port).lower()})
+        if self.waf_fail_open is not None and str(self.waf_fail_open).lower() != \
+                self.elb_attributes['waf_fail_open_enabled']:
+            update_attributes.append({'Key': 'waf.fail_open.enabled', 'Value': str(self.waf_fail_open).lower()})
 
         if update_attributes:
             try:
