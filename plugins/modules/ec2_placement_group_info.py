bbfe0d0ccf026e827b3f5dec17957732a796d8a9
Mark Chappell <mchappel@redhat.com>
2022-04-28 12:37:58 +0200

    Docs linting fixups (#1100)
    Docs linting fixups
    
    SUMMARY
    While testing out linting/validation of the generated docs, a number of issues were highlighted.
    ISSUE TYPE
    
    Docs Pull Request
    
    COMPONENT NAME
    plugins/modules/aws_batch_job_definition.py
    plugins/modules/aws_config_rule.py
    plugins/modules/aws_secret.py
    plugins/modules/aws_waf_rule.py
    plugins/modules/cloudfront_invalidation.py
    plugins/modules/ec2_placement_group_info.py
    plugins/modules/iam_user.py
    plugins/modules/route53_health_check.py
    plugins/modules/s3_sync.py
    ADDITIONAL INFORMATION
    
    Reviewed-by: Markus Bergholz <git@osuv.de>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/c5363d56dc761e6a8b53be7d0ebd243af20adec7

--- plugins/modules/ec2_placement_group_info.py
+++ plugins/modules/ec2_placement_group_info.py
@@ -45,7 +45,8 @@ EXAMPLES = r'''
   register: specific_ec2_placement_groups
 
 - ansible.builtin.debug:
-    msg: "{{ specific_ec2_placement_groups | json_query(\"[?name=='my-cluster']\") }}"
+    msg: >
+      {{ specific_ec2_placement_groups | json_query("[?name=='my-cluster']") }}
 
 '''
 
