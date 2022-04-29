690df9c2d3f69323837542b96fa7318650fd5308
Gonéri Le Bouder <goneri@lebouder.net>
2022-04-29 16:36:03 -0400

    setup_sshkey: drop a dep on Crypto
    Adjust ec2-fingerprint.py so it use cryptography instead of the
    deprecated Crypto library.
    
    Introduce a dummy change in ec2_key test-suite to trigger the
    test-suite.

--- tests/integration/targets/setup_sshkey/files/ec2-fingerprint.py
+++ tests/integration/targets/setup_sshkey/files/ec2-fingerprint.py
@@ -8,24 +8,25 @@ ssh-keygen -f id_rsa.pub -e -m PKCS8 | openssl pkey -pubin -outform DER | openss
 (but without needing the OpenSSL CLI)
 """
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
 
 import hashlib
 import sys
-from Crypto.PublicKey import RSA
+from cryptography.hazmat.primitives import serialization
 
 if len(sys.argv) == 0:
     ssh_public_key = "id_rsa.pub"
 else:
     ssh_public_key = sys.argv[1]
 
-with open(ssh_public_key, 'r') as key_fh:
-    data = key_fh.read()
-
-# Convert from SSH format to DER format
-public_key = RSA.importKey(data).exportKey('DER')
-md5digest = hashlib.md5(public_key).hexdigest()
+with open(ssh_public_key, "rb") as key_file:
+    public_key = serialization.load_ssh_public_key(
+        key_file.read(),
+    )
+pub_der = public_key.public_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+)
+md5digest = hashlib.md5(pub_der).hexdigest()
 # Format the md5sum into the normal format
 pairs = zip(md5digest[::2], md5digest[1::2])
 md5string = ":".join(["".join(pair) for pair in pairs])
