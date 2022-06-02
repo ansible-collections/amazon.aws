ansible-collections/amazon.aws

commit 37a58123cb21ed31523f7f7be8863b0b6abee249

ec2_instance - deprecate purge_tags=False (#849)
ec2_instance - deprecate purge_tags=False

Depends-On: #844
SUMMARY
Deprecate purge_tags=False for ec2_instance
ISSUE TYPE

Feature Pull Request

COMPONENT NAME
plugins/modules/ec2_instance.py
ADDITIONAL INFORMATION

Reviewed-by: Alina Buzachis <None>

plugins/modules/ec2_instance.py
@@ -95,16 +95,6 @@ options:
         description:
         - Host configuration secret key generated by the Tower job template.
         type: str
-  tags:
-    description:
-      - A hash/dictionary of tags to add to the new instance or to add/remove from an existing one.
-    type: dict
-  purge_tags:
-    description:
-      - Delete any tags not specified in the task that are on the instance.
-        This means you have to specify all the desired tags on each task affecting an instance.
-    default: false
-    type: bool
   image:
     description:
       - An image to use for the instance. The M(amazon.aws.ec2_ami_info) module may be used to retrieve images.
@@ -352,6 +342,7 @@ options:
 extends_documentation_fragment:
 - amazon.aws.aws
 - amazon.aws.ec2
+- amazon.aws.tags.deprecated_purge
 
 '''
 
@@ -1789,11 +1780,18 @@ def determine_iam_role(name_or_arn):
 
 
 def handle_existing(existing_matches, state):
-    tags = dict(module.params.get('tags') or {})
+    tags = dict(module.params.get('tags', None))
+    purge_tags = module.params.get('purge_tags')
     name = module.params.get('name')
-    purge_tags = module.params.get('purge_tags', False)
+
+    # Name is a tag rather than a direct parameter, we need to inject 'Name'
+    # into tags, but since tags isn't explicitly passed we'll treat it not being
+    # set as purge_tags == False
     if name:
-        tags['Name'] = name
+        if purge_tags and tags is None:
+            purge_tags = False
+            tags = {}
+        tags.update({'Name': name})
 
     changed = False
     all_changes = list()
@@ -2004,8 +2002,8 @@ def main():
         security_group=dict(type='str'),
         instance_role=dict(type='str'),
         name=dict(type='str'),
-        tags=dict(type='dict'),
-        purge_tags=dict(type='bool', default=False),
+        tags=dict(type='dict', aliases=['resource_tags']),
+        purge_tags=dict(type='bool'),
         filters=dict(type='dict', default=None),
         launch_template=dict(type='dict'),
         key_name=dict(type='str'),
@@ -2047,6 +2045,14 @@ def main():
         ],
         supports_check_mode=True
     )
+
+    if module.params.get('purge_tags') is None:
+        module.deprecate(
+            'The purge_tags parameter currently defaults to False.'
+            ' For consistency across the collection, this default value'
+            ' will change to True in release 5.0.0.',
+            version='5.0.0', collection_name='amazon.aws')
+        module.params['purge_tags'] = False
 
     if not module.params.get('instance_type') and not module.params.get('launch_template') and module.params.get('state') != 'absent':
         module.deprecate("Default value instance_type has been deprecated, in the future you must set an instance_type or a launch_template",

commit 6dd4c312013f916fedc2aa01addee25728d3b28c

ec2_instance - Fix NoneType error on no input tags (#856)
ec2_instance - Fix NoneType error on no input tags

SUMMARY
Reverts bug introduced in #849 which throws a NoneType exception when tags are not input
ISSUE TYPE

Bugfix Pull Request

COMPONENT NAME
ec2_instance
ADDITIONAL INFORMATION
The error gets thrown when trying to wrap dict(None). See https://ansible.softwarefactory-project.io/zuul/build/5e37b5947f7045c6bf01e88f8ae5271f
TASK [elb_classic_lb : Wait for instance a] ************************************
task path: /home/zuul/.ansible/collections/ansible_collections/amazon/aws/tests/integration/targets/elb_classic_lb/tasks/simple_instances.yml:18
Using module file /home/zuul/.ansible/collections/ansible_collections/amazon/aws/plugins/modules/ec2_instance.py
Pipelining is enabled.
<testhost> ESTABLISH LOCAL CONNECTION FOR USER: zuul
<testhost> EXEC /bin/sh -c 'ANSIBLE_DEBUG_BOTOCORE_LOGS=True /home/zuul/venv/bin/python && sleep 0'
The full traceback is:
Traceback (most recent call last):
  File "<stdin>", line 121, in <module>
  File "<stdin>", line 113, in _ansiballz_main
  File "<stdin>", line 61, in invoke_module
  File "/usr/lib64/python3.8/runpy.py", line 207, in run_module
    return _run_module_code(code, init_globals, run_name, mod_spec)
  File "/usr/lib64/python3.8/runpy.py", line 97, in _run_module_code
    _run_code(code, mod_globals, init_globals,
  File "/usr/lib64/python3.8/runpy.py", line 87, in _run_code
    exec(code, run_globals)
  File "/tmp/ansible_ec2_instance_payload_ofa2yzhm/ansible_ec2_instance_payload.zip/ansible_collections/amazon/aws/plugins/modules/ec2_instance.py", line 2105, in <module>
  File "/tmp/ansible_ec2_instance_payload_ofa2yzhm/ansible_ec2_instance_payload.zip/ansible_collections/amazon/aws/plugins/modules/ec2_instance.py", line 2097, in main
  File "/tmp/ansible_ec2_instance_payload_ofa2yzhm/ansible_ec2_instance_payload.zip/ansible_collections/amazon/aws/plugins/modules/ec2_instance.py", line 1783, in handle_existing

plugins/modules/ec2_instance.py
@@ -1780,7 +1780,7 @@ def determine_iam_role(name_or_arn):
 
 
 def handle_existing(existing_matches, state):
-    tags = dict(module.params.get('tags', None))
+    tags = module.params.get('tags')
     purge_tags = module.params.get('purge_tags')
     name = module.params.get('name')
 
@@ -1788,7 +1788,7 @@ def handle_existing(existing_matches, state):
     # into tags, but since tags isn't explicitly passed we'll treat it not being
     # set as purge_tags == False
     if name:
-        if purge_tags and tags is None:
+        if tags is None:
             purge_tags = False
             tags = {}
         tags.update({'Name': name})

tests/integration/targets/ec2_instance/roles/ec2_instance/tasks/tags_and_vpc_settings.yml
@@ -138,11 +138,45 @@
         - check_tags.instances[0].tags.Name.startswith(resource_prefix)
         - "'{{ check_tags.instances[0].state.name }}' in  ['pending', 'running']"
 
+  - name: "Try setting purge_tags to True without specifiying tags (should NOT purge tags)"
+    ec2_instance:
+      state: present
+      name: "{{ resource_prefix }}-test-basic-vpc-create"
+      image_id: "{{ ec2_ami_id }}"
+      purge_tags: true
+      security_groups: "{{ sg.group_id }}"
+      vpc_subnet_id: "{{ testing_subnet_b.subnet.id }}"
+      instance_type: "{{ ec2_instance_type }}"
+    register: _purge_tags_without_tags
+
+  - name: Assert tags were not purged
+    assert:
+      that:
+        - _purge_tags_without_tags.instances[0].tags | length > 1
+
+  - name: "Purge all tags (aside from Name)"
+    ec2_instance:
+      state: present
+      name: "{{ resource_prefix }}-test-basic-vpc-create"
+      image_id: "{{ ec2_ami_id }}"
+      purge_tags: true
+      tags: {}
+      security_groups: "{{ sg.group_id }}"
+      vpc_subnet_id: "{{ testing_subnet_b.subnet.id }}"
+      instance_type: "{{ ec2_instance_type }}"
+    register: _purge_tags
+
+  - name: Assert tags were purged
+    assert:
+      that:
+        - _purge_tags.instances[0].tags | length == 1
+        - _purge_tags.instances[0].tags.Name.startswith(resource_prefix)
+
   - name: "Terminate instance"
     ec2_instance:
       state: absent
       filters:
-        "tag:TestId": "{{ ec2_instance_tag_TestId }}"
+        "tag:Name": "{{ resource_prefix }}-test-basic-vpc-create"
       wait: false
     register: result
   - assert: