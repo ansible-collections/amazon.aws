2d5b2e82be8c96c9f81c8af905c4eabd6a8a24b1
Mark Chappell <mchappel@redhat.com>
2022-10-10 10:57:49 +0200

    Improve unit test coverage of module_utils.policy and refactor _hashable_policy slightly (#1136)
    Improve unit test coverage of module_utils.policy
    
    SUMMARY
    
    Improve unit test coverage of module_utils.policy
    Slight refactor of _hashable_policy to reduce complexity (and make testing easier)
    Deprecates sort_json_policy_dict, it's very naive and was only used by ecs_ecr
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    plugins/module_utils/policy.py
    ADDITIONAL INFORMATION
    
    Reviewed-by: Gonéri Le Bouder <goneri@lebouder.net>

---
+++ tests/unit/module_utils/policy/test_py3cmp.py
@@ -0,0 +1,43 @@
+# (c) 2022 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.amazon.aws.plugins.module_utils.policy import _py3cmp
+
+
+def test_py3cmp_simple():
+    assert _py3cmp(1, 1) == 0
+    assert _py3cmp(1, 2) == -1
+    assert _py3cmp(2, 1) == 1
+    assert _py3cmp("1", "1") == 0
+    assert _py3cmp("1", "2") == -1
+    assert _py3cmp("2", "1") == 1
+    assert _py3cmp("a", "a") == 0
+    assert _py3cmp("a", "b") == -1
+    assert _py3cmp("b", "a") == 1
+    assert _py3cmp(("a",), ("a",)) == 0
+    assert _py3cmp(("a",), ("b",)) == -1
+    assert _py3cmp(("b",), ("a",)) == 1
+
+
+def test_py3cmp_mixed():
+    # Replicates the Python2 comparison behaviour of placing strings before tuples
+    assert _py3cmp(("a",), "a") == 1
+    assert _py3cmp("a", ("a",)) == -1
+
+    assert _py3cmp(("a",), "b") == 1
+    assert _py3cmp("b", ("a",)) == -1
+    assert _py3cmp(("b",), "a") == 1
+    assert _py3cmp("a", ("b",)) == -1
+
+    # intended for use by _hashable_policy, so expects either a string or a tuple
+    with pytest.raises(TypeError) as context:
+        _py3cmp((1,), 1)
+    with pytest.raises(TypeError) as context:
+        _py3cmp(1, (1,))
---
+++ tests/unit/module_utils/policy/test_simple_hashable_policy.py
@@ -0,0 +1,32 @@
+# (c) 2022 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible_collections.amazon.aws.plugins.module_utils.policy import _hashable_policy
+from ansible_collections.amazon.aws.tests.unit.compat.mock import sentinel
+
+
+def test_hashable_policy_none():
+    assert _hashable_policy(None, []) == []
+
+
+def test_hashable_policy_boolean():
+    assert _hashable_policy(True, []) == ("true", )
+    assert _hashable_policy(False, []) == ("false", )
+
+
+def test_hashable_policy_int():
+    assert _hashable_policy(1, []) == ("1", )
+    assert _hashable_policy(42, []) == ("42", )
+    assert _hashable_policy(0, []) == ("0", )
+
+
+def test_hashable_policy_string():
+    assert _hashable_policy("simple_string", []) == ["simple_string"]
+    assert _hashable_policy("123456789012", []) == ["123456789012"]
+    # This is a special case, we generally expect to have gone via _canonify_root_arn
+    assert _hashable_policy("arn:aws:iam::123456789012:root", []) == ["123456789012"]
