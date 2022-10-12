b3b038d0c299e8fdb1be649762ef52be2b0eb8c0
Mandar Kulkarni <mandar242@gmail.com>
2022-10-12 11:36:26 -0700

    Sanity fix, unit test header

--- tests/unit/plugins/modules/test_ec2_ami.py
+++ tests/unit/plugins/modules/test_ec2_ami.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from unittest.mock import MagicMock, Mock, patch, call
 
