7d2528a956b892fcc54c0a2baffa6e0f1158c2dc
Mandar Kulkarni <mandar242@gmail.com>
2022-10-12 13:37:18 -0700

    ec2_ami: Add support for params BootMode, TpmSupport, UefiData (#1037)
    ec2_ami: Add support for params BootMode, TpmSupport, UefiData
    
    SUMMARY
    Depends-On: #1066
    
    Added support for params BootMode, TpmSupport, UefiData in ec2_ami.
    
    Fixes #944
    ISSUE TYPE
    
    
    Feature Pull Request
    
    COMPONENT NAME
    
    ec2_ami
    ADDITIONAL INFORMATION
    
    
    
    Example playbook
    - name: abc
      hosts: localhost
      gather_facts: false
      tasks:
        - name: AMI Creation with boot_mode and tpm_support
          amazon.aws.ec2_ami:
            name: ami-create-test_legacy-bios
            state: present
            architecture: x86_64
            virtualization_type: hvm
            root_device_name: /dev/sda1
            device_mapping:
              - device_name: /dev/sda1
                snapshot_id: snap-xxxxxxxxx
            wait: yes
            region: us-east-2
            boot_mode: legacy-bios
            tpm_support: v2.0
            tags:
              name: ami-create-test
    
    Reviewed-by: Gonéri Le Bouder <goneri@lebouder.net>
    Reviewed-by: Mandar Kulkarni <mandar242@gmail.com>
    Reviewed-by: Mike Graves <mgraves@redhat.com>

---
+++ tests/unit/plugins/modules/test_ec2_ami.py
@@ -0,0 +1,44 @@
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from unittest.mock import MagicMock, Mock, patch, call
+
+import pytest
+
+from ansible_collections.amazon.aws.plugins.modules import ec2_ami
+
+module_name = "ansible_collections.amazon.aws.plugins.modules.ec2_ami"
+
+
+@patch(module_name + ".get_image_by_id")
+def test_create_image_uefi_data(m_get_image_by_id):
+    module = MagicMock()
+    connection = MagicMock()
+
+    m_get_image_by_id.return_value = {
+        "ImageId": "ami-0c7a795306730b288",
+        "BootMode": "uefi",
+        "TpmSupport": "v2.0",
+    }
+
+    module.params = {
+        "name": "my-image",
+        "boot_mode": "uefi",
+        "tpm_support": "v2.0",
+        "uefi_data": "QU1aTlVFRkk9xcN0AAAAAHj5a7fZ9+3aT2gcVRgA8Ek3NipiPST0pCiCIlTJtj20FzENCcQa",
+    }
+
+    ec2_ami.create_image(module, connection)
+    assert connection.register_image.call_count == 1
+    connection.register_image.assert_has_calls(
+        [
+            call(
+                aws_retry=True,
+                Description=None,
+                Name="my-image",
+                BootMode="uefi",
+                TpmSupport="v2.0",
+                UefiData="QU1aTlVFRkk9xcN0AAAAAHj5a7fZ9+3aT2gcVRgA8Ek3NipiPST0pCiCIlTJtj20FzENCcQa"
+            )
+        ]
+    )
