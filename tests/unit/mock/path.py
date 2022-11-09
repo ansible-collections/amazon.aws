efbe5a678072fc5c5e1ea20c87a0cb7fd44f8bc1
Mark Chappell <mchappel@redhat.com>
2022-11-09 09:57:04 +0100

    Remove unused helpers (originally copied from Ansible as part of the collections migration)

--- tests/unit/mock/path.py
+++
@@ -1,5 +0,0 @@
-from unittest.mock import MagicMock
-from ansible.utils.path import unfrackpath
-
-
-mock_unfrackpath_noop = MagicMock(spec_set=unfrackpath, side_effect=lambda x, *args, **kwargs: x)
