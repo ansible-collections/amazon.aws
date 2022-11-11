9ef6b75fd82c4239a79274e6c89fedbe7d3c0aef
Gonéri Le Bouder <goneri@lebouder.net>
2022-11-11 09:09:33 -0500

    CI TESTING - DNM

--- plugins/modules/ec2_ami.py
+++ plugins/modules/ec2_ami.py
@@ -2,7 +2,7 @@
 # Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-DOCUMENTATION = '''
+aDOCUMENTATION = '''
 ---
 module: ec2_ami
 version_added: 1.0.0
