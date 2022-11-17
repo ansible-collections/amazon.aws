58d94716f10051f668ccf1a7d298cf597e77daef
Mark Chappell <mchappel@redhat.com>
2022-11-17 15:31:15 +0100

    ec2_security_group - refactor rule expansion (#1261)
    ec2_security_group - refactor rule expansion
    
    SUMMARY
    Refactors the rule expansion and adds unit tests.
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    ec2_security_group
    ADDITIONAL INFORMATION
    
    Reviewed-by: Alina Buzachis <None>
    Reviewed-by: Mark Chappell <None>

---
+++ tests/unit/plugins/modules/ec2_security_group/test_expand_rules.py
@@ -0,0 +1,224 @@
+# (c) 2022 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import pytest
+import sys
+from unittest.mock import sentinel
+
+import ansible_collections.amazon.aws.plugins.modules.ec2_security_group as ec2_security_group_module
+
+PORT_EXPANSION = [
+    ({"from_port": 83}, ({"from_port": 83, "to_port": None},)),
+    ({"to_port": 36}, ({"from_port": None, "to_port": 36},)),
+    ({"icmp_type": 90}, ({"from_port": 90, "to_port": None},)),
+    ({"icmp_type": 74, "icmp_code": 66}, ({"from_port": 74, "to_port": 66},)),
+    # Note: ports is explicitly a list of strings because we support "<port a>-<port b>"
+    ({"ports": ["1"]}, ({"from_port": 1, "to_port": 1},)),
+    ({"ports": ["41-85"]}, ({"from_port": 41, "to_port": 85},)),
+    (
+        {"ports": ["63", "74"]},
+        (
+            {"from_port": 63, "to_port": 63},
+            {"from_port": 74, "to_port": 74},
+        ),
+    ),
+    (
+        {"ports": ["97-30", "41-80"]},
+        (
+            {"from_port": 30, "to_port": 97},
+            {"from_port": 41, "to_port": 80},
+        ),
+    ),
+    (
+        {"ports": ["95", "67-79"]},
+        (
+            {"from_port": 95, "to_port": 95},
+            {"from_port": 67, "to_port": 79},
+        ),
+    ),
+    # There are legitimate cases with no port info
+    ({}, ({},)),
+]
+PORTS_EXPANSION = [
+    (["28"], [(28, 28)]),
+    (["80-83"], [(80, 83)]),
+    # We tolerate the order being backwards
+    (["83-80"], [(80, 83)]),
+    (["41", "1"], [(41, 41), (1, 1)]),
+    (["70", "39-0"], [(70, 70), (0, 39)]),
+    (["57-6", "31"], [(6, 57), (31, 31)]),
+    # https://github.com/ansible-collections/amazon.aws/pull/1241
+    (["-1"], [(-1, -1)]),
+]
+SOURCE_EXPANSION = [
+    (
+        {"cidr_ip": ["192.0.2.0/24"]},
+        ({"cidr_ip": "192.0.2.0/24"},),
+    ),
+    (
+        {"cidr_ipv6": ["2001:db8::/32"]},
+        ({"cidr_ipv6": "2001:db8::/32"},),
+    ),
+    (
+        {"group_id": ["sg-123456789"]},
+        ({"group_id": "sg-123456789"},),
+    ),
+    (
+        {"group_name": ["MyExampleGroupName"]},
+        ({"group_name": "MyExampleGroupName"},),
+    ),
+    (
+        {"ip_prefix": ["pl-123456abcde123456"]},
+        ({"ip_prefix": "pl-123456abcde123456"},),
+    ),
+    (
+        {"cidr_ip": ["192.0.2.0/24", "198.51.100.0/24"]},
+        ({"cidr_ip": "192.0.2.0/24"}, {"cidr_ip": "198.51.100.0/24"},),
+    ),
+    (
+        {"cidr_ipv6": ["2001:db8::/32", "100::/64"]},
+        ({"cidr_ipv6": "2001:db8::/32"}, {"cidr_ipv6": "100::/64"},),
+    ),
+    (
+        {"group_id": ["sg-123456789", "sg-abcdef1234"]},
+        ({"group_id": "sg-123456789"}, {"group_id": "sg-abcdef1234"},),
+    ),
+    (
+        {"group_name": ["MyExampleGroupName", "AnotherExample"]},
+        ({"group_name": "MyExampleGroupName"}, {"group_name": "AnotherExample"},),
+    ),
+    (
+        {"ip_prefix": ["pl-123456abcde123456", "pl-abcdef12345abcdef"]},
+        ({"ip_prefix": "pl-123456abcde123456"}, {"ip_prefix": "pl-abcdef12345abcdef"}),
+    ),
+    (
+        {
+            "cidr_ip": ["192.0.2.0/24"],
+            "cidr_ipv6": ["2001:db8::/32"],
+            "group_id": ["sg-123456789"],
+            "group_name": ["MyExampleGroupName"],
+            "ip_prefix": ["pl-123456abcde123456"],
+        },
+        (
+            {"cidr_ip": "192.0.2.0/24"},
+            {"cidr_ipv6": "2001:db8::/32"},
+            {"group_id": "sg-123456789"},
+            {"group_name": "MyExampleGroupName"},
+            {"ip_prefix": "pl-123456abcde123456"},
+        ),
+    ),
+    (
+        {
+            "cidr_ip": ["192.0.2.0/24", "198.51.100.0/24"],
+            "cidr_ipv6": ["2001:db8::/32", "100::/64"],
+            "group_id": ["sg-123456789", "sg-abcdef1234"],
+            "group_name": ["MyExampleGroupName", "AnotherExample"],
+            "ip_prefix": ["pl-123456abcde123456", "pl-abcdef12345abcdef"],
+        },
+        (
+            {"cidr_ip": "192.0.2.0/24"},
+            {"cidr_ip": "198.51.100.0/24"},
+            {"cidr_ipv6": "2001:db8::/32"},
+            {"cidr_ipv6": "100::/64"},
+            {"group_id": "sg-123456789"},
+            {"group_id": "sg-abcdef1234"},
+            {"group_name": "MyExampleGroupName"},
+            {"group_name": "AnotherExample"},
+            {"ip_prefix": "pl-123456abcde123456"},
+            {"ip_prefix": "pl-abcdef12345abcdef"},
+        ),
+    ),
+]
+
+RULE_EXPANSION = [
+    (
+        {"ports": ["24"], "cidr_ip": ["192.0.2.0/24"], "sentinel": sentinel.RULE_VALUE},
+        [
+            {"from_port": 24, "to_port": 24, "cidr_ip": "192.0.2.0/24", "sentinel": sentinel.RULE_VALUE},
+        ],
+    ),
+    (
+        {"ports": ["24", "50"], "cidr_ip": ["192.0.2.0/24", "198.51.100.0/24"], "sentinel": sentinel.RULE_VALUE},
+        [
+            {"from_port": 24, "to_port": 24, "cidr_ip": "192.0.2.0/24", "sentinel": sentinel.RULE_VALUE},
+            {"from_port": 24, "to_port": 24, "cidr_ip": "198.51.100.0/24", "sentinel": sentinel.RULE_VALUE},
+            {"from_port": 50, "to_port": 50, "cidr_ip": "192.0.2.0/24", "sentinel": sentinel.RULE_VALUE},
+            {"from_port": 50, "to_port": 50, "cidr_ip": "198.51.100.0/24", "sentinel": sentinel.RULE_VALUE},
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize("rule, expected", PORT_EXPANSION)
+def test_expand_ports_from_rule(rule, expected):
+    assert ec2_security_group_module.expand_ports_from_rule(rule) == expected
+
+    # We shouldn't care about extra values lurking in the rule definition
+    rule["junk"] = sentinel.EXTRA_JUNK
+    assert ec2_security_group_module.expand_ports_from_rule(rule) == expected
+
+
+@pytest.mark.parametrize("rule, expected", SOURCE_EXPANSION)
+def test_expand_sources_from_rule(rule, expected):
+    assert ec2_security_group_module.expand_sources_from_rule(rule) == expected
+
+    # We shouldn't care about extra values lurking in the rule definition
+    rule["junk"] = sentinel.EXTRA_JUNK
+    assert ec2_security_group_module.expand_sources_from_rule(rule) == expected
+
+
+@pytest.mark.parametrize("rule, expected", PORTS_EXPANSION)
+def test_expand_ports_list(rule, expected):
+    assert ec2_security_group_module.expand_ports_list(rule) == expected
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="requires Python 3.7 or higher - sentinel doesn't behave well with deepcopy in Python 3.6"
+)
+@pytest.mark.parametrize("source_type", sorted(ec2_security_group_module.SOURCE_TYPES_ALL))
+def test_strip_rule_source(source_type):
+    rule = {source_type: sentinel.SOURCE_VALUE}
+    assert ec2_security_group_module._strip_rule(rule) == {}
+    assert rule == {source_type: sentinel.SOURCE_VALUE}
+
+    rule = {source_type: sentinel.SOURCE_VALUE, "sentinel": sentinel.SENTINEL_VALUE}
+    assert ec2_security_group_module._strip_rule(rule) == {"sentinel": sentinel.SENTINEL_VALUE}
+    assert rule == {source_type: sentinel.SOURCE_VALUE, "sentinel": sentinel.SENTINEL_VALUE}
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="requires Python 3.7 or higher - sentinel doesn't behave well with deepcopy in Python 3.6"
+)
+@pytest.mark.parametrize("port_type", sorted(ec2_security_group_module.PORT_TYPES_ALL))
+def test_strip_rule_port(port_type):
+    rule = {port_type: sentinel.PORT_VALUE}
+    assert ec2_security_group_module._strip_rule(rule) == {}
+    assert rule == {port_type: sentinel.PORT_VALUE}
+
+    rule = {port_type: sentinel.PORT_VALUE, "sentinel": sentinel.SENTINEL_VALUE}
+    assert ec2_security_group_module._strip_rule(rule) == {"sentinel": sentinel.SENTINEL_VALUE}
+    assert rule == {port_type: sentinel.PORT_VALUE, "sentinel": sentinel.SENTINEL_VALUE}
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="requires Python 3.7 or higher - sentinel doesn't behave well with deepcopy in Python 3.6"
+)
+@pytest.mark.parametrize("rule, expected", RULE_EXPANSION)
+def test_rule_expand(rule, expected):
+    assert ec2_security_group_module.expand_rule(rule) == expected
+
+
+##########################################################
+# Examples where we explicitly expect to raise an exception
+
+
+def test_expand_ports_list_bad():
+    with pytest.raises(ec2_security_group_module.SecurityGroupError):
+        ec2_security_group_module.expand_ports_list(["junk"])
+
+
+def test_expand_sources_from_rule_bad():
+    with pytest.raises(ec2_security_group_module.SecurityGroupError):
+        ec2_security_group_module.expand_sources_from_rule(dict())
---
+++ tests/unit/plugins/modules/ec2_security_group/test_formatting.py
@@ -0,0 +1,238 @@
+# (c) 2022 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import pytest
+from unittest.mock import sentinel
+
+import ansible_collections.amazon.aws.plugins.modules.ec2_security_group as ec2_security_group_module
+
+SORT_ORDER = [
+    (dict(), dict()),
+    (
+        dict(ip_permissions=[], ip_permissions_egress=[]),
+        dict(ip_permissions=[], ip_permissions_egress=[]),
+    ),
+    (
+        dict(
+            ip_permissions=[
+                dict(
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[
+                        dict(cidr_ipv6="2001:DB8:8000::/34"),
+                        dict(cidr_ipv6="2001:DB8:4000::/34"),
+                    ],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    ip_protocol="-1",
+                    ip_ranges=[
+                        dict(cidr_ip="198.51.100.0/24"),
+                        dict(cidr_ip="192.0.2.0/24"),
+                    ],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    from_port="22",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    to_port="22",
+                    user_id_group_pairs=[
+                        dict(group_id="sg-3950599b", user_id="123456789012"),
+                        dict(group_id="sg-fbfd1e3a", user_id="012345678901"),
+                        dict(group_id="sg-00ec640f", user_id="012345678901"),
+                    ],
+                ),
+                dict(
+                    from_port=38,
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[
+                        dict(prefix_list_id="pl-2263adef"),
+                        dict(prefix_list_id="pl-0a5fccee"),
+                        dict(prefix_list_id="pl-65911ba9"),
+                    ],
+                    to_port=38,
+                    user_id_group_pairs=[],
+                ),
+            ],
+            ip_permissions_egress=[
+                dict(
+                    ip_protocol="-1",
+                    ip_ranges=[
+                        dict(cidr_ip="198.51.100.0/24"),
+                        dict(cidr_ip="192.0.2.0/24"),
+                    ],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    from_port=443,
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    to_port=443,
+                    user_id_group_pairs=[
+                        dict(group_id="sg-fbfd1e3a", user_id="012345678901"),
+                        dict(group_id="sg-00ec640f", user_id="012345678901"),
+                    ],
+                ),
+            ],
+        ),
+        dict(
+            ip_permissions=[
+                dict(
+                    ip_protocol="-1",
+                    ip_ranges=[
+                        dict(cidr_ip="192.0.2.0/24"),
+                        dict(cidr_ip="198.51.100.0/24"),
+                    ],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[
+                        dict(cidr_ipv6="2001:DB8:4000::/34"),
+                        dict(cidr_ipv6="2001:DB8:8000::/34"),
+                    ],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    from_port=38,
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[
+                        dict(prefix_list_id="pl-0a5fccee"),
+                        dict(prefix_list_id="pl-2263adef"),
+                        dict(prefix_list_id="pl-65911ba9"),
+                    ],
+                    to_port=38,
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    from_port="22",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    to_port="22",
+                    user_id_group_pairs=[
+                        dict(group_id="sg-00ec640f", user_id="012345678901"),
+                        dict(group_id="sg-3950599b", user_id="123456789012"),
+                        dict(group_id="sg-fbfd1e3a", user_id="012345678901"),
+                    ],
+                ),
+            ],
+            ip_permissions_egress=[
+                dict(
+                    ip_protocol="-1",
+                    ip_ranges=[
+                        dict(cidr_ip="192.0.2.0/24"),
+                        dict(cidr_ip="198.51.100.0/24"),
+                    ],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    user_id_group_pairs=[],
+                ),
+                dict(
+                    from_port=443,
+                    ip_protocol="tcp",
+                    ip_ranges=[],
+                    ipv6_ranges=[],
+                    prefix_list_ids=[],
+                    to_port=443,
+                    user_id_group_pairs=[
+                        dict(group_id="sg-00ec640f", user_id="012345678901"),
+                        dict(group_id="sg-fbfd1e3a", user_id="012345678901"),
+                    ],
+                ),
+            ],
+        ),
+    ),
+]
+
+
+@pytest.mark.parametrize("group, expected", SORT_ORDER)
+def test_sort_security_group(group, expected):
+    assert ec2_security_group_module.sort_security_group(group) == expected
+
+    # We shouldn't care about extra values lurking in the security group definition
+    group["junk"] = sentinel.EXTRA_JUNK
+    expected["junk"] = sentinel.EXTRA_JUNK
+    assert ec2_security_group_module.sort_security_group(group) == expected
+
+
+def test_get_rule_sort_key():
+    # Random text, to try and ensure the content of the string doesn't affect the key returned
+    dict_to_sort = dict(
+        cidr_ip="MtY0d3Ps6ePsMM0zB18g",
+        cidr_ipv6="ffbCwK2xhCsy8cyXqHuz",
+        prefix_list_id="VXKCoW296XxIRiBrTUw8",
+        group_id="RZpolpZ5wYPPpbqVo1Db",
+        sentinel=sentinel.EXTRA_RULE_KEY,
+    )
+
+    # Walk through through the keys we use and check that they have the priority we expect
+    for key_name in ["cidr_ip", "cidr_ipv6", "prefix_list_id", "group_id"]:
+        assert ec2_security_group_module.get_rule_sort_key(dict_to_sort) == dict_to_sort[key_name]
+        # Remove the current key so that the next time round another key will have priority
+        dict_to_sort.pop(key_name)
+
+    assert dict_to_sort == {"sentinel": sentinel.EXTRA_RULE_KEY}
+    assert ec2_security_group_module.get_rule_sort_key(dict_to_sort) is None
+
+
+def test_get_ip_permissions_sort_key():
+    dict_to_sort = dict(
+        ip_ranges=[
+            dict(cidr_ip="198.51.100.0/24", original_index=0),
+            dict(cidr_ip="192.0.2.0/24", original_index=1),
+            dict(cidr_ip="203.0.113.0/24", original_index=2),
+        ],
+        ipv6_ranges=[
+            dict(cidr_ipv6="2001:DB8:4000::/34", original_index=0),
+            dict(cidr_ipv6="2001:DB8:0000::/34", original_index=1),
+            dict(cidr_ipv6="2001:DB8:8000::/34", original_index=2),
+        ],
+        prefix_list_ids=[
+            dict(prefix_list_id="pl-2263adef", original_index=0),
+            dict(prefix_list_id="pl-0a5fccee", original_index=1),
+            dict(prefix_list_id="pl-65911ba9", original_index=2),
+        ],
+        user_id_group_pairs=[
+            dict(group_id="sg-3950599b", original_index=0),
+            dict(group_id="sg-fbfd1e3a", original_index=1),
+            dict(group_id="sg-00ec640f", original_index=2),
+        ],
+        sentinel=sentinel.EXTRA_RULE_KEY,
+    )
+
+    expected_keys = dict(
+        ip_ranges="ipv4:192.0.2.0/24",
+        ipv6_ranges="ipv6:2001:DB8:0000::/34",
+        prefix_list_ids="pl:pl-0a5fccee",
+        user_id_group_pairs="ugid:sg-00ec640f",
+    )
+
+    # Walk through through the keys we use and check that they have the priority we expect
+    for key_name in ["ip_ranges", "ipv6_ranges", "prefix_list_ids", "user_id_group_pairs"]:
+        sort_key = ec2_security_group_module.get_ip_permissions_sort_key(dict_to_sort)
+        assert sort_key == expected_keys[key_name]
+        # Remove the current key so that the next time round another key will have priority
+        dict_to_sort.pop(key_name)
+
+    assert dict_to_sort == {"sentinel": sentinel.EXTRA_RULE_KEY}
+    assert ec2_security_group_module.get_ip_permissions_sort_key(dict_to_sort) is None
