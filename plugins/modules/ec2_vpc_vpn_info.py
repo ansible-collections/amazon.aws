5f15ee6726f9147d3598bf766705b1ac4a5f870a
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2023-01-10 19:22:13 +0100

    Ansible User-Agent identification for community.aws (#1632)
    Ansible User-Agent identification for community.aws
    
    SUMMARY
    
    The value will be similar to this APN/1.0 Ansible/2.14.1 community.aws/6.0.0-dev0
    
    ISSUE TYPE
    
    Feature Pull Request
    
    Reviewed-by: Mark Chappell <None>
    Reviewed-by: Bikouo Aubin <None>
    Reviewed-by: Alina Buzachis <None>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/a8cbce24071bcc62fe4594c38aff1baf18bd2862

--- plugins/modules/ec2_vpc_vpn_info.py
+++ plugins/modules/ec2_vpc_vpn_info.py
@@ -166,7 +166,7 @@ try:
 except ImportError:
     pass  # caught by AnsibleAWSModule
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import (ansible_dict_to_boto3_filter_list,
                                                                      boto3_tag_list_to_ansible_dict,
                                                                      camel_dict_to_snake_dict,
