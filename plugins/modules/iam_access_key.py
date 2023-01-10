48bf7437576e3718ef103746d4348ce267d6e8ca
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2023-01-10 19:22:13 +0100

    Ansible User-Agent identification for community.aws (#1632)
    Ansible User-Agent identification for community.aws
    
    SUMMARY
    
    The value will be similar to this APN/1.0 Ansible/2.14.1 community.aws/6.0.0-dev0
    
    ISSUE TYPE
    
    Feature Pull Request
    
    Reviewed-by: Mark Chappell <None>
    Reviewed-by: Bikouo Aubin <None>
    Reviewed-by: Alina Buzachis <None>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/a8cbce24071bcc62fe4594c38aff1baf18bd2862

--- plugins/modules/iam_access_key.py
+++ plugins/modules/iam_access_key.py
@@ -126,7 +126,7 @@ except ImportError:
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.core import is_boto3_error_code
 from ansible_collections.amazon.aws.plugins.module_utils.core import normalize_boto3_result
 from ansible_collections.amazon.aws.plugins.module_utils.core import scrub_none_parameters
