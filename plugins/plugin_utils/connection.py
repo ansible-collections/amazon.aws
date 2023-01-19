7514a468095624d011ac28a739f0dc70f9ae8747
Mark Chappell <mchappel@redhat.com>
2023-01-19 13:15:21 +0100

    Add 'base' connection plugin code (plugin_utils.connection)

---
+++ plugins/plugin_utils/connection.py
@@ -0,0 +1,19 @@
+# (c) 2023 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from ansible.errors import AnsibleConnectionFailure
+from ansible.plugins.connection import ConnectionBase
+
+from ansible_collections.amazon.aws.plugins.plugin_utils.base import AWSPluginBase
+
+
+class AWSConnectionBase(AWSPluginBase, ConnectionBase):
+
+    def _do_fail(self, message):
+        raise AnsibleConnectionFailure(message)
+
+    def __init__(self, *args, boto3_version=None, botocore_version=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.require_aws_sdk(botocore_version=botocore_version, boto3_version=boto3_version)
