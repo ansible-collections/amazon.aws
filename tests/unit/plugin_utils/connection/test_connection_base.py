4a066e56bdaa95fb2fac6f4c905a063692a174c2
Mark Chappell <mchappel@redhat.com>
2023-01-19 22:25:03 +0100

    Add 'base' connection plugin code (plugin_utils.connection) (#1340)
    Add 'base' connection plugin code
    
    Depends-On: ansible/ansible-zuul-jobs#1748
    SUMMARY
    Adds a plugin_utils.connection.AWSConnectionBase to support future work on aws_ssm plugin.
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    plugin_utils/connection.py
    ADDITIONAL INFORMATION
    
    Reviewed-by: Alina Buzachis <None>
    Reviewed-by: Markus Bergholz <git@osuv.de>

---
+++ tests/unit/plugin_utils/connection/test_connection_base.py
@@ -0,0 +1,41 @@
+# (c) 2023 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import pytest
+from unittest.mock import call
+from unittest.mock import MagicMock
+from unittest.mock import sentinel
+
+from ansible.errors import AnsibleConnectionFailure
+
+import ansible_collections.amazon.aws.plugins.plugin_utils.connection as utils_connection
+
+
+def test_fail(monkeypatch):
+    monkeypatch.setattr(utils_connection.AWSConnectionBase, "__abstractmethods__", set())
+    monkeypatch.setattr(utils_connection.ConnectionBase, "__init__", MagicMock(name="__init__"))
+
+    connection_plugin = utils_connection.AWSConnectionBase()  # pylint: disable=abstract-class-instantiated
+    with pytest.raises(AnsibleConnectionFailure, match=str(sentinel.ERROR_MSG)):
+        connection_plugin._do_fail(sentinel.ERROR_MSG)
+
+
+def test_init(monkeypatch):
+    kwargs = {"example": sentinel.KWARG}
+    require_aws_sdk = MagicMock(name="require_aws_sdk")
+    require_aws_sdk.return_value = sentinel.RETURNED_SDK
+
+    monkeypatch.setattr(utils_connection.AWSConnectionBase, "__abstractmethods__", set())
+    monkeypatch.setattr(utils_connection.ConnectionBase, "__init__", MagicMock(name="__init__"))
+    monkeypatch.setattr(utils_connection.AWSConnectionBase, "require_aws_sdk", require_aws_sdk)
+
+    connection_plugin = utils_connection.AWSConnectionBase(sentinel.PARAM_TERMS, sentinel.PARAM_VARS, **kwargs)  # pylint: disable=abstract-class-instantiated
+    assert require_aws_sdk.call_args == call(botocore_version=None, boto3_version=None)
+
+    connection_plugin = utils_connection.AWSConnectionBase(  # pylint: disable=abstract-class-instantiated
+        sentinel.PARAM_ONE, sentinel.PARAM_TWO,
+        boto3_version=sentinel.PARAM_BOTO3, botocore_version=sentinel.PARAM_BOTOCORE,
+        **kwargs)
+    assert require_aws_sdk.call_args == call(botocore_version=sentinel.PARAM_BOTOCORE, boto3_version=sentinel.PARAM_BOTO3)
