50c56fe5b83e1ba6308fe1d50eec1c0bd364ac5b
Mark Chappell <mchappel@redhat.com>
2023-02-04 18:32:48 +0100

    Various lint fixups (#1707)
    Various lint fixups
    
    SUMMARY
    minor linting fixups
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    aws_ssm
    batch_job_definition
    inspector_target
    ADDITIONAL INFORMATION
    See Also: ansible-collections/news-for-maintainers#34
    
    Reviewed-by: Felix Fontein <felix@fontein.de>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/549ca71c638fafe6eac30fac16f4e7eb4f8ef7b5

--- plugins/connection/aws_ssm.py
+++ plugins/connection/aws_ssm.py
@@ -282,10 +282,13 @@ except ImportError as e:
 
 from functools import wraps
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import HAS_BOTO3
-from ansible.errors import AnsibleConnectionFailure, AnsibleError, AnsibleFileNotFound
+from ansible.errors import AnsibleConnectionFailure
+from ansible.errors import AnsibleError
+from ansible.errors import AnsibleFileNotFound
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils.six.moves import xrange
-from ansible.module_utils._text import to_bytes, to_native, to_text
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils._text import to_text
 from ansible.plugins.connection import ConnectionBase
 from ansible.plugins.shell.powershell import _common_args
 from ansible.utils.display import Display
