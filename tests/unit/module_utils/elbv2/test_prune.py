ansible-collections/amazon.aws

commit 9e182cf0e7a44179bcde4ba5c0dd9180147480bf

More work on ELBv2 module_utils (#940)
More work on ELBv2 module_utils

SUMMARY

Refactors LB creation and makes sure that  ip_address_type is set on creation (bug found when working on fixing NLB tests)
Fixes bug with DefaultAction comparisons
Extends tests for _prune_ForwardConfig
Extends tests for _prune_secrets
Fixes KeyError bug uncovered when extending tests for _prune_secrets

ISSUE TYPE

Bugfix Pull Request

COMPONENT NAME
plugins/module_utils/elbv2.py
ADDITIONAL INFORMATION
Fixes: ansible-collections/community.aws#604
See also: ansible-collections/community.aws#1365

Reviewed-by: Alina Buzachis <None>

plugins/module_utils/elbv2.py
@@ -19,6 +19,54 @@ from .ec2 import get_ec2_security_group_ids_from_names
 from .elb_utils import convert_tg_name_to_arn
 from .elb_utils import get_elb
 from .elb_utils import get_elb_listener
+from .waiters import get_waiter
+
+
+def _simple_forward_config_arn(config, parent_arn):
+    config = deepcopy(config)
+
+    stickiness = config.pop('TargetGroupStickinessConfig', {'Enabled': False})
+    # Stickiness options set, non default value
+    if stickiness != {'Enabled': False}:
+        return False
+
+    target_groups = config.pop('TargetGroups', [])
+
+    # non-default config left over, probably invalid
+    if config:
+        return False
+    # Multiple TGS, not simple
+    if len(target_groups) > 1:
+        return False
+
+    if not target_groups:
+        # with no TGs defined, but an ARN set, this is one of the minimum possible configs
+        return parent_arn or False
+
+    target_group = target_groups[0]
+    # We don't care about the weight with a single TG
+    target_group.pop('Weight', None)
+
+    target_group_arn = target_group.pop('TargetGroupArn', None)
+
+    # non-default config left over
+    if target_group:
+        return False
+
+    # We didn't find an ARN
+    if not (target_group_arn or parent_arn):
+        return False
+
+    # Only one
+    if not parent_arn:
+        return target_group_arn
+    if not target_group_arn:
+        return parent_arn
+
+    if parent_arn != target_group_arn:
+        return False
+
+    return target_group_arn
 
 
 # ForwardConfig may be optional if we've got a single TargetGroupArn entry
@@ -31,21 +79,16 @@ def _prune_ForwardConfig(action):
         return action
     if "ForwardConfig" not in action:
         return action
-    # Where we have multiple TGs, action['TargetGroupArn'] shouldn't be set
-    if "TargetGroupArn" not in action:
-        return action
 
-    # This is the same as having TargetGroupArn set
-    equivalent_action = {
-        'TargetGroupStickinessConfig': {'Enabled': False},
-        'TargetGroups': [{"TargetGroupArn": action["TargetGroupArn"], "Weight": 1}],
-    }
-    if action["ForwardConfig"] != equivalent_action:
+    parent_arn = action.get('TargetGroupArn', None)
+    arn = _simple_forward_config_arn(action["ForwardConfig"], parent_arn)
+    if not arn:
         return action
 
     # Remove the redundant ForwardConfig
     newAction = action.copy()
     del(newAction["ForwardConfig"])
+    newAction["TargetGroupArn"] = arn
     return newAction
 
 
@@ -53,8 +96,13 @@ def _prune_ForwardConfig(action):
 # or the module will always see the new and current actions as different
 # and try to apply the same config
 def _prune_secret(action):
-    if action['Type'] == 'authenticate-oidc':
-        action['AuthenticateOidcConfig'].pop('ClientSecret')
+    if action['Type'] != 'authenticate-oidc':
+        return action
+
+    action['AuthenticateOidcConfig'].pop('ClientSecret', None)
+    if action['AuthenticateOidcConfig'].get('UseExistingClientSecret', False):
+        action['AuthenticateOidcConfig'].pop('UseExistingClientSecret')
+
     return action
 
 
@@ -75,22 +123,47 @@ class ElasticLoadBalancerV2(object):
         self.subnet_mappings = module.params.get("subnet_mappings")
         self.subnets = module.params.get("subnets")
         self.deletion_protection = module.params.get("deletion_protection")
+        self.elb_ip_addr_type = module.params.get("ip_address_type")
         self.wait = module.params.get("wait")
 
         if module.params.get("tags") is not None:
             self.tags = ansible_dict_to_boto3_tag_list(module.params.get("tags"))
         else:
             self.tags = None
+
         self.purge_tags = module.params.get("purge_tags")
 
         self.elb = get_elb(connection, module, self.name)
         if self.elb is not None:
             self.elb_attributes = self.get_elb_attributes()
-            self.elb['tags'] = self.get_elb_tags()
             self.elb_ip_addr_type = self.get_elb_ip_address_type()
+            self.elb['tags'] = self.get_elb_tags()
         else:
             self.elb_attributes = None
-            self.elb_ip_addr_type = None
+
+    def wait_for_ip_type(self, elb_arn, ip_type):
+        """
+        Wait for load balancer to reach 'active' status
+
+        :param elb_arn: The load balancer ARN
+        :return:
+        """
+
+        if not self.wait:
+            return
+
+        waiter_names = {
+            'ipv4': 'load_balancer_ip_address_type_ipv4',
+            'dualstack': 'load_balancer_ip_address_type_dualstack',
+        }
+        if ip_type not in waiter_names:
+            return
+
+        try:
+            waiter = get_waiter(self.connection, waiter_names.get(ip_type))
+            waiter.wait(LoadBalancerArns=[elb_arn])
+        except (BotoCoreError, ClientError) as e:
+            self.module.fail_json_aws(e)
 
     def wait_for_status(self, elb_arn):
         """
@@ -100,8 +173,28 @@ class ElasticLoadBalancerV2(object):
         :return:
         """
 
+        if not self.wait:
+            return
+
         try:
-            waiter = self.connection.get_waiter('load_balancer_available')
+            waiter = get_waiter(self.connection, 'load_balancer_available')
+            waiter.wait(LoadBalancerArns=[elb_arn])
+        except (BotoCoreError, ClientError) as e:
+            self.module.fail_json_aws(e)
+
+    def wait_for_deletion(self, elb_arn):
+        """
+        Wait for load balancer to reach 'active' status
+
+        :param elb_arn: The load balancer ARN
+        :return:
+        """
+
+        if not self.wait:
+            return
+
+        try:
+            waiter = get_waiter(self.connection, 'load_balancers_deleted')
             waiter.wait(LoadBalancerArns=[elb_arn])
         except (BotoCoreError, ClientError) as e:
             self.module.fail_json_aws(e)
@@ -200,6 +293,8 @@ class ElasticLoadBalancerV2(object):
         except (BotoCoreError, ClientError) as e:
             self.module.fail_json_aws(e)
 
+        self.wait_for_deletion(self.elb['LoadBalancerArn'])
+
         self.changed = True
 
     def compare_subnets(self):
@@ -264,15 +359,57 @@ class ElasticLoadBalancerV2(object):
         Modify ELB ip address type
         :return:
         """
-        if self.elb_ip_addr_type != ip_addr_type:
-            try:
-                AWSRetry.jittered_backoff()(
-                    self.connection.set_ip_address_type
-                )(LoadBalancerArn=self.elb['LoadBalancerArn'], IpAddressType=ip_addr_type)
-            except (BotoCoreError, ClientError) as e:
-                self.module.fail_json_aws(e)
+        if ip_addr_type is None:
+            return
+        if self.elb_ip_addr_type == ip_addr_type:
+            return
 
+        try:
+            AWSRetry.jittered_backoff()(
+                self.connection.set_ip_address_type
+            )(LoadBalancerArn=self.elb['LoadBalancerArn'], IpAddressType=ip_addr_type)
+        except (BotoCoreError, ClientError) as e:
+            self.module.fail_json_aws(e)
+
+        self.changed = True
+        self.wait_for_ip_type(self.elb['LoadBalancerArn'], ip_addr_type)
+
+    def _elb_create_params(self):
+        # Required parameters
+        params = dict()
+        params['Name'] = self.name
+        params['Type'] = self.type
+
+        # Other parameters
+        if self.elb_ip_addr_type is not None:
+            params['IpAddressType'] = self.elb_ip_addr_type
+        if self.subnets is not None:
+            params['Subnets'] = self.subnets
+        if self.subnet_mappings is not None:
+            params['SubnetMappings'] = self.subnet_mappings
+        if self.tags:
+            params['Tags'] = self.tags
+        # Scheme isn't supported for GatewayLBs, so we won't add it here, even though we don't
+        # support them yet.
+
+        return params
+
+    def create_elb(self):
+        """
+        Create a load balancer
+        :return:
+        """
+
+        params = self._elb_create_params()
+
+        try:
+            self.elb = AWSRetry.jittered_backoff()(self.connection.create_load_balancer)(**params)['LoadBalancers'][0]
             self.changed = True
+            self.new_load_balancer = True
+        except (BotoCoreError, ClientError) as e:
+            self.module.fail_json_aws(e)
+
+        self.wait_for_status(self.elb['LoadBalancerArn'])
 
 
 class ApplicationLoadBalancer(ElasticLoadBalancerV2):
@@ -314,37 +451,14 @@ class ApplicationLoadBalancer(ElasticLoadBalancerV2):
         if self.elb is not None and self.elb['Type'] != 'application':
             self.module.fail_json(msg="The load balancer type you are trying to manage is not application. Try elb_network_lb module instead.")
 
-    def create_elb(self):
-        """
-        Create a load balancer
-        :return:
-        """
+    def _elb_create_params(self):
+        params = super()._elb_create_params()
 
-        # Required parameters
-        params = dict()
-        params['Name'] = self.name
-        params['Type'] = self.type
-
-        # Other parameters
-        if self.subnets is not None:
-            params['Subnets'] = self.subnets
-        if self.subnet_mappings is not None:
-            params['SubnetMappings'] = self.subnet_mappings
         if self.security_groups is not None:
             params['SecurityGroups'] = self.security_groups
         params['Scheme'] = self.scheme
-        if self.tags:
-            params['Tags'] = self.tags
 
-        try:
-            self.elb = AWSRetry.jittered_backoff()(self.connection.create_load_balancer)(**params)['LoadBalancers'][0]
-            self.changed = True
-            self.new_load_balancer = True
-        except (BotoCoreError, ClientError) as e:
-            self.module.fail_json_aws(e)
-
-        if self.wait:
-            self.wait_for_status(self.elb['LoadBalancerArn'])
+        return params
 
     def compare_elb_attributes(self):
         """
@@ -485,35 +599,12 @@ class NetworkLoadBalancer(ElasticLoadBalancerV2):
         if self.elb is not None and self.elb['Type'] != 'network':
             self.module.fail_json(msg="The load balancer type you are trying to manage is not network. Try elb_application_lb module instead.")
 
-    def create_elb(self):
-        """
-        Create a load balancer
-        :return:
-        """
+    def _elb_create_params(self):
+        params = super()._elb_create_params()
 
-        # Required parameters
-        params = dict()
-        params['Name'] = self.name
-        params['Type'] = self.type
-
-        # Other parameters
-        if self.subnets is not None:
-            params['Subnets'] = self.subnets
-        if self.subnet_mappings is not None:
-            params['SubnetMappings'] = self.subnet_mappings
         params['Scheme'] = self.scheme
-        if self.tags:
-            params['Tags'] = self.tags
 
-        try:
-            self.elb = AWSRetry.jittered_backoff()(self.connection.create_load_balancer)(**params)['LoadBalancers'][0]
-            self.changed = True
-            self.new_load_balancer = True
-        except (BotoCoreError, ClientError) as e:
-            self.module.fail_json_aws(e)
-
-        if self.wait:
-            self.wait_for_status(self.elb['LoadBalancerArn'])
+        return params
 
     def modify_elb_attributes(self):
         """

tests/unit/module_utils/elbv2/test_prune.py
@@ -1,0 +1,182 @@
+#
+# (c) 2021 Red Hat Inc.
+#
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.amazon.aws.plugins.module_utils import elbv2
+
+example_arn = 'arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/nlb-123456789abc/abcdef0123456789'
+example_arn2 = 'arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/nlb-0123456789ab/0123456789abcdef'
+
+one_action = [
+    dict(
+        ForwardConfig=dict(
+            TargetGroupStickinessConfig=dict(Enabled=False),
+            TargetGroups=[
+                dict(TargetGroupArn=example_arn, Weight=1),
+            ]
+        ),
+        TargetGroupArn=example_arn, Type='forward',
+    )
+]
+
+one_action_two_tg = [
+    dict(
+        ForwardConfig=dict(
+            TargetGroupStickinessConfig=dict(Enabled=False),
+            TargetGroups=[
+                dict(TargetGroupArn=example_arn, Weight=1),
+                dict(TargetGroupArn=example_arn2, Weight=1),
+            ]
+        ),
+        TargetGroupArn=example_arn, Type='forward',
+    )
+]
+
+simplified_action = dict(Type='forward', TargetGroupArn=example_arn)
+# Examples of various minimalistic actions which are all the same
+simple_actions = [
+    dict(Type='forward', TargetGroupArn=example_arn),
+
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn)])),
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn, Weight=1)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn, Weight=1)])),
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn, Weight=42)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroups=[dict(TargetGroupArn=example_arn, Weight=42)])),
+
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False),
+                                                                        TargetGroups=[dict(TargetGroupArn=example_arn)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False), TargetGroups=[dict(TargetGroupArn=example_arn)])),
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False),
+                                                                        TargetGroups=[dict(TargetGroupArn=example_arn, Weight=1)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False), TargetGroups=[dict(TargetGroupArn=example_arn, Weight=1)])),
+    dict(Type='forward', TargetGroupArn=example_arn, ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False),
+                                                                        TargetGroups=[dict(TargetGroupArn=example_arn, Weight=42)])),
+    dict(Type='forward', ForwardConfig=dict(TargetGroupStickinessConfig=dict(Enabled=False), TargetGroups=[dict(TargetGroupArn=example_arn, Weight=42)])),
+]
+
+# Test that _prune_ForwardConfig() doesn't mangle things we don't expect
+complex_actions = [
+    # Non-Forwarding
+    dict(
+        Type='authenticate-oidc', TargetGroupArn=example_arn,
+        AuthenticateOidcConfig=dict(
+            Issuer='https://idp.ansible.test/oidc-config',
+            AuthorizationEndpoint='https://idp.ansible.test/authz',
+            TokenEndpoint='https://idp.ansible.test/token',
+            UserInfoEndpoint='https://idp.ansible.test/user',
+            ClientId='ExampleClient',
+            UseExistingClientSecret=False,
+        ),
+    ),
+    dict(
+        Type='redirect',
+        RedirectConfig=dict(Protocol='HTTPS', Port=443, Host='redirect.ansible.test', Path='/', StatusCode='HTTP_302'),
+    ),
+    # Multiple TGs
+    dict(
+        TargetGroupArn=example_arn, Type='forward',
+        ForwardConfig=dict(
+            TargetGroupStickinessConfig=dict(Enabled=False),
+            TargetGroups=[
+                dict(TargetGroupArn=example_arn, Weight=1),
+                dict(TargetGroupArn=example_arn2, Weight=1),
+            ]
+        ),
+    ),
+    # Sticky-Sessions
+    dict(
+        Type='forward', TargetGroupArn=example_arn,
+        ForwardConfig=dict(
+            TargetGroupStickinessConfig=dict(Enabled=True, DurationSeconds=3600),
+            TargetGroups=[dict(TargetGroupArn=example_arn)]
+        )
+    ),
+]
+
+simplified_oidc_action = dict(
+    Type='authenticate-oidc', TargetGroupArn=example_arn,
+    AuthenticateOidcConfig=dict(
+        Issuer='https://idp.ansible.test/oidc-config',
+        AuthorizationEndpoint='https://idp.ansible.test/authz',
+        TokenEndpoint='https://idp.ansible.test/token',
+        UserInfoEndpoint='https://idp.ansible.test/user',
+        ClientId='ExampleClient',
+    ),
+)
+oidc_actions = [
+    dict(
+        Type='authenticate-oidc', TargetGroupArn=example_arn,
+        AuthenticateOidcConfig=dict(
+            Issuer='https://idp.ansible.test/oidc-config',
+            AuthorizationEndpoint='https://idp.ansible.test/authz',
+            TokenEndpoint='https://idp.ansible.test/token',
+            UserInfoEndpoint='https://idp.ansible.test/user',
+            ClientId='ExampleClient',
+            UseExistingClientSecret=True,
+        ),
+    ),
+    dict(
+        Type='authenticate-oidc', TargetGroupArn=example_arn,
+        AuthenticateOidcConfig=dict(
+            Issuer='https://idp.ansible.test/oidc-config',
+            AuthorizationEndpoint='https://idp.ansible.test/authz',
+            TokenEndpoint='https://idp.ansible.test/token',
+            UserInfoEndpoint='https://idp.ansible.test/user',
+            ClientId='ExampleClient',
+            ClientSecret='MyVerySecretString',
+        ),
+    ),
+]
+
+
+####
+
+
+# Original tests
+def test__prune_secret():
+    assert elbv2._prune_secret(one_action[0]) == one_action[0]
+
+
+def test__prune_ForwardConfig():
+    expectation = {"TargetGroupArn": example_arn, "Type": "forward"}
+    pruned_config = elbv2._prune_ForwardConfig(one_action[0])
+    assert pruned_config == expectation
+
+    # https://github.com/ansible-collections/community.aws/issues/1089
+    pruned_config = elbv2._prune_ForwardConfig(one_action_two_tg[0])
+    assert pruned_config == one_action_two_tg[0]
+
+
+####
+
+
+@pytest.mark.parametrize("action", simple_actions)
+def test__prune_ForwardConfig_simplifiable_actions(action):
+    pruned_config = elbv2._prune_ForwardConfig(action)
+    assert pruned_config == simplified_action
+
+
+@pytest.mark.parametrize("action", complex_actions)
+def test__prune_ForwardConfig_non_simplifiable_actions(action):
+    pruned_config = elbv2._prune_ForwardConfig(action)
+    assert pruned_config == action
+
+
+@pytest.mark.parametrize("action", oidc_actions)
+def test__prune_secret_simplifiable_actions(action):
+    pruned_config = elbv2._prune_secret(action)
+    assert pruned_config == simplified_oidc_action
+
+
+@pytest.mark.parametrize("action", complex_actions)
+def test__prune_secret_non_simplifiable_actions(action):
+    pruned_config = elbv2._prune_secret(action)
+    assert pruned_config == action

commit c6906a3f97f776b7c6bf83ac51ee6f2456fa7ae5

elbv2: respect UseExistingClientSecret (#1270)
elbv2: respect UseExistingClientSecret

SUMMARY
Since amazon.aws 5.0.0, elb_application_lb runs into an exception, when using Type: authenticate-oidc in a rule, even when UseExistingClientSecret: True parameter is given. That works as expected with amazon.aws 4.x.x.
The logic gets broken in  #940
Basically AWS won't return both, UseExistingClientSecret and  ClientSecret.
But when requesting against boto3,  both parameters are mutually exclusive!
When the user set UseExistingClientSecret: True, the ClientSecret must be removed for the request.
When the user does not set UseExistingClientSecret or set it to False,  the UseExistingClientSecret must be included in the request.
While diving deeper, I've noticed a basic change detection problem for default values, that are not requested, but AWS will return them. I've summerized it in #1284
However, this PR does not target #1284, it just fixes the exception and restores the functionality and hotfix the change-detection only for Type: authenticate-oidc.
origin PR description

The error was: botocore.errorfactory.InvalidLoadBalancerActionException: An error occurred (InvalidLoadBalancerAction) when calling the ModifyRule operation: You must either specify a client secret or set UseExistingClientSecret to true

UseExistingClientSecret is not respected anymore since a.a 5
Introduced in #940
Furthermore, AWS returns also Scope and  SessionTimeout parameters that are filled with default values if not requested.
'Scope': 'openid',
'SessionTimeout': 604800,

That make the module always returns a change, if they are not requested.
This fix does not break backwards compatibility, because the values are already set by aws, when not requested yet.
ISSUE TYPE


Bugfix Pull Request

COMPONENT NAME
plugins/module_utils/elbv2.yml
ADDITIONAL INFORMATION


          - Conditions:
              - Field: host-header
                Values:
                  - some.tld

plugins/module_utils/elbv2.py
@@ -89,16 +89,31 @@ def _prune_ForwardConfig(action):
     return newAction
 
 
-# the AWS api won't return the client secret, so we'll have to remove it
-# or the module will always see the new and current actions as different
-# and try to apply the same config
+# remove the client secret if UseExistingClientSecret, because aws won't return it
+# add default values when they are not requested
 def _prune_secret(action):
     if action['Type'] != 'authenticate-oidc':
         return action
 
-    action['AuthenticateOidcConfig'].pop('ClientSecret', None)
+    if not action['AuthenticateOidcConfig'].get('Scope', False):
+        action['AuthenticateOidcConfig']['Scope'] = 'openid'
+
+    if not action['AuthenticateOidcConfig'].get('SessionTimeout', False):
+        action['AuthenticateOidcConfig']['SessionTimeout'] = 604800
+
     if action['AuthenticateOidcConfig'].get('UseExistingClientSecret', False):
-        action['AuthenticateOidcConfig'].pop('UseExistingClientSecret')
+        action['AuthenticateOidcConfig'].pop('ClientSecret', None)
+
+    return action
+
+
+# while AWS api also won't return UseExistingClientSecret key
+# it must be added, because it's requested and compared
+def _append_use_existing_client_secretn(action):
+    if action['Type'] != 'authenticate-oidc':
+        return action
+
+    action['AuthenticateOidcConfig']['UseExistingClientSecret'] = True
 
     return action
 
@@ -993,9 +1008,10 @@ class ELBListenerRules:
             current_actions_sorted = _sort_actions(current_rule['Actions'])
             new_actions_sorted = _sort_actions(new_rule['Actions'])
 
+            new_current_actions_sorted = [_append_use_existing_client_secretn(i) for i in current_actions_sorted]
             new_actions_sorted_no_secret = [_prune_secret(i) for i in new_actions_sorted]
 
-            if [_prune_ForwardConfig(i) for i in current_actions_sorted] != [_prune_ForwardConfig(i) for i in new_actions_sorted_no_secret]:
+            if [_prune_ForwardConfig(i) for i in new_current_actions_sorted] != [_prune_ForwardConfig(i) for i in new_actions_sorted_no_secret]:
                 modified_rule['Actions'] = new_rule['Actions']
         # If the action lengths are different, then replace with the new actions
         else:

plugins/modules/elb_application_lb.py
@@ -138,6 +138,9 @@ options:
               - A list of ALB Listener Rules.
               - 'For the complete documentation of possible Conditions and Actions please see the boto3 documentation:'
               - 'https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elbv2.html#ElasticLoadBalancingv2.Client.create_rule'
+              - >
+                Keep in mind that AWS uses default values for parameters that are not requested. For example for I(Scope)
+                and I(SessionTimeout) when the action type is C(authenticate-oidc).
             suboptions:
                 Conditions:
                     type: list

tests/unit/module_utils/elbv2/test_prune.py
@@ -106,6 +106,9 @@ simplified_oidc_action = dict(
         TokenEndpoint='https://idp.ansible.test/token',
         UserInfoEndpoint='https://idp.ansible.test/user',
         ClientId='ExampleClient',
+        Scope='openid',
+        SessionTimeout=604800,
+        UseExistingClientSecret=True,
     ),
 )
 oidc_actions = [
@@ -118,6 +121,8 @@ oidc_actions = [
             UserInfoEndpoint='https://idp.ansible.test/user',
             ClientId='ExampleClient',
             UseExistingClientSecret=True,
+            Scope='openid',
+            SessionTimeout=604800
         ),
     ),
     dict(
@@ -129,6 +134,7 @@ oidc_actions = [
             UserInfoEndpoint='https://idp.ansible.test/user',
             ClientId='ExampleClient',
             ClientSecret='MyVerySecretString',
+            UseExistingClientSecret=True,
         ),
     ),
 ]