493aee248088d3cda63483ed3f2cfe5516da3a3e
Mark Chappell <mchappel@redhat.com>
2023-03-01 10:33:21 +0100

    unused import

--- plugins/modules/ec2_vpc_net.py
+++ plugins/modules/ec2_vpc_net.py
@@ -209,7 +209,6 @@ from ansible.module_utils.common.network import to_subnet
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_message
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import ansible_dict_to_boto3_tag_list
