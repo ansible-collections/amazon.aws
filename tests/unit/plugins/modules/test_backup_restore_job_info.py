f992d1809966ef151815e411feaed40ba7ac98f0
Mandar Kulkarni <mandar242@gmail.com>
2023-03-22 17:53:54 -0700

    add unit tests for _list_restore_jobs

--- tests/unit/plugins/modules/test_backup_restore_job_info.py
+++ tests/unit/plugins/modules/test_backup_restore_job_info.py
@@ -23,3 +23,36 @@ module_name = "ansible_collections.amazon.aws.plugins.modules.backup_restore_job
 )
 def test_build_request_args(account_id, status, created_before, created_after, completed_before, completed_after, expected):
     assert backup_restore_job_info.build_request_args(account_id, status, created_before, created_after, completed_before, completed_after) == expected
+
+
+def test__list_restore_jobs():
+
+    connection = MagicMock()
+    conn_paginator = MagicMock()
+    paginate = MagicMock()
+
+    request_args = {"ByAccountId": '123456789012'}
+
+    restore_job = {
+        'AccountId': '123456789012',
+        'BackupSizeInBytes': '8589934592',
+        'CompletionDate': '2023-03-13T15:53:07.172000-07:00',
+        'CreatedResourceArn': 'arn:aws:ec2:us-east-2:123456789012:instance/i-01234567ec51af3f',
+        'CreationDate': '2023-03-13T15:53:07.172000-07:00',
+        'IamRoleArn': 'arn:aws:iam::123456789012:role/service-role/AWSBackupDefaultServiceRole',
+        'PercentDone': '0.00%',
+        'RecoveryPointArn': 'arn:aws:ec2:us-east-2::image/ami-01234567ec51af3f',
+        'ResourceType': 'EC2', 'RestoreJobId': '52BEE289-xxxx-xxxx-xxxx-47DCAA2E7ACD',
+        'Status': 'COMPLETED'
+    }
+
+    connection.get_paginator.return_value = conn_paginator
+    conn_paginator.paginate.return_value = paginate
+
+    paginate.build_full_result.return_value = {'RestoreJobs': [restore_job]}
+
+    result = backup_restore_job_info._list_restore_jobs(connection=connection, **request_args)
+
+    assert result == paginate.build_full_result.return_value
+    connection.get_paginator.assert_called_with("list_restore_jobs")
+    conn_paginator.paginate.assert_called_with(**request_args)
