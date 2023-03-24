3010fb1b71f89934b66fa091279b68345f55fd05
Mandar Kulkarni <mandar242@gmail.com>
2023-03-24 13:37:48 -0700

    add unit tests for list_restore_jobs

--- tests/unit/plugins/modules/test_backup_restore_job_info.py
+++ tests/unit/plugins/modules/test_backup_restore_job_info.py
@@ -71,3 +71,46 @@ def test__list_restore_jobs():
     assert result == paginate.build_full_result.return_value
     connection.get_paginator.assert_called_with("list_restore_jobs")
     conn_paginator.paginate.assert_called_with(**request_args)
+
+
+@patch(module_name + "._list_restore_jobs")
+def test_list_restore_jobs(m__list_restore_jobs):
+    connection = MagicMock()
+    module = MagicMock()
+
+    request_args = {"ByAccountId": "123456789012"}
+
+    m__list_restore_jobs.return_value = {
+        "RestoreJobs": [
+            {
+                "AccountId": "123456789012",
+                "BackupSizeInBytes": "8589934592",
+                "CompletionDate": "2023-03-13T15:53:07.172000-07:00",
+                "CreatedResourceArn": "arn:aws:ec2:us-east-2:123456789012:instance/i-01234567ec51af3f",
+                "CreationDate": "2023-03-13T15:53:07.172000-07:00",
+                "IamRoleArn": "arn:aws:iam::123456789012:role/service-role/AWSBackupDefaultServiceRole",
+                "PercentDone": "0.00%",
+                "RecoveryPointArn": "arn:aws:ec2:us-east-2::image/ami-01234567ec51af3f",
+                "ResourceType": "EC2",
+                "RestoreJobId": "52BEE289-xxxx-xxxx-xxxx-47DCAA2E7ACD",
+                "Status": "COMPLETED",
+            }
+        ]
+    }
+
+    list_restore_jobs_result = backup_restore_job_info.list_restore_jobs(connection, module, request_args)
+
+    assert m__list_restore_jobs.call_count == 1
+    m__list_restore_jobs.assert_called_with(connection, **request_args)
+    assert len(list_restore_jobs_result) == 1
+
+
+@patch(module_name + ".AnsibleAWSModule")
+def test_main_success(m_AnsibleAWSModule):
+    m_module = MagicMock()
+    m_AnsibleAWSModule.return_value = m_module
+
+    backup_restore_job_info.main()
+
+    m_module.client.assert_called_with("backup")
+    m_module.exit_json.assert_called_with(changed=False, restore_jobs=[])
