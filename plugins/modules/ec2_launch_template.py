162d94d0844fbc26587cfe49321ed8ceb7371996
Squirrel <squirrel532@protonmail.com>
2023-04-12 15:45:13 +0800

    Add version_description to ec2_launch_template (#1763)
    Add version_description to ec2_launch_template
    
    SUMMARY
    Add version_description to ec2_launch_template module, which allows user update the VersionDescription of a launch template.
    Fix #1762
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    ec2_launch_template
    ADDITIONAL INFORMATION
    
    Reviewed-by: Markus Bergholz <git@osuv.de>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/3c3698d79ce5f56cefd24efe98cc49e6330f4c29

--- plugins/modules/ec2_launch_template.py
+++ plugins/modules/ec2_launch_template.py
@@ -38,6 +38,12 @@ options:
     - Which version should be the default when users spin up new instances based on this template? By default, the latest version will be made the default.
     type: str
     default: latest
+  version_description:
+    version_added: 5.5.0
+    description:
+    - The description of a launch template version.
+    default: ""
+    type: str
   state:
     description:
     - Whether the launch template should exist or not.
@@ -576,8 +582,10 @@ def create_or_update(module, template_options):
         template, template_versions = existing_templates(module)
         out['changed'] = True
     elif template and template_versions:
-        most_recent = sorted(template_versions, key=lambda x: x['VersionNumber'])[-1]
-        if lt_data == most_recent['LaunchTemplateData']:
+        most_recent = sorted(template_versions, key=lambda x: x["VersionNumber"])[-1]
+        if lt_data == most_recent["LaunchTemplateData"] and module.params["version_description"] == most_recent.get(
+            "VersionDescription", ""
+        ):
             out['changed'] = False
             return out
         try:
@@ -586,6 +594,7 @@ def create_or_update(module, template_options):
                     LaunchTemplateId=template['LaunchTemplateId'],
                     LaunchTemplateData=lt_data,
                     ClientToken=uuid4().hex,
+                    VersionDescription=str(module.params["version_description"]),
                     aws_retry=True,
                 )
             elif module.params.get('source_version') == 'latest':
@@ -593,7 +602,8 @@ def create_or_update(module, template_options):
                     LaunchTemplateId=template['LaunchTemplateId'],
                     LaunchTemplateData=lt_data,
                     ClientToken=uuid4().hex,
-                    SourceVersion=str(most_recent['VersionNumber']),
+                    SourceVersion=str(most_recent["VersionNumber"]),
+                    VersionDescription=str(module.params["version_description"]),
                     aws_retry=True,
                 )
             else:
@@ -609,7 +619,8 @@ def create_or_update(module, template_options):
                     LaunchTemplateId=template['LaunchTemplateId'],
                     LaunchTemplateData=lt_data,
                     ClientToken=uuid4().hex,
-                    SourceVersion=str(source_version['VersionNumber']),
+                    SourceVersion=str(source_version["VersionNumber"]),
+                    VersionDescription=str(module.params["version_description"]),
                     aws_retry=True,
                 )
 
@@ -782,11 +793,12 @@ def main():
     )
 
     arg_spec = dict(
-        state=dict(choices=['present', 'absent'], default='present'),
-        template_name=dict(aliases=['name']),
-        template_id=dict(aliases=['id']),
-        default_version=dict(default='latest'),
-        source_version=dict(default='latest')
+        state=dict(choices=["present", "absent"], default="present"),
+        template_name=dict(aliases=["name"]),
+        template_id=dict(aliases=["id"]),
+        default_version=dict(default="latest"),
+        source_version=dict(default="latest"),
+        version_description=dict(default=""),
     )
 
     arg_spec.update(template_options)
