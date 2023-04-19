b19cb6967bdf2a666df20deef0a27aa123a7dd98
Gonéri Le Bouder <goneri@lebouder.net>
2023-04-19 03:29:06 -0400

    elasticache_info: ignore CacheClusterNotFound exeption during tag collect (#1777)
    elasticache_info: ignore CacheClusterNotFound exeption during tag collect
    
    If we call get_elasticache_tags_with_backoff() on a cluster with an invalid
    state (eg: deleting), AWS will trigger an CacheClusterNotFound.
    With this change, elasticache_info will ignore the cluster and continue with
    the next one.
    
    Reviewed-by: Mark Chappell

--- elasticache_info.py
+++ elasticache_info.py
@@ -471,6 +471,9 @@ def get_elasticache_clusters(client, module):
         arn = "arn:aws:elasticache:%s:%s:cluster:%s" % (region, account_id, cluster['cache_cluster_id'])
         try:
             tags = get_elasticache_tags_with_backoff(client, arn)
+        except is_boto3_error_code("CacheClusterNotFound"):
+            # e.g: Cluster was listed but is in deleting state
+            continue
         except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
             module.fail_json_aws(e, msg="Couldn't get tags for cluster %s")
 
