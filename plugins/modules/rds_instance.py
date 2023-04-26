b7e77b44d64cd5bfed4d34f70561d8a462bb34f1
Mandar Kulkarni <mandar242@gmail.com>
2023-04-26 09:09:25 -0700

    add version_added for ca_certificate_identifier

--- plugins/modules/rds_instance.py
+++ plugins/modules/rds_instance.py
@@ -99,6 +99,7 @@ options:
         description:
           - The identifier of the CA certificate for the DB instance.
         type: str
+        version_added: 5.5.0
     character_set_name:
         description:
           - The character set to associate with the DB instance.
