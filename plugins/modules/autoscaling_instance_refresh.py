787fae453b70da36786ffeb5901b4955c7a73a49
Mark Chappell <mchappel@redhat.com>
2023-04-26 19:26:07 +0200

    Big Black PR (#1784)
    * Black prep
    
    * Black
    
    * changelog
    
    * Fix pylint unused-import in tests
    
    * Split SSM connection plugin changes
    
    * disable glue tests - bucket's missing
    
    * Disable s3_logging and s3_sync tests
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/2c4575c248776c65d66b06cd60fa09b0dae1cd6f

--- plugins/modules/autoscaling_instance_refresh.py
+++ plugins/modules/autoscaling_instance_refresh.py
@@ -179,75 +179,75 @@ def start_or_cancel_instance_refresh(conn, module):
         }
     """
 
-    asg_state = module.params.get('state')
-    asg_name = module.params.get('name')
-    preferences = module.params.get('preferences')
+    asg_state = module.params.get("state")
+    asg_name = module.params.get("name")
+    preferences = module.params.get("preferences")
 
     args = {}
-    args['AutoScalingGroupName'] = asg_name
-    if asg_state == 'started':
-        args['Strategy'] = module.params.get('strategy')
+    args["AutoScalingGroupName"] = asg_name
+    if asg_state == "started":
+        args["Strategy"] = module.params.get("strategy")
     if preferences:
-        if asg_state == 'cancelled':
-            module.fail_json(msg='can not pass preferences dict when canceling a refresh')
+        if asg_state == "cancelled":
+            module.fail_json(msg="can not pass preferences dict when canceling a refresh")
         _prefs = scrub_none_parameters(preferences)
-        args['Preferences'] = snake_dict_to_camel_dict(_prefs, capitalize_first=True)
+        args["Preferences"] = snake_dict_to_camel_dict(_prefs, capitalize_first=True)
     cmd_invocations = {
-        'cancelled': conn.cancel_instance_refresh,
-        'started': conn.start_instance_refresh,
+        "cancelled": conn.cancel_instance_refresh,
+        "started": conn.start_instance_refresh,
     }
     try:
         if module.check_mode:
-            if asg_state == 'started':
-                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get('InstanceRefreshes', '[]')
+            if asg_state == "started":
+                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get(
+                    "InstanceRefreshes", "[]"
+                )
                 if ongoing_refresh:
-                    module.exit_json(changed=False, msg='In check_mode - Instance Refresh is already in progress, can not start new instance refresh.')
+                    module.exit_json(
+                        changed=False,
+                        msg="In check_mode - Instance Refresh is already in progress, can not start new instance refresh.",
+                    )
                 else:
-                    module.exit_json(changed=True, msg='Would have started instance refresh if not in check mode.')
-            elif asg_state == 'cancelled':
-                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get('InstanceRefreshes', '[]')[0]
-                if ongoing_refresh.get('Status', '') in ['Cancelling', 'Cancelled']:
-                    module.exit_json(changed=False, msg='In check_mode - Instance Refresh already cancelled or is pending cancellation.')
+                    module.exit_json(changed=True, msg="Would have started instance refresh if not in check mode.")
+            elif asg_state == "cancelled":
+                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get(
+                    "InstanceRefreshes", "[]"
+                )[0]
+                if ongoing_refresh.get("Status", "") in ["Cancelling", "Cancelled"]:
+                    module.exit_json(
+                        changed=False,
+                        msg="In check_mode - Instance Refresh already cancelled or is pending cancellation.",
+                    )
                 elif not ongoing_refresh:
-                    module.exit_json(chaned=False, msg='In check_mode - No active referesh found, nothing to cancel.')
+                    module.exit_json(chaned=False, msg="In check_mode - No active referesh found, nothing to cancel.")
                 else:
-                    module.exit_json(changed=True, msg='Would have cancelled instance refresh if not in check mode.')
+                    module.exit_json(changed=True, msg="Would have cancelled instance refresh if not in check mode.")
         result = cmd_invocations[asg_state](aws_retry=True, **args)
-        instance_refreshes = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name, InstanceRefreshIds=[result['InstanceRefreshId']])
-        result = dict(
-            instance_refreshes=camel_dict_to_snake_dict(instance_refreshes['InstanceRefreshes'][0])
+        instance_refreshes = conn.describe_instance_refreshes(
+            AutoScalingGroupName=asg_name, InstanceRefreshIds=[result["InstanceRefreshId"]]
         )
+        result = dict(instance_refreshes=camel_dict_to_snake_dict(instance_refreshes["InstanceRefreshes"][0]))
         return module.exit_json(**result)
     except (BotoCoreError, ClientError) as e:
-        module.fail_json_aws(
-            e,
-            msg='Failed to {0} InstanceRefresh'.format(
-                asg_state.replace('ed', '')
-            )
-        )
+        module.fail_json_aws(e, msg="Failed to {0} InstanceRefresh".format(asg_state.replace("ed", "")))
 
 
 def main():
-
     argument_spec = dict(
         state=dict(
-            type='str',
+            type="str",
             required=True,
-            choices=['started', 'cancelled'],
+            choices=["started", "cancelled"],
         ),
         name=dict(required=True),
-        strategy=dict(
-            type='str',
-            default='Rolling',
-            required=False
-        ),
+        strategy=dict(type="str", default="Rolling", required=False),
         preferences=dict(
-            type='dict',
+            type="dict",
             required=False,
             options=dict(
-                min_healthy_percentage=dict(type='int', default=90),
-                instance_warmup=dict(type='int'),
-            )
+                min_healthy_percentage=dict(type="int", default=90),
+                instance_warmup=dict(type="int"),
+            ),
         ),
     )
 
@@ -256,15 +256,12 @@ def main():
         supports_check_mode=True,
     )
     autoscaling = module.client(
-        'autoscaling',
-        retry_decorator=AWSRetry.jittered_backoff(
-            retries=10,
-            catch_extra_error_codes=['InstanceRefreshInProgress']
-        )
+        "autoscaling",
+        retry_decorator=AWSRetry.jittered_backoff(retries=10, catch_extra_error_codes=["InstanceRefreshInProgress"]),
     )
 
     start_or_cancel_instance_refresh(autoscaling, module)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
