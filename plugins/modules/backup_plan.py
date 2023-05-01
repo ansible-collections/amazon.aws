a3172c0fdec2dcb13675f5afa86707abec5289b6
Helen Bailey <hebailey@redhat.com>
2023-05-01 15:28:54 -0600

    WIP add integration tests

--- plugins/modules/backup_plan.py
+++ plugins/modules/backup_plan.py
@@ -4,22 +4,17 @@
 # Copyright: Contributors to the Ansible project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import json
-from ansible.module_utils.resource_tags import ensure_tags
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
-from ansible_collections.amazon.aws.plugins.module_utils.core import is_boto3_error_code
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
 
 DOCUMENTATION = r"""
+---
 module: backup_plan
-short_description: create, delete and modify AWS Backup plans
 version_added: 6.0.0
+short_description: create, delete and modify AWS Backup plans
 description:
-  - Manages AWS Backup plans.
-  - For more information see the AWS documentation for backup plans U(https://docs.aws.amazon.com/aws-backup/latest/devguide/about-backup-plans.html).
+  - Manage AWS Backup plans.
+  - For more information see the AWS documentation for Backup plans U(https://docs.aws.amazon.com/aws-backup/latest/devguide/about-backup-plans.html).
+author:
+  - Kristof Imre Szabo (@krisek)
 options:
   backup_plan_name:
     description:
@@ -43,34 +38,30 @@ options:
     default: present
     choices: ['present', 'absent']
     type: str
-notes: []
-author:
-  - Kristof Imre Szabo (@krisek)
 extends_documentation_fragment:
   - amazon.aws.common.modules
   - amazon.aws.region.modules
   - amazon.aws.boto3
   - amazon.aws.tags
 """
-EXAMPLES = """
+
+EXAMPLES = r"""
 - name: create backup plan
-  backup_plan:
+  amazon.aws.backup_plan:
+    state: present
     backup_plan_name: elastic
     rules:
-    - RuleName: every_morning
-      TargetBackupVaultName: elastic
-      ScheduleExpression: "cron(0 5 ? * * *)"
-      StartWindowMinutes: 120
-      CompletionWindowMinutes: 10080
-      Lifecycle:
-        DeleteAfterDays: 7
-      EnableContinuousBackup: true
-  register: elastic_plan
-- name: show results
-  ansible.builtin.debug:
-    var: elastic_plan
+      - RuleName: every_morning
+        TargetBackupVaultName: elastic
+        ScheduleExpression: "cron(0 5 ? * * *)"
+        StartWindowMinutes: 120
+        CompletionWindowMinutes: 10080
+        Lifecycle:
+          DeleteAfterDays: 7
+        EnableContinuousBackup: true
+
 """
-RETURN = """
+RETURN = r"""
 backup_plan_name:
   description: backup plan name
   returned: always
@@ -121,121 +112,211 @@ backup_plan:
       returned: always
       type: str
 """
+
+import json
+from typing import Optional
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
+
 try:
-    import botocore
+    from botocore.exceptions import ClientError, BotoCoreError
 except ImportError:
     pass  # Handled by AnsibleAWSModule
 
 
+def create_backup_plan(module: AnsibleAWSModule, client, params: dict):
+    """
+    Creates a Backup Plan
+
+    module : AnsibleAWSModule object
+    client : boto3 backup client connection object
+    params : The parameters to create a backup plan
+    """
+    params = {k: v for k, v in params.items() if v is not None}
+    try:
+        response = client.create_backup_plan(**params)
+    except (
+        BotoCoreError,
+        ClientError,
+    ) as err:
+        module.fail_json_aws(err, msg="Failed to create Backup Plan")
+    return response
+
+
+def get_plan_details(module, client, backup_plan_name: str):
+    try:
+        plan = paginated_list(client, filter_by=backup_plan_name)[0]
+    except (BotoCoreError, ClientError) as err:
+        module.debug("Unable to get backup plan details {0}".format(err))
+        plan = None
+    except IndexError:
+        plan = None
+    return plan
+
+
+def paginated_list(
+    client, filter_by: Optional[str] = None, **pagination_params
+) -> list[dict]:
+    results = []
+    paginator = client.get_paginator("list_backup_plans")
+    page_iterator = paginator.paginate(**pagination_params)
+    if filter_by:
+        filtered_iterator = page_iterator.search(
+            f"BackupPlansList[?BackupPlanName=='{filter_by}']"
+        )
+        results.extend(list(filtered_iterator))
+    else:
+        results.extend([item["BackupPlansList"] for item in page_iterator])
+    return results
+
+
+def plan_update_needed(client, backup_plan_id: str, backup_plan_data: dict) -> bool:
+    update_needed = False
+
+    # we need to get current rules to manage the plan
+    full_plan = client.get_backup_plan(BackupPlanId=backup_plan_id)
+
+    configured_rules = json.dumps(
+        [
+            {key: val for key, val in rule.items() if key != "RuleId"}
+            for rule in full_plan.get("BackupPlan", {}).get("Rules", [])
+        ],
+        sort_keys=True,
+    )
+    supplied_rules = json.dumps(backup_plan_data["BackupPlan"]["Rules"], sort_keys=True)
+
+    if configured_rules != supplied_rules:
+        # rules to be updated
+        update_needed = True
+
+    configured_advanced_backup_settings = json.dumps(
+        full_plan.get("BackupPlan", {}).get("AdvancedBackupSettings", None),
+        sort_keys=True,
+    )
+    supplied_advanced_backup_settings = json.dumps(
+        backup_plan_data["BackupPlan"]["AdvancedBackupSettings"], sort_keys=True
+    )
+    if configured_advanced_backup_settings != supplied_advanced_backup_settings:
+        # advanced settings to be updated
+        update_needed = True
+    return update_needed
+
+
+def update_backup_plan(
+    module: AnsibleAWSModule, client, backup_plan_id: str, backup_plan_data: dict
+):
+    try:
+        response = client.update_backup_plan(
+            BackupPlanId=backup_plan_id,
+            BackupPlan=backup_plan_data["BackupPlan"],
+        )
+    except (
+        BotoCoreError,
+        ClientError,
+    ) as err:
+        module.fail_json_aws(err, msg="Failed to create Backup Plan")
+    return response
+
+
+def delete_backup_plan(module: AnsibleAWSModule, client, backup_plan_id: str):
+    """
+    Delete a Backup Plan
+
+    module : AnsibleAWSModule object
+    client : boto3 client connection object
+    backup_plan_id : Backup Plan ID
+    """
+    try:
+        client.delete_backup_plan(BackupPlanId=backup_plan_id)
+    except (BotoCoreError, ClientError) as err:
+        module.fail_json_aws(err, msg="Failed to delete the Backup Plan")
+
+
 def main():
     argument_spec = dict(
-        backup_plan_name=dict(type="str", required=True),
-        rules=dict(type="list", required=False),
-        advanced_backup_settings=dict(type="list", required=False),
+        state=dict(default="present", choices=["present", "absent"]),
+        backup_plan_name=dict(required=True, type="str"),
+        rules=dict(type="list"),
+        advanced_backup_settings=dict(default=[], type="list"),
+        creator_request_id=dict(type="str"),
         tags=dict(required=False, type="dict", aliases=["resource_tags"]),
         purge_tags=dict(default=True, type="bool"),
-        state=dict(default="present", choices=["present", "absent"]),
     )
+
     required_if = [
         ("state", "present", ["backup_plan_name", "rules"]),
         ("state", "absent", ["backup_plan_name"]),
     ]
+
     module = AnsibleAWSModule(argument_spec=argument_spec, required_if=required_if)
+
+    # collect parameters
     state = module.params.get("state")
-    backup_plan_name = module.params.get("backup_plan_name")
-    rules = module.params.get("rules")
-    advanced_backup_settings = module.params.get("advanced_backup_settings")
-    try:
-        client = module.client("backup", retry_decorator=AWSRetry.jittered_backoff())
-    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-        module.fail_json_aws(e, msg="Failed to connect to AWS")
-    changed = False
-    # try to check if backup_plan is there
-    paginator = client.get_paginator("list_backup_plans")
-    backup_plans = []
-    for page in paginator.paginate():
-        backup_plans.extend(page["BackupPlansList"])
-    exist = False
-    response = {}
-    for backup_plan in backup_plans:
-        if backup_plan["BackupPlanName"] == backup_plan_name:
-            exist = True
-            response = backup_plan
+    backup_plan_name = module.params["backup_plan_name"]
+    purge_tags = module.params["purge_tags"]
+    client = module.client("backup")
+    results = {"changed": False, "exists": False}
+
+    # TODO: support check mode?
+
+    current_plan = get_plan_details(module, client, backup_plan_name)
+
     if state == "present":
-        if exist:  # state is present and backup plan exists => got to update
-            # we need to get rules to manage the plan
-            full_plan = client.get_backup_plan(BackupPlanId=response["BackupPlanId"])
+        new_plan_data = {
+            "BackupPlan": {
+                "BackupPlanName": backup_plan_name,
+                "Rules": module.params["rules"],
+                "AdvancedBackupSettings": module.params.get("advanced_backup_settings"),
+            },
+            "BackupPlanTags": module.params.get("tags"),
+            "CreatorRequestId": module.params.get("creator_request_id"),
+        }
 
-            update_needed = False
-            configured_rules = [
-                {key: val for key, val in rule.items() if key != "RuleId"}
-                for rule in full_plan.get("BackupPlan", {}).get("Rules", [])
-            ]
+        if current_plan is None:  # Plan does not exist, create it
+            create_backup_plan(module, client, new_plan_data)
+            # TODO: add tags
+            # ensure_tags(
+            #     client,
+            #     module,
+            #     response["BackupPlanArn"],
+            #     purge_tags=module.params.get("purge_tags"),
+            #     tags=module.params.get("tags"),
+            #     resource_type="BackupPlan",
+            # )
+            results["exists"] = True
 
-            if json.dumps(configured_rules, sort_keys=True) != json.dumps(rules, sort_keys=True):
-                # rules to be updated
-                update_needed = True
-            advanced_backup_settings_from_aws = json.dumps(
-                full_plan.get("BackupPlan", {}).get("AdvancedBackupSettings", None), sort_keys=True
-            )
-            advanced_backup_settings_from_param = json.dumps(advanced_backup_settings, sort_keys=True)
-            if advanced_backup_settings_from_aws != advanced_backup_settings_from_param:
-                # advanced settings to be updated
-                update_needed = True
-            if update_needed:
-                backup_plan_data = {"BackupPlanName": backup_plan_name}
-                if rules:
-                    backup_plan_data["Rules"] = rules
-                if advanced_backup_settings:
-                    backup_plan_data["AdvancedBackupSettings"] = advanced_backup_settings
-                update_response = client.update_backup_plan(
-                    aws_retry=True, BackupPlanId=response.get("BackupPlanId"), BackupPlan=backup_plan_data
-                )
-                changed = True
-            if ensure_tags(
-                client,
-                module,
-                response["BackupPlanArn"],
-                purge_tags=module.params.get("purge_tags"),
-                tags=module.params.get("tags"),
-                resource_type="BackupPlan",
-            ):
-                changed = True
-        else:  # state is present but backup plan doesnt exist => got to create
-            backup_plan_data = {"BackupPlanName": backup_plan_name}
-            if rules:
-                backup_plan_data["Rules"] = rules
-            if advanced_backup_settings:
-                backup_plan_data["AdvancedBackupSettings"] = advanced_backup_settings
-            response = client.create_backup_plan(aws_retry=True, BackupPlan=backup_plan_data)
-            response["BackupPlanName"] = backup_plan_name
-            ensure_tags(
-                client,
-                module,
-                response["BackupPlanArn"],
-                purge_tags=module.params.get("purge_tags"),
-                tags=module.params.get("tags"),
-                resource_type="BackupPlan",
-            )
-            changed = True
+        else:  # Plan exists, update if needed
+            results["exists"] = True
+            current_plan_id = current_plan["BackupPlanId"]
+            if plan_update_needed(client, current_plan_id, new_plan_data):
+                update_backup_plan(module, client, current_plan_id, new_plan_data)
+            if purge_tags:
+                pass
+                # TODO: Update plan tags
+                # ensure_tags(
+                #     client,
+                #     module,
+                #     response["BackupPlanArn"],
+                #     purge_tags=module.params.get("purge_tags"),
+                #     tags=module.params.get("tags"),
+                #     resource_type="BackupPlan",
+                # )
+        results["changed"] = True
+        new_plan = get_plan_details(module, client, backup_plan_name)
+        results["backup_plan"] = camel_dict_to_snake_dict(new_plan)
+
     elif state == "absent":
-        if exist:
-            try:
-                response_delete = client.delete_backup_plan(aws_retry=True, BackupPlanId=response.get("BackupPlanId"))
-                if response_delete["ResponseMetadata"]["HTTPStatusCode"] == 200:
-                    changed = True
-            except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-                module.fail_json_aws(e, msg="Failed to delete plan")
+        if current_plan is None:  # Plan does not exist, can't delete it
+            module.fail_json(
+                msg=f"Backup plan {backup_plan_name} not found.",
+                # TODO: existing_backup_plans="list existing backup plan names",
+            )
+        else:  # Plan exists, delete it
+            delete_backup_plan(module, client, current_plan["BackupPlanId"])
+            results["changed"] = True
+            results["exists"] = False
 
-    formatted_results = camel_dict_to_snake_dict(response)
-    # Turn the resource tags from boto3 into an ansible friendly tag dictionary
-    formatted_results["tags"] = boto3_tag_list_to_ansible_dict(formatted_results.get("tags", []))
-    module.exit_json(
-        changed=changed,
-        backup_plan=formatted_results,
-        backup_plan_name=response.get("BackupPlanName"),
-        backup_plan_id=response.get("BackupPlanId"),
-    )
+    module.exit_json(**results)
 
 
 if __name__ == "__main__":
