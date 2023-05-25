880b810d6c2cbb024dcbf202dd288e6eba8ca911
krisek <kris@lxsystems.de>
2023-05-25 10:41:36 +0200

    Update plugins/modules/ec2_vpc_endpoint_service.py
    Co-authored-by: Mark Chappell <mchappel@redhat.com>

--- plugins/modules/ec2_vpc_endpoint_service.py
+++ plugins/modules/ec2_vpc_endpoint_service.py
@@ -168,16 +168,14 @@ import json
 from datetime import datetime
 from typing import Optional
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.core import is_boto3_error_code
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
 
-from ansible.module_utils.resource_tags import ensure_tags
-from ansible_collections.amazon.aws.plugins.module_utils.tagging import ansible_dict_to_boto3_tag_list  # pylint: disable=unused-import
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ensure_ec2_tags
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import ansible_dict_to_boto3_tag_list
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 
 try:
     from botocore.exceptions import ClientError, BotoCoreError
