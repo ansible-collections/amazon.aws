10d80fff2eeb73fc0efdad9d3c1ae5e90b738229
Alina Buzachis <abuzachis@redhat.com>
2023-05-30 15:23:31 +0200

    ec2_metadata_facts - Handle decompression when EC2 instance user-data is compressed (#1575)
    ec2_metadata_facts - Handle decompression when EC2 instance user-data is compressed
    
    SUMMARY
    
    Handle decompression when user-data is compressed. The fetch_url method from ansible.module_utils.urls does not decompress the user-data because the header is missing (The API does not set 'Content-Encoding' = 'gzip').
    
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    
    ec2_metadata_facts
    ADDITIONAL INFORMATION
    
    Reviewed-by: Mike Graves <mgraves@redhat.com>
    Reviewed-by: Mark Chappell
    Reviewed-by: Alina Buzachis
    (cherry picked from commit f1fe603084fdf05b70a856ea137a608eb663ccb9)

--- tests/unit/plugins/modules/test_ec2_metadata_facts.py
+++ tests/unit/plugins/modules/test_ec2_metadata_facts.py
@@ -1,6 +1,7 @@
 # This file is part of Ansible
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+import gzip
 import io
 import pytest
 from unittest.mock import MagicMock
@@ -59,3 +60,41 @@ def test_fetch_recusive(m_fetch_url, ec2_instance):
     ]
     ec2_instance.fetch("http://169.254.169.254/latest/meta-data/")
     assert ec2_instance._data == {"http://169.254.169.254/latest/meta-data/whatever/my-key": "my-value"}
+
+
+@patch(module_name + ".fetch_url")
+def test__fetch_user_data_compressed(m_fetch_url, ec2_instance):
+    user_data = b"""Content-Type: multipart/mixed; boundary="MIMEBOUNDARY"
+MIME-Version: 1.0
+
+--MIMEBOUNDARY
+Content-Transfer-Encoding: 7bit
+Content-Type: text/cloud-config
+Mime-Version: 1.0
+
+packages: ['httpie']
+
+--MIMEBOUNDARY--
+"""
+
+    m_fetch_url.return_value = (io.BytesIO(gzip.compress(user_data)), {"status": 200})
+    assert ec2_instance._fetch("http://169.254.169.254/latest/user-data") == user_data.decode("utf-8")
+
+
+@patch(module_name + ".fetch_url")
+def test__fetch_user_data_plain(m_fetch_url, ec2_instance):
+    user_data = b"""Content-Type: multipart/mixed; boundary="MIMEBOUNDARY"
+MIME-Version: 1.0
+
+--MIMEBOUNDARY
+Content-Transfer-Encoding: 7bit
+Content-Type: text/cloud-config
+Mime-Version: 1.0
+
+packages: ['httpie']
+
+--MIMEBOUNDARY--
+"""
+
+    m_fetch_url.return_value = (io.BytesIO(user_data), {"status": 200})
+    assert ec2_instance._fetch("http://169.254.169.254/latest/user-data") == user_data.decode("utf-8")
