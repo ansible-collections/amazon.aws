827643ba666ecedb0d8b1b80f5ee144e57109c39
Alina Buzachis <abuzachis@redhat.com>
2023-06-06 21:10:54 +0200

    Fix sanity (#1599)
    austoscaling_group* - Fix sanity errors
    
    SUMMARY
    
    Fix sanity errors -  No need to backport to stable-6 or stable-5 because the back ported PRs include this fix already (#1598).
    
    ISSUE TYPE
    
    
    Bugfix Pull Request
    Docs Pull Request
    Feature Pull Request
    New Module Pull Request
    
    COMPONENT NAME
    
    ADDITIONAL INFORMATION
    
    Reviewed-by: Mark Chappell

--- plugins/modules/autoscaling_group.py
+++ plugins/modules/autoscaling_group.py
@@ -907,7 +907,7 @@ def get_properties(autoscaling_group):
         tg_chunks = [
             properties["target_group_arns"][i: i + tg_chunk_size]
             for i in range(0, len(properties["target_group_arns"]), tg_chunk_size)
-        ]
+        ]  # fmt: skip
         for chunk in tg_chunks:
             tg_result = tg_paginator.paginate(TargetGroupArns=chunk).build_full_result()
             properties["target_group_names"].extend([tg["TargetGroupName"] for tg in tg_result["TargetGroups"]])
