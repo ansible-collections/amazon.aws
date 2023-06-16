858a711e0f861266ff6c84335e050f9191d42b65
Mark Chappell <mchappel@redhat.com>
2023-06-16 15:42:21 +0200

    sort imports

--- plugins/modules/iam_instance_profile.py
+++ plugins/modules/iam_instance_profile.py
@@ -107,20 +107,17 @@ iam_instance_profile:
 
 from copy import deepcopy
 
-from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
-
+from ansible_collections.amazon.aws.plugins.module_utils.iam import add_role_to_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import AnsibleIAMError
 from ansible_collections.amazon.aws.plugins.module_utils.iam import create_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import delete_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import list_iam_instance_profiles
 from ansible_collections.amazon.aws.plugins.module_utils.iam import normalize_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import remove_role_from_iam_instance_profile
-from ansible_collections.amazon.aws.plugins.module_utils.iam import add_role_to_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import tag_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.iam import untag_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import compare_aws_tags
 
 
--- plugins/modules/iam_instance_profile_info.py
+++ plugins/modules/iam_instance_profile_info.py
@@ -88,12 +88,11 @@ iam_instance_profiles:
       sample: []
 """
 
-from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
-
 from ansible_collections.amazon.aws.plugins.module_utils.iam import AnsibleIAMError
 from ansible_collections.amazon.aws.plugins.module_utils.iam import list_iam_instance_profiles
 from ansible_collections.amazon.aws.plugins.module_utils.iam import normalize_iam_instance_profile
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 
 
 def describe_iam_instance_profiles(module, client):
