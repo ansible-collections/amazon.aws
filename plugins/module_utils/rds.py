d66e292de9707baeac4b25536509e6bd365fdde0
Taeho Park <tpark@redhat.com>
2023-07-25 17:38:08 -0400

    included unit test for stop and start rds

--- plugins/module_utils/rds.py
+++ plugins/module_utils/rds.py
@@ -104,6 +104,8 @@ def get_rds_method_attribute(method_name, module):
         resource = "cluster"
         if method_name == "delete_db_cluster":
             waiter = "cluster_deleted"
+        elif method_name == "stop_db_cluster":
+            waiter = "cluster_stopped"
         else:
             waiter = "cluster_available"
         # Handle retry codes
--- tests/unit/module_utils/test_rds.py
+++ tests/unit/module_utils/test_rds.py
@@ -183,6 +183,36 @@ def test__wait_for_cluster_snapshot_status_failed(input, expected):
             ),
         ),
         (
+            "start_db_cluster",
+            {
+                "new_db_cluster_identifier": "test",
+            },
+            *expected(
+                rds.Boto3ClientMethod(
+                    name="start_db_cluster",
+                    waiter="cluster_available",
+                    operation_description="start DB cluster",
+                    resource="cluster",
+                    retry_codes=["InvalidDBClusterState"],
+                )
+            ),
+        ),
+        (
+            "stop_db_cluster",
+            {
+                "new_db_cluster_identifier": "test",
+            },
+            *expected(
+                rds.Boto3ClientMethod(
+                    name="stop_db_cluster",
+                    waiter="cluster_stopped",
+                    operation_description="stop DB cluster",
+                    resource="cluster",
+                    retry_codes=["InvalidDBClusterState"],
+                )
+            ),
+        ),
+        (
             "fake_method",
             {"wait": False},
             *expected(
