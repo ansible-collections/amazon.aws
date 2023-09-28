76035e823e686981323c888073e5e7b469f44b40
Alina Buzachis <abuzachis@redhat.com>
2023-09-28 15:19:25 +0200

    Use the amazon.aws path for importing AnsibleAWSModule
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/iam_mfa_device_info.py
+++ plugins/modules/iam_mfa_device_info.py
@@ -64,7 +64,7 @@ except ImportError:
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
-from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def list_mfa_devices(connection, module):
