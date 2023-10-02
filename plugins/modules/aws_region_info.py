0e70ea30c8443b60749a0d89c4c315c1c2a3dfb4
Alina Buzachis <abuzachis@redhat.com>
2023-10-02 17:25:23 +0200

    Apply isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/aws_region_info.py
+++ plugins/modules/aws_region_info.py
@@ -63,10 +63,9 @@ except ImportError:
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
-
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def main():
