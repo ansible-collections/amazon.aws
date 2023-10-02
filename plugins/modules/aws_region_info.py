ee68f019ac6381789d7be68e51ebe50b02148da1
Alina Buzachis <abuzachis@redhat.com>
2023-10-02 17:19:32 +0200

    Update AnsibleAWSModule import path

--- plugins/modules/aws_region_info.py
+++ plugins/modules/aws_region_info.py
@@ -65,7 +65,7 @@ from ansible.module_utils.common.dict_transformations import camel_dict_to_snake
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 
-from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def main():
