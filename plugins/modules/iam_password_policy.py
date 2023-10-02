3855172b56cea9300dc88ee5c75c8be17b136c0a
Alina Buzachis <abuzachis@redhat.com>
2023-10-02 16:44:54 +0200

    Apply isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/iam_password_policy.py
+++ plugins/modules/iam_password_policy.py
@@ -103,7 +103,6 @@ except ImportError:
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_code
-
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
