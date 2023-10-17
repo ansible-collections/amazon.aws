0a6ecf74bbf35f618c260dff02ddd002c282954e
Mark Chappell <mchappel@redhat.com>
2023-10-17 21:27:02 +0200

    Apply isort and flynt (#1962)
    SUMMARY
    
    Apply isort - see also ansible-collections/amazon.aws#1771
    Apply flynt - see also ansible-collections/amazon.aws#1802
    
    ISSUE TYPE
    
        Feature Pull Request
    
    COMPONENT NAME
    
    ADDITIONAL INFORMATION
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/c0117b5466958bd0f8b5fc620306adde1422a62d

--- plugins/modules/ec2_vpc_vpn_info.py
+++ plugins/modules/ec2_vpc_vpn_info.py
@@ -168,8 +168,8 @@ except ImportError:
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
-from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
+from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 
 from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 
