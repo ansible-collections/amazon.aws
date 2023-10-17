ac2a893cf671b4c00a1b69c2c54fffaf43a327ca
Mark Chappell <mchappel@redhat.com>
2023-10-17 21:27:02 +0200

    Apply isort and flynt (#1962)
    SUMMARY
    
    Apply isort - see also ansible-collections/amazon.aws#1771
    Apply flynt - see also ansible-collections/amazon.aws#1802
    
    ISSUE TYPE
    
        Feature Pull Request
    
    COMPONENT NAME
    
    ADDITIONAL INFORMATION
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/c0117b5466958bd0f8b5fc620306adde1422a62d

--- plugins/modules/ec2_vpc_vgw.py
+++ plugins/modules/ec2_vpc_vgw.py
@@ -143,8 +143,8 @@ except ImportError:
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_code
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ensure_ec2_tags
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
-from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_specifications
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_specifications
 from ansible_collections.amazon.aws.plugins.module_utils.waiters import get_waiter
 
 from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
