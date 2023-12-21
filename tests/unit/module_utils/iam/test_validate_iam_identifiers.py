1e32e0cecb72228f6c1b051075a7819c5d07feb3
Mark Chappell <mchappel@redhat.com>
2023-12-21 17:59:35 +0100

    black

--- tests/unit/module_utils/iam/test_validate_iam_identifiers.py
+++ tests/unit/module_utils/iam/test_validate_iam_identifiers.py
@@ -13,10 +13,20 @@ validate_test_data = [
     (dict(path="/"), None, None, None),
     (dict(name="Example"), None, None, None),
     # Path tests
-    (dict(path="/12345abcd"), "path must begin and end with /", "path must begin and end with /", "path must begin and end with /",),
+    (
+        dict(path="/12345abcd"),
+        "path must begin and end with /",
+        "path must begin and end with /",
+        "path must begin and end with /",
+    ),
     (dict(path="/12345abcd/"), None, None, None),
     (dict(path=f"/{'12345abcd0'*51}/"), None, None, None),  # Max length 512 chars
-    (dict(path=f"/{'12345abcd/'*51}a/"), "path may not exceed 512", "path may not exceed 512", "path may not exceed 512",),
+    (
+        dict(path=f"/{'12345abcd/'*51}a/"),
+        "path may not exceed 512",
+        "path may not exceed 512",
+        "path may not exceed 512",
+    ),
     (dict(path="/12345+=,.@_-abcd/"), None, None, None),  # limited allowed special characters
     (dict(path="/12345&abcd/"), "path must match pattern", "path must match pattern", "path must match pattern"),
     (dict(path="/12345:abcd/"), "path must match pattern", "path must match pattern", "path must match pattern"),
@@ -25,7 +35,12 @@ validate_test_data = [
     (dict(name=f"{'12345abcd0'*6}1234"), None, None, None),  # Max length
     (dict(name=f"{'12345abcd0'*6}12345"), "name may not exceed 64", "name may not exceed 64", None),
     (dict(name=f"{'12345abcd0'*12}12345678"), "name may not exceed 64", "name may not exceed 64", None),
-    (dict(name=f"{'12345abcd0'*12}123456789"), "name may not exceed 64", "name may not exceed 64", "name may not exceed 128",),
+    (
+        dict(name=f"{'12345abcd0'*12}123456789"),
+        "name may not exceed 64",
+        "name may not exceed 64",
+        "name may not exceed 128",
+    ),
     (dict(name="12345+=,.@_-abcd"), None, None, None),  # limited allowed special characters
     (dict(name="12345&abcd"), "name must match pattern", "name must match pattern", "name must match pattern"),
     (dict(name="12345:abcd"), "name must match pattern", "name must match pattern", "name must match pattern"),
