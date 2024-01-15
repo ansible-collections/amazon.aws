31ca495f5533140ca397b6a17417b1456c4655bc
Mandar Kulkarni <mandar242@gmail.com>
2024-01-15 15:30:26 -0800

    restructure code to remove redundancy, update changlog

--- plugins/module_utils/elbv2.py
+++ plugins/module_utils/elbv2.py
@@ -967,22 +967,18 @@ class ELBListener:
 
             # handle multiple certs by adding only 1 cert during listener creation and make calls to add_listener_certificates to add other certs
             listener_certificates = self.listener.get("Certificates", [])
-            if len(listener_certificates) <= 1:
-                create_listener_result = AWSRetry.jittered_backoff()(self.connection.create_listener)(
-                    LoadBalancerArn=self.elb_arn, **self.listener
-                )
-            else:
+            if len(listener_certificates) > 0:
                 first_certificate, other_certs = listener_certificates[0], listener_certificates[1:]
                 self.listener["Certificates"] = [first_certificate]
-                # create listener
-                create_listener_result = AWSRetry.jittered_backoff()(self.connection.create_listener)(
-                    LoadBalancerArn=self.elb_arn, **self.listener
+            # create listener
+            create_listener_result = AWSRetry.jittered_backoff()(self.connection.create_listener)(
+                LoadBalancerArn=self.elb_arn, **self.listener
+            )
+            # only one cert can be specified per call to add_listener_certificates
+            for cert in other_certs:
+                AWSRetry.jittered_backoff()(self.connection.add_listener_certificates)(
+                    ListenerArn=create_listener_result["Listeners"][0]["ListenerArn"], Certificates=[cert]
                 )
-                # only one cert can be specified per call to add_listener_certificates
-                for cert in other_certs:
-                    AWSRetry.jittered_backoff()(self.connection.add_listener_certificates)(
-                        ListenerArn=create_listener_result["Listeners"][0]["ListenerArn"], Certificates=[cert]
-                    )
 
         except (BotoCoreError, ClientError) as e:
             self.module.fail_json_aws(e)
