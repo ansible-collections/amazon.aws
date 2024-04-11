0beb954cb6af5ba761c79b0960d43cd246fcc229
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2024-04-11 12:26:21 +0200

    s3_object - fix copy idempotency issue with multipart upload object (#2024)
    s3_object - fix copy idempotency issue with multipart upload object
    
    SUMMARY
    
    Fixes #2016
    To ensure the idempotency when copying objects created with multipart upload, the idempotency of the source object will be computed locally using object content as it differs from what is stored on AWS object header.
    
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    
    s3_object
    ADDITIONAL INFORMATION
    
    Reviewed-by: Colin Nolan
    Reviewed-by: Bikouo Aubin
    Reviewed-by: Helen Bailey <hebailey@redhat.com>
    (cherry picked from commit 37804cc765a8e373792c45f138ed60f0c8237d4d)

--- plugins/module_utils/s3.py
+++ plugins/module_utils/s3.py
@@ -58,7 +58,7 @@ def calculate_etag(module, filename, etag, s3, bucket, obj, version=None):
     if not HAS_MD5:
         return None
 
-    if "-" in etag:
+    if etag is not None and "-" in etag:
         # Multi-part ETag; a hash of the hashes of each part.
         parts = int(etag[1:-1].split("-")[1])
         try:
@@ -73,7 +73,7 @@ def calculate_etag_content(module, content, etag, s3, bucket, obj, version=None)
     if not HAS_MD5:
         return None
 
-    if "-" in etag:
+    if etag is not None and "-" in etag:
         # Multi-part ETag; a hash of the hashes of each part.
         parts = int(etag[1:-1].split("-")[1])
         try:
