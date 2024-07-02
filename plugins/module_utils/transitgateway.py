e2553a93528aec3b18524cca7cdad41099baad3c
Carlos Schimidt <40364204+cschimid@users.noreply.github.com>
2024-07-02 11:33:10 -0300

    Add support to enable multicast on transit gateway (#2063)
    SUMMARY
    
    Need to enable multicast while creating transit gateway
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    transit_gateway
    ADDITIONAL INFORMATION
    
    Reviewed-by: Alina Buzachis
    Reviewed-by: Bikouo Aubin
    Reviewed-by: Carlos Schimidt
    Reviewed-by: Mark Chappell
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/4752c05c56076e1b904d7ee4dfd6c350601da0af

--- plugins/module_utils/transitgateway.py
+++ plugins/module_utils/transitgateway.py
@@ -192,6 +192,9 @@ class TransitGatewayVpcAttachmentManager(TGWAttachmentBoto3Mixin, BaseTGWManager
     def set_dns_support(self, value):
         return self._set_option("DnsSupport", value)
 
+    def set_multicast_support(self, value):
+        return self._set_option("MulticastSupport", value)
+
     def set_ipv6_support(self, value):
         return self._set_option("Ipv6Support", value)
 
