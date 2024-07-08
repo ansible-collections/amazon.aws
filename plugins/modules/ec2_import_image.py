28bfec61a7399bd4b405fc39fb780b9f317fa18e
abikouo <abikouo@redhat.com>
2024-07-08 12:33:26 +0200

    fix sanity issue

--- plugins/modules/ec2_import_image.py
+++ plugins/modules/ec2_import_image.py
@@ -303,11 +303,6 @@ import_image:
       type: dict
 """
 
-try:
-    import botocore
-except ImportError:
-    pass  # Handled by AnsibleAWSModule
-
 from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
