bc6714c600b2113a1116b61f683a083f398bc77a
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2024-07-15 19:25:15 +0200

    Refactor ec2_eip* modules (#2165)
    SUMMARY
    Refactor ec2_eip,ec2_eip_info modules
    ISSUE TYPE
    
    
    Feature Pull Request
    
    COMPONENT NAME
    
    ec2_eip ec2_eip_info
    
    Reviewed-by: Helen Bailey <hebailey@redhat.com>
    Reviewed-by: Bikouo Aubin
    Reviewed-by: Alina Buzachis

---
+++ tests/unit/plugins/modules/ec2_eip/test_generate_tag_dict.py
@@ -0,0 +1,32 @@
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from unittest.mock import ANY
+from unittest.mock import MagicMock
+
+import pytest
+
+from ansible_collections.amazon.aws.plugins.modules import ec2_eip
+
+
+@pytest.fixture
+def ansible_module():
+    module = MagicMock()
+    module.params = {}
+    module.fail_json.side_effect = SystemExit(1)
+    return module
+
+
+@pytest.mark.parametrize(
+    "tag_name,tag_value,expected",
+    [
+        (None, ANY, None),
+        ("tag:SomeKey", None, {"tag-key": "SomeKey"}),
+        ("SomeKey", None, {"tag-key": "SomeKey"}),
+        ("tag:AnotherKey", "SomeValue", {"tag:AnotherKey": "SomeValue"}),
+        ("AnotherKey", "AnotherValue", {"tag:AnotherKey": "AnotherValue"}),
+    ],
+)
+def test_generate_tag_dict(ansible_module, tag_name, tag_value, expected):
+    ansible_module.params.update({"tag_name": tag_name, "tag_value": tag_value})
+    assert expected == ec2_eip.generate_tag_dict(ansible_module)
