5a3e972fa9fc033e419a178ddfc22c335830ab83
Alina Buzachis <abuzachis@redhat.com>
2024-09-05 18:32:32 +0200

    Rename module_utils/elb_utils.py to module_utils/elbv2_utils.py since it include util esed by the elbv2 client
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/autoscaling_group.py
+++ plugins/modules/autoscaling_group.py
@@ -666,7 +666,7 @@ from ansible.module_utils.common.dict_transformations import camel_dict_to_snake
 from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_code
-from ansible_collections.amazon.aws.plugins.module_utils.elb_utils import describe_target_groups
+from ansible_collections.amazon.aws.plugins.module_utils.elbv2_utils import describe_target_groups
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import scrub_none_parameters
