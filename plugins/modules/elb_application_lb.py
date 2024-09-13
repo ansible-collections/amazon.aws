aa6934b2b571cc4fa0ba4d129c11dd990b432a3d
Alina Buzachis <abuzachis@redhat.com>
2024-09-13 14:16:30 +0200

    Apply isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/elb_application_lb.py
+++ plugins/modules/elb_application_lb.py
@@ -765,13 +765,13 @@ except ImportError:
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
-from ansible_collections.amazon.aws.plugins.module_utils.elbv2_utils import AnsibleELBv2Error
-from ansible_collections.amazon.aws.plugins.module_utils.elbv2_utils import get_elb_listener_rules
 from ansible_collections.amazon.aws.plugins.module_utils.elbv2 import ApplicationLoadBalancer
 from ansible_collections.amazon.aws.plugins.module_utils.elbv2 import ELBListener
 from ansible_collections.amazon.aws.plugins.module_utils.elbv2 import ELBListenerRule
 from ansible_collections.amazon.aws.plugins.module_utils.elbv2 import ELBListenerRules
 from ansible_collections.amazon.aws.plugins.module_utils.elbv2 import ELBListeners
+from ansible_collections.amazon.aws.plugins.module_utils.elbv2_utils import AnsibleELBv2Error
+from ansible_collections.amazon.aws.plugins.module_utils.elbv2_utils import get_elb_listener_rules
 from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
