af97b421d8e483a2a0bd62bbabf295186f080c8a
Mandar Kulkarni <mandar242@gmail.com>
2024-09-27 10:16:09 -0700

    linter fix

--- tests/unit/plugins/modules/ec2_eip/test_reverse_dns_record_updates.py
+++ tests/unit/plugins/modules/ec2_eip/test_reverse_dns_record_updates.py
@@ -2,6 +2,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from unittest.mock import MagicMock
+
 import pytest
 
 from ansible_collections.amazon.aws.plugins.modules import ec2_eip
