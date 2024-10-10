5694d172571691cddf296a2836f4d6598f757bd4
Andrea Tartaglia <me@andreatartaglia.com>
2024-10-10 16:51:19 +0200

    check if Value is returned by the API before checking value

--- plugins/modules/ec2_ami_info.py
+++ plugins/modules/ec2_ami_info.py
@@ -283,8 +283,10 @@ def list_ec2_images(ec2_client, module, request_args):
                 image["launch_permissions"] = [camel_dict_to_snake_dict(perm) for perm in launch_permissions]
                 imdsv2_enabled = describe_image_attribute(
                     ec2_client, attribute="imdsSupport", image_id=image["image_id"]
-                ).get("ImdsSupport", "")
-                image["imdsv2_enabled"] = True if imdsv2_enabled["Value"] == "v2.0" else False
+                ).get("ImdsSupport", {})
+                image["imdsv2_enabled"] = (
+                    True if "Value" in imdsv2_enabled and imdsv2_enabled["Value"] == "v2.0" else False
+                )
         except is_ansible_aws_error_code("AuthFailure"):
             # describing launch permissions of images owned by others is not permitted, but shouldn't cause failures
             pass
