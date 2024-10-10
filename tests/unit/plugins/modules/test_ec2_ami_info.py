6f5eed4e2aa1b0c774e76a2b9dddb11aaade8570
Andrea Tartaglia <me@andreatartaglia.com>
2024-10-10 16:51:55 +0200

    update unit test to add imds support tests

--- tests/unit/plugins/modules/test_ec2_ami_info.py
+++ tests/unit/plugins/modules/test_ec2_ami_info.py
@@ -140,6 +140,7 @@ def test_list_ec2_images(m_get_images, m_describe_image_attribute, ec2_client):
     m_describe_image_attribute.return_value = {
         "ImageId": "ami-1234567890",
         "LaunchPermissions": [{"UserId": "1234567890"}, {"UserId": "0987654321"}],
+        "ImdsSupport": {"Value": "v2.0"},
     }
 
     images = m_get_images.return_value
@@ -166,13 +167,14 @@ def test_list_ec2_images(m_get_images, m_describe_image_attribute, ec2_client):
         [call(ec2_client, attribute="launchPermission", image_id=images[1]["image_id"])],
     )
     m_describe_image_attribute.assert_has_calls(
-        [call(ec2_client, attribute="ImdsSupport", image_id=images[0]["image_id"])],
-        [call(ec2_client, attribute="ImdsSupport", image_id=images[1]["image_id"])],
+        [call(ec2_client, attribute="imdsSupport", image_id=images[0]["image_id"])],
+        [call(ec2_client, attribute="imdsSupport", image_id=images[1]["image_id"])],
     )
 
     assert len(list_ec2_images_result) == 2
     assert list_ec2_images_result[0]["image_id"] == "ami-1234567890"
     assert list_ec2_images_result[1]["image_id"] == "ami-1523498760"
+    assert list_ec2_images_result[0]["imdsv2_enabled"]
 
 
 @patch(module_name + ".AnsibleAWSModule")
