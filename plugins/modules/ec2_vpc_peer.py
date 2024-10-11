4c9329e6bdfff1ce3195c872ceb49116179732d8
Alina Buzachis <abuzachis@redhat.com>
2024-10-11 16:52:16 +0200

    Update AnsibleAWSModule import path

--- plugins/modules/ec2_vpc_peer.py
+++ plugins/modules/ec2_vpc_peer.py
@@ -366,7 +366,7 @@ from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 
-from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def wait_for_state(client, module, state, pcx_id):
