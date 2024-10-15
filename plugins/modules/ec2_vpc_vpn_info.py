d46af7e0ed94d6d5c1c2d482e9358dda49c98a67
Alina Buzachis <abuzachis@redhat.com>
2024-10-15 17:35:31 +0200

    Refactor ec2_vpc_vpn* modules (#2160)
    SUMMARY
    
    Refactor ec2_vpc_vpn* modules
    
    ISSUE TYPE
    
    Bugfix Pull Request
    Docs Pull Request
    Feature Pull Request
    New Module Pull Request
    
    COMPONENT NAME
    
    ec2_vpc_vpn
    ec2_vpc_vpn_info
    ADDITIONAL INFORMATION
    
    Reviewed-by: GomathiselviS <gomathiselvi@gmail.com>
    Reviewed-by: Bikouo Aubin
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/d2403ab9630a58ea37b8f7d60c0aab908ec1c83d

--- plugins/modules/ec2_vpc_vpn_info.py
+++ plugins/modules/ec2_vpc_vpn_info.py
@@ -8,9 +8,9 @@ DOCUMENTATION = r"""
 ---
 module: ec2_vpc_vpn_info
 version_added: 1.0.0
-short_description: Gather information about VPN Connections in AWS.
+short_description: Gather information about EC2 VPN Connections in AWS
 description:
-  - Gather information about VPN Connections in AWS.
+  - Gather information about EC2 VPN Connections in AWS.
 author:
   - Madhura Naniwadekar (@Madhura-CSI)
 options:
@@ -23,7 +23,7 @@ options:
     default: {}
   vpn_connection_ids:
     description:
-      - Get details of a specific VPN connections using vpn connection ID/IDs. This value should be provided as a list.
+      - Get details of specific EC2 VPN Connection(s) using vpn connection ID/IDs. This value should be provided as a list.
     required: false
     type: list
     elements: str
@@ -36,33 +36,34 @@ extends_documentation_fragment:
 
 EXAMPLES = r"""
 # # Note: These examples do not set authentication details, see the AWS Guide for details.
-- name: Gather information about all vpn connections
+- name: Gather information about all  EC2 VPN Connections
   community.aws.ec2_vpc_vpn_info:
 
-- name: Gather information about a filtered list of vpn connections, based on tags
+- name: Gather information about a filtered list of  EC2 VPN Connections, based on tags
   community.aws.ec2_vpc_vpn_info:
     filters:
-      "tag:Name": test-connection
+      "tag:Name": "test-connection"
   register: vpn_conn_info
 
-- name: Gather information about vpn connections by specifying connection IDs.
+- name: Gather information about EC2 VPN Connections by specifying connection IDs
   community.aws.ec2_vpc_vpn_info:
     filters:
-      vpn-gateway-id: vgw-cbe66beb
+      "vpn-gateway-id": "vgw-cbe66beb"
   register: vpn_conn_info
 """
 
 RETURN = r"""
 vpn_connections:
-    description: List of one or more VPN Connections.
+    description: List of one or more EC2 VPN Connections.
+    type: list
+    elements: dict
     returned: always
-    type: complex
     contains:
       category:
         description: The category of the VPN connection.
         returned: always
         type: str
-        sample: VPN
+        sample: "VPN"
       customer_gatway_configuration:
         description: The configuration information for the VPN connection's customer gateway (in the native XML format).
         returned: always
@@ -71,50 +72,112 @@ vpn_connections:
         description: The ID of the customer gateway at your end of the VPN connection.
         returned: always
         type: str
-        sample: cgw-17a53c37
+        sample: "cgw-17a53c37"
+      gateway_association_state:
+        description: The current state of the gateway association.
+        type: str
+        sample: "associated"
       options:
         description: The VPN connection options.
-        returned: always
-        type: dict
-        sample: {
-                    "static_routes_only": false
-                }
+        type: list
+        elements: dict
+        contains:
+          static_routes_only:
+            description: If the VPN connection only allows static routes.
+            type: bool
+            sample: true
+          enable_acceleration:
+            description: Indicates whether acceleration is enabled for the VPN connection.
+            type: bool
+            sample: false
+          local_ipv4_network_cidr:
+            description: The IPv4 CIDR on the customer gateway (on-premises) side of the VPN connection.
+            type: str
+            sample: "0.0.0.0/0"
+          outside_ip_address_type:
+            description: The external IP address of the VPN tunnel.
+            type: str
+            sample: "PublicIpv4"
+          remote_ipv4_network_cidr:
+            description: The IPv4 CIDR on the Amazon Web Services side of the VPN connection.
+            type: str
+            sample: "0.0.0.0/0"
+          tunnel_inside_ip_version:
+            description: Indicates whether the VPN tunnels process IPv4 or IPv6 traffic.
+            type: str
+            sample: "ipv4"
+          tunnel_options:
+            description: Indicates the VPN tunnel options.
+            type: list
+            elements: dict
+            sample: [
+                  {
+                      "log_options": {
+                          "cloud_watch_log_options": {
+                              "log_enabled": false
+                          }
+                      },
+                      "outside_ip_address": "34.225.101.10",
+                      "pre_shared_key": "8n7hnjNE8zhIt4VpMOIfcrw6XnUTHLW9",
+                      "tunnel_inside_cidr": "169.254.31.8/30"
+                  },
+              ]
+            contains:
+              log_options:
+                description: Options for logging VPN tunnel activity.
+                type: dict
+                contains:
+                  cloud_watch_log_options:
+                    description: Options for sending VPN tunnel logs to CloudWatch.
+                    type: dict
+              outside_ip_address:
+                description: The external IP address of the VPN tunnel.
+                type: str
+              pre_shared_key:
+                description:
+                  - The pre-shared key (PSK) to establish initial authentication between the
+                    virtual private gateway and the customer gateway.
+                type: str
+              tunnel_inside_cidr:
+                description: The range of inside IPv4 addresses for the tunnel.
+                type: str
       routes:
         description: List of static routes associated with the VPN connection.
         returned: always
-        type: complex
+        type: list
+        elements: dict
         contains:
           destination_cidr_block:
-            description: The CIDR block associated with the local subnet of the customer data center.
-            returned: always
+            description:
+              - The CIDR block associated with the local subnet of the customer data center.
             type: str
-            sample: 10.0.0.0/16
+          source:
+            description: Indicates how the routes were provided.
+            type: str
           state:
             description: The current state of the static route.
-            returned: always
             type: str
-            sample: available
       state:
         description: The current state of the VPN connection.
         returned: always
         type: str
-        sample: available
+        sample: "available"
       tags:
         description: Any tags assigned to the VPN connection.
         returned: always
         type: dict
         sample: {
-                    "Name": "test-conn"
+                  "Name": "test-conn"
                 }
       type:
         description: The type of VPN connection.
         returned: always
         type: str
-        sample: ipsec.1
+        sample: "ipsec.1"
       vgw_telemetry:
          description: Information about the VPN tunnel.
          returned: always
-         type: complex
+         type: dict
          contains:
            accepted_route_count:
              description: The number of accepted routes.
@@ -130,17 +193,17 @@ vpn_connections:
                description: The Internet-routable IP address of the virtual private gateway's outside interface.
                returned: always
                type: str
-               sample: 13.127.79.191
+               sample: "13.127.79.191"
            status:
                description: The status of the VPN tunnel.
                returned: always
                type: str
-               sample: DOWN
+               sample: "DOWN"
            status_message:
                description: If an error occurs, a description of the error.
                returned: always
                type: str
-               sample: IPSEC IS DOWN
+               sample: "IPSEC IS DOWN"
            certificate_arn:
                description: The Amazon Resource Name of the virtual private gateway tunnel endpoint certificate.
                returned: when a private certificate is used for authentication
@@ -150,50 +213,51 @@ vpn_connections:
         description: The ID of the VPN connection.
         returned: always
         type: str
-        sample: vpn-f700d5c0
+        sample: "vpn-f700d5c0"
       vpn_gateway_id:
         description: The ID of the virtual private gateway at the AWS side of the VPN connection.
         returned: always
         type: str
-        sample: vgw-cbe56bfb
+        sample: "vgw-cbe56bfb"
 """
 
 import json
-
-try:
-    from botocore.exceptions import BotoCoreError
-    from botocore.exceptions import ClientError
-except ImportError:
-    pass  # caught by AnsibleAWSModule
+from typing import Any
+from typing import Dict
+from typing import NoReturn
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import describe_vpn_connections
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 
 from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 
 
-def date_handler(obj):
+def date_handler(obj: Dict[str, Any]) -> Dict[str, Any]:
     return obj.isoformat() if hasattr(obj, "isoformat") else obj
 
 
-def list_vpn_connections(connection, module):
-    params = dict()
+def list_vpn_connections(client, module: AnsibleAWSModule) -> NoReturn:
+    params: Dict[str, Any] = {}
 
     params["Filters"] = ansible_dict_to_boto3_filter_list(module.params.get("filters"))
     params["VpnConnectionIds"] = module.params.get("vpn_connection_ids")
 
     try:
-        result = json.loads(json.dumps(connection.describe_vpn_connections(**params), default=date_handler))
+        result = json.loads(json.dumps(describe_vpn_connections(client, **params), default=date_handler))
     except ValueError as e:
-        module.fail_json_aws(e, msg="Cannot validate JSON data")
-    except (ClientError, BotoCoreError) as e:
+        module.fail_json(e, msg="Cannot validate JSON data")
+    except AnsibleEC2Error as e:
         module.fail_json_aws(e, msg="Could not describe customer gateways")
-    snaked_vpn_connections = [camel_dict_to_snake_dict(vpn_connection) for vpn_connection in result["VpnConnections"]]
+
+    snaked_vpn_connections = [camel_dict_to_snake_dict(vpn_connection) for vpn_connection in result]
     if snaked_vpn_connections:
         for vpn_connection in snaked_vpn_connections:
             vpn_connection["tags"] = boto3_tag_list_to_ansible_dict(vpn_connection.get("tags", []))
+
     module.exit_json(changed=False, vpn_connections=snaked_vpn_connections)
 
 
