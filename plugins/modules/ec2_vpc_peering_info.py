1bfe86b68dc0d0d1732156f81210cd080aa000ec
Alina Buzachis <abuzachis@redhat.com>
2024-10-16 14:15:49 +0200

    Apply isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/ec2_vpc_peering_info.py
+++ plugins/modules/ec2_vpc_peering_info.py
@@ -343,10 +343,9 @@ from ansible.module_utils.common.dict_transformations import camel_dict_to_snake
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import normalize_boto3_result
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import describe_vpc_peering_connections
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
-
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def get_vpc_peers(client, module: AnsibleAWSModule) -> List[Dict[str, Any]]:
