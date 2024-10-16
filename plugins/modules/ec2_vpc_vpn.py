86e8eff4eadaad166825232e388bbac5b7670fea
Alina Buzachis <abuzachis@redhat.com>
2024-10-16 13:31:30 +0200

    Update AnsibleAWSModule import path

--- plugins/modules/ec2_vpc_vpn.py
+++ plugins/modules/ec2_vpc_vpn.py
@@ -450,7 +450,7 @@ from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ensure_ec2_t
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import compare_aws_tags
 
-from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def find_vpn_connection(
