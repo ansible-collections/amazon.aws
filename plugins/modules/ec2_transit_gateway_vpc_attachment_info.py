9f253b662f4deea02515813c9a712208e7fc59bf
Alina Buzachis <abuzachis@redhat.com>
2024-10-18 14:13:00 +0200

    Apply isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/ec2_transit_gateway_vpc_attachment_info.py
+++ plugins/modules/ec2_transit_gateway_vpc_attachment_info.py
@@ -8,6 +8,7 @@ DOCUMENTATION = r"""
 module: ec2_transit_gateway_vpc_attachment_info
 short_description: describes AWS Transit Gateway VPC attachments
 version_added: 4.0.0
+version_added_collection: community.aws
 description:
   - Describes AWS Transit Gateway VPC Attachments.
 options:
@@ -153,10 +154,10 @@ from typing import List
 
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import describe_transit_gateway_vpc_attachments
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import boto3_resource_to_ansible_dict
 
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.community.aws.plugins.module_utils.transitgateway import get_states
 
 
