ee22aa688a042e22794c16a91e78eca7f12e0021
Mandar Kulkarni <mandar242@gmail.com>
2024-10-24 11:27:43 -0700

    linter fix

--- plugins/modules/ec2_transit_gateway_info.py
+++ plugins/modules/ec2_transit_gateway_info.py
@@ -168,10 +168,9 @@ from ansible.module_utils.common.dict_transformations import camel_dict_to_snake
 
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import describe_ec2_transit_gateways
+from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
-
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
 
 
 def get_transit_gateway_response(module: AnsibleAWSModule, connection) -> Dict[str, Any]:
