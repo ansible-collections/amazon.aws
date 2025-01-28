d08f835f1c76d6e8deb1611065c22be909718a5e
Mark Chappell <mchappel@redhat.com>
2025-01-28 14:20:56 +0100

    Synchronise tox configuration between amazon.aws and community.aws (#2486)
    SUMMARY
    tox configs needed a little cleanup to ensure that things were consistently in the import path when running pylint.
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    pyproject.toml
    tests/unit/conftest.py
    tox.ini
    ADDITIONAL INFORMATION
    See also: ansible-collections/community.aws#2219
    
    Reviewed-by: Alina Buzachis
    (cherry picked from commit 62ea880c6c5bb78dfbe2134b03fa7138acb516ec)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
+
 # This file is part of Ansible
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# pylint: disable=unused-import
-
-import pytest
-
-from .utils.amazon_placebo_fixtures import fixture_maybe_sleep
-from .utils.amazon_placebo_fixtures import fixture_placeboify
+# While it may seem appropriate to import our custom fixtures here, the pytest_ansible pytest plugin
+# isn't as agressive as the ansible_test._util.target.pytest.plugins.ansible_pytest_collections plugin
+# when it comes to rewriting the import paths and as such we can't import fixtures via their
+# absolute import path or across collections.
---
+++ tests/unit/plugins/conftest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# pylint: disable=unused-import
+
+import pytest
+
+from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import fixture_maybe_sleep
+from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import fixture_placeboify
