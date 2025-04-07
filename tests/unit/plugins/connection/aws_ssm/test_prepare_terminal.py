446b5f3a7127575128ede34749b9094f3d6a11a0
Mandar Kulkarni <mandar242@gmail.com>
2025-04-07 13:56:08 -0700

    connection/aws_ssm - create TerminalManager class and move related methods (#2270)
    SUMMARY
    
    create TerminalManager class and move related methods
    ACA-2102
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    connection/aws_ssm
    ADDITIONAL INFORMATION
    
    Reviewed-by: Bikouo Aubin
    Reviewed-by: Mandar Kulkarni <mandar242@gmail.com>
    Reviewed-by: Mike Graves <mgraves@redhat.com>
    Reviewed-by: GomathiselviS <gomathiselvi@gmail.com>
    
    This commit was initially merged in https://github.com/ansible-collections/community.aws
    See: https://github.com/ansible-collections/community.aws/commit/2c1e8c492b73ad34afa206f2faefff76f54190be

--- tests/unit/plugins/connection/aws_ssm/test_prepare_terminal.py
+++ tests/unit/plugins/connection/aws_ssm/test_prepare_terminal.py
@@ -16,6 +16,8 @@ import pytest
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import HAS_BOTO3
 
+from ansible_collections.community.aws.plugins.connection.aws_ssm import TerminalManager
+
 if not HAS_BOTO3:
     pytestmark = pytest.mark.skip("test_poll.py requires the python modules 'boto3' and 'botocore'")
 
@@ -47,15 +49,18 @@ def test_ensure_ssm_session_has_started(m_to_text, m_to_bytes, connection_aws_ss
 
     connection_aws_ssm._stdout.read.side_effect = stdout_lines
 
+    if not hasattr(connection_aws_ssm, "terminal_manager"):
+        connection_aws_ssm.terminal_manager = TerminalManager(connection_aws_ssm)
+
     poll_mock.results = [True for i in range(len(stdout_lines))]
     connection_aws_ssm.poll = MagicMock()
     connection_aws_ssm.poll.side_effect = poll_mock
 
     if timeout_failure:
         with pytest.raises(TimeoutError):
-            connection_aws_ssm._ensure_ssm_session_has_started()
+            connection_aws_ssm.terminal_manager.ensure_ssm_session_has_started()
     else:
-        connection_aws_ssm._ensure_ssm_session_has_started()
+        connection_aws_ssm.terminal_manager.ensure_ssm_session_has_started()
 
 
 @pytest.mark.parametrize(
@@ -67,8 +72,8 @@ def test_ensure_ssm_session_has_started(m_to_text, m_to_bytes, connection_aws_ss
         (["stty ", "-ech"], True),
     ],
 )
-@patch("ansible_collections.community.aws.plugins.connection.aws_ssm.to_bytes")
-@patch("ansible_collections.community.aws.plugins.connection.aws_ssm.to_text")
+@patch("ansible_collections.community.aws.plugins.plugin_utils.terminalmanager.to_bytes")
+@patch("ansible_collections.community.aws.plugins.plugin_utils.terminalmanager.to_text")
 def test_disable_echo_command(m_to_text, m_to_bytes, connection_aws_ssm, stdout_lines, timeout_failure):
     m_to_text.side_effect = str
     m_to_bytes.side_effect = lambda x, **kw: str(x)
@@ -80,19 +85,22 @@ def test_disable_echo_command(m_to_text, m_to_bytes, connection_aws_ssm, stdout_
     connection_aws_ssm.poll = MagicMock()
     connection_aws_ssm.poll.side_effect = poll_mock
 
+    if not hasattr(connection_aws_ssm, "terminal_manager"):
+        connection_aws_ssm.terminal_manager = TerminalManager(connection_aws_ssm)
+
     if timeout_failure:
         with pytest.raises(TimeoutError):
-            connection_aws_ssm._disable_echo_command()
+            connection_aws_ssm.terminal_manager.disable_echo_command()
     else:
-        connection_aws_ssm._disable_echo_command()
+        connection_aws_ssm.terminal_manager.disable_echo_command()
 
     connection_aws_ssm._session.stdin.write.assert_called_once_with("stty -echo\n")
 
 
 @pytest.mark.parametrize("timeout_failure", [True, False])
-@patch("ansible_collections.community.aws.plugins.connection.aws_ssm.random")
-@patch("ansible_collections.community.aws.plugins.connection.aws_ssm.to_bytes")
-@patch("ansible_collections.community.aws.plugins.connection.aws_ssm.to_text")
+@patch("ansible_collections.community.aws.plugins.plugin_utils.terminalmanager.random")
+@patch("ansible_collections.community.aws.plugins.plugin_utils.terminalmanager.to_bytes")
+@patch("ansible_collections.community.aws.plugins.plugin_utils.terminalmanager.to_text")
 def test_disable_prompt_command(m_to_text, m_to_bytes, m_random, connection_aws_ssm, timeout_failure):
     m_to_text.side_effect = str
     m_to_bytes.side_effect = lambda x, **kw: str(x)
@@ -100,6 +108,9 @@ def test_disable_prompt_command(m_to_text, m_to_bytes, m_random, connection_aws_
 
     connection_aws_ssm.poll = MagicMock()
     connection_aws_ssm.poll.side_effect = poll_mock
+
+    if not hasattr(connection_aws_ssm, "terminal_manager"):
+        connection_aws_ssm.terminal_manager = TerminalManager(connection_aws_ssm)
 
     m_random.choice = MagicMock()
     m_random.choice.side_effect = lambda x: "a"
@@ -115,8 +126,8 @@ def test_disable_prompt_command(m_to_text, m_to_bytes, m_random, connection_aws_
 
     if timeout_failure:
         with pytest.raises(TimeoutError):
-            connection_aws_ssm._disable_prompt_command()
+            connection_aws_ssm.terminal_manager.disable_prompt_command()
     else:
-        connection_aws_ssm._disable_prompt_command()
+        connection_aws_ssm.terminal_manager.disable_prompt_command()
 
     connection_aws_ssm._session.stdin.write.assert_called_once_with(prompt_cmd)
