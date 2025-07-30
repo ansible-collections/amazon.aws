c5378a66515694efabe8fbc800ff9f85f953adde
GomathiselviS <gomathiselvi@gmail.com>
2025-07-30 07:40:28 -0700

    Fix unit tests

--- tests/unit/plugins/module_utils/modules/ansible_aws_module/test_minimal_versions.py
+++ tests/unit/plugins/module_utils/modules/ansible_aws_module/test_minimal_versions.py
@@ -102,6 +102,7 @@ class TestMinimalVersionTestSuite:
         assert return_val.get("invocation") is not None
         assert return_val.get("failed") is None
         assert return_val.get("error") is None
+        assert str(return_val.get("warnings")) is not None
         warnings = return_val.get("warnings")
         assert len(warnings) == 1
         # Assert that we have a warning about the version but be
@@ -135,6 +136,7 @@ class TestMinimalVersionTestSuite:
         assert return_val.get("invocation") is not None
         assert return_val.get("failed") is None
         assert return_val.get("error") is None
+        assert str(return_val.get("warnings")) is not None
         warnings = return_val.get("warnings")
         assert len(warnings) == 1
         # Assert that we have a warning about the version but be
@@ -168,7 +170,7 @@ class TestMinimalVersionTestSuite:
         assert return_val.get("invocation") is not None
         assert return_val.get("failed") is None
         assert return_val.get("error") is None
-
+        assert str(return_val.get("warnings")) is not None
         warnings = return_val.get("warnings")
         assert len(warnings) == 2
 
