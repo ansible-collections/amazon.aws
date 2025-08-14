50d8bc2eebf5c947701f75c3a07d1562e41a5117
sean cavanaugh <sean@cavanaugh.pro>
2025-08-14 14:02:20 -0400

    isort: reorder ansible.module_utils imports in module_utils/modules.py

--- plugins/module_utils/modules.py
+++ plugins/module_utils/modules.py
@@ -47,11 +47,11 @@ from typing import NoReturn
 from typing import Optional
 
 from ansible.module_utils._text import to_native
+from ansible.module_utils.ansible_release import __version__ as _ANSIBLE_CORE_VERSION
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.basic import env_fallback
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
-from ansible.module_utils.ansible_release import __version__ as _ANSIBLE_CORE_VERSION
 
 from .botocore import boto3_at_least
 from .botocore import boto3_conn
