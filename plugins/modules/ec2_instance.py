fd165c1ea8b9bcecf213dbf84f29b6c5c8bc11fd
iamdevdhanush <dhanushdprabhu18@gmail.com>
2026-01-16 00:29:21 +0530

    Fix mutable default for security_groups in ec2_instance

--- plugins/modules/ec2_instance.py
+++ plugins/modules/ec2_instance.py
@@ -2706,7 +2706,7 @@ def main():
         ebs_optimized=dict(type="bool"),
         vpc_subnet_id=dict(type="str", aliases=["subnet_id"]),
         availability_zone=dict(type="str"),
-        security_groups=dict(default=[], type="list", elements="str"),
+        security_groups=dict(default=None, type="list", elements="str"),
         security_group=dict(type="str"),
         iam_instance_profile=dict(type="str", aliases=["instance_role"]),
         name=dict(type="str"),
