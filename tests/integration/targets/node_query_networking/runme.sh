008a8dfe558901e135df5ef107c42234b2f59761
aubin bikouo <abikouo@redhat.com>
2026-01-26 15:41:45 +0100

    update integration test

--- tests/integration/targets/node_query_networking/runme.sh
+++ tests/integration/targets/node_query_networking/runme.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+set -eux
+
+function cleanup() {
+    ansible-playbook teardown.yml "$@"
+    rm -f aws_vars.yml
+    exit 1
+}
+
+rm -rf aws_vars.yml
+
+trap 'cleanup "${@}"'  ERR
+
 export ANSIBLE_ROLES_PATH=../
 
-ansible-playbook play.yaml "$@"
+# Setup
+ansible-playbook setup.yml "$@"
+
+# Validate
+ansible-playbook validate.yml -i inventory.ini "$@"
+
+# clean resources
+ansible-playbook teardown.yml "$@"
+rm -f aws_vars.yml
