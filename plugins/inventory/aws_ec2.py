96486eb61878e2daaccc17521784d6a7d74a91ca
Hannah DeFazio <h2defazio@gmail.com>
2026-02-17 15:51:41 -0500

    Deprecate tags, add ec2_tags and rds_tags host variables (#2847)
    SUMMARY
    ACA-4986
    The aws_ec2 and aws_rds inventory plugins automatically expose a host variable named tags (amazon.aws/issues#2763). However, "tags" is a reserved name in Ansible (source). This collision triggers a persistent [WARNING]: Found variable using reserved name 'tags'. To resolve this, this pr  introduce a new, non-reserved keys (ec2_tags and rds_tags) that contain the same dictionary data as the current tags key and deprecates the tags key.
    Fixes #2763
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    aws_ec2 and aws_rds
    
    Reviewed-by: Mark Chappell
    Reviewed-by: Alina Buzachis
    Reviewed-by: Hannah DeFazio <h2defazio@gmail.com>
    Reviewed-by: Mike Graves <mgraves@redhat.com>
    Reviewed-by: Mandar
    (cherry picked from commit ae27cb775bcec4620df0e7ec999c686025526c7b)

--- plugins/inventory/aws_ec2.py
+++ plugins/inventory/aws_ec2.py
@@ -19,6 +19,9 @@ description:
 notes:
   - If no credentials are provided and the control node has an associated IAM instance profile then the
     role will be used for authentication.
+  - The C(tags) host variable is deprecated and will be removed in a release after 2026-12-01.
+    Use C(ec2_tags) instead to avoid conflicts with Ansible reserved variable names.
+  - The C(ec2_tags) host variable was added in version 11.2.0.
 author:
   - Sloane Hertel (@s-hertel)
 options:
@@ -116,7 +119,7 @@ options:
   use_contrib_script_compatible_ec2_tag_keys:
     description:
       - Expose the host tags with ec2_tag_TAGNAME keys like the old ec2.py inventory script.
-      - The use of this feature is discouraged and we advise to migrate to the new ``tags`` structure.
+      - The use of this feature is discouraged and we advise to migrate to the new ``ec2_tags`` structure.
     type: bool
     default: false
     version_added: 1.5.0
@@ -225,7 +228,7 @@ keyed_groups:
     key: 'architecture'
   # Add hosts to tag_Name_Value groups for each Name/Value tag pair
   - prefix: tag
-    key: tags
+    key: ec2_tags
   # Add hosts to e.g. instance_type_z3_tiny
   - prefix: instance_type
     key: instance_type
@@ -233,13 +236,13 @@ keyed_groups:
   - key: 'security_groups|json_query("[].group_id")'
     prefix: 'security_groups'
   # Create a group for each value of the Application tag
-  - key: tags.Application
+  - key: ec2_tags.Application
     separator: ''
   # Create a group per region e.g. aws_region_us_east_2
   - key: placement.region
     prefix: aws_region
   # Create a group (or groups) based on the value of a custom tag "Role" and add them to a metagroup called "project"
-  - key: tags['Role']
+  - key: ec2_tags['Role']
     prefix: foo
     parent_group: "project"
 # Set individual variables with compose
@@ -277,7 +280,7 @@ filters:
   instance-state-name: running
 keyed_groups:
   - prefix: tag
-    key: tags
+    key: ec2_tags
 compose:
   ansible_host: public_dns_name
 groups:
@@ -486,13 +489,15 @@ def _prepare_host_vars(
     use_contrib_script_compatible_ec2_tag_keys=False,
 ):
     host_vars = camel_dict_to_snake_dict(original_host_vars, ignore_list=["Tags"])
-    host_vars["tags"] = boto3_tag_list_to_ansible_dict(original_host_vars.get("Tags", []))
+    host_vars["ec2_tags"] = boto3_tag_list_to_ansible_dict(original_host_vars.get("Tags", []))
+    # ec2_tags is the new key, tags is deprecated but kept for backward compatibility
+    host_vars["tags"] = host_vars["ec2_tags"]
 
     # Allow easier grouping by region
     host_vars["placement"]["region"] = host_vars["placement"]["availability_zone"][:-1]
 
     if use_contrib_script_compatible_ec2_tag_keys:
-        for k, v in host_vars["tags"].items():
+        for k, v in host_vars["ec2_tags"].items():
             host_vars[f"ec2_tag_{k}"] = v
 
     if hostvars_prefix or hostvars_suffix:
@@ -941,6 +946,12 @@ class InventoryModule(AWSInventoryBase):
 
     def parse(self, inventory, loader, path, cache=True):
         super().parse(inventory, loader, path, cache=cache)
+
+        self.display.deprecated(
+            "The 'tags' host variable is deprecated. Use 'ec2_tags' instead.",
+            date="2026-12-01",
+            collection_name="amazon.aws",
+        )
 
         if self.get_option("use_contrib_script_compatible_sanitization"):
             self._sanitize_group_name = self._legacy_script_compatible_group_sanitization
