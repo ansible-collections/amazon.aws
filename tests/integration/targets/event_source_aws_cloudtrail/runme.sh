786d7886139c8db3fc313e5a915f0c7af60555b3
Benny Rahmanim <brahmani@redhat.com>
2026-03-05 14:17:56 +0200

    some updates to use merged vars file

--- tests/integration/targets/event_source_aws_cloudtrail/runme.sh
+++ tests/integration/targets/event_source_aws_cloudtrail/runme.sh
@@ -9,6 +9,19 @@ set -eux
 
 export ANSIBLE_ROLES_PATH=../
 
-ansible-rulebook --inventory inventory-cloudtaril.yml --rulebook rulebooks/aws_manage_cloudtrail_ec2_keypair.yml --vars vars.yml &
+CMD_ARGS=("$@")
+# For ansible-rulebook --vars: pass only the var file path (e.g. after -e), not "-e $'@...'"
+if [[ ${#CMD_ARGS[@]} -ge 2 && "${CMD_ARGS[0]}" == "-e" ]]; then
+    VAR_FILE="${CMD_ARGS[1]}"
+    # Strip $'@ prefix and trailing ' (e.g. $'@/path/to/file.json' -> /path/to/file.json)
+    VAR_FILE="${VAR_FILE#@}"
+fi
 
-ansible-playbook ec2_keypair.yml -i inventory-cloudtrail.yml "$@" -e @vars.yml
+jq -s 'add' "${VAR_FILE}" vars.json > /tmp/vars.json
+
+ansible-rulebook --inventory inventory --rulebook rulebooks/aws_manage_cloudtrail_ec2_keypair.yml --vars /tmp/vars.json &
+
+# sleep 10 seconds for make sure ansible-rulebook had no errors
+sleep 10
+
+ansible-playbook ec2_keypair.yml -i inventory -e @/tmp/vars.json
