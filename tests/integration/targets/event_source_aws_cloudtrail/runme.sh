28ff52b2eea6c0cb91479234db3ef8bec00f5fcd
Rahmanim Benny <brahmani@redhat.com>
2026-03-18 16:33:44 +0200

    ACA-4570 add event source AWS CloudTrail integration tests (#2868)
    SUMMARY
    
    Add event source AWS CloudTrail integration tests
    
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    
    ADDITIONAL INFORMATION
    
    The integration test will remain disabled until we have GHA that will trigger the event source aws tests.
    
    Reviewed-by: Matthew Johnson
    Reviewed-by: GomathiselviS <gomathiselvi@gmail.com>
    Reviewed-by: Alina Buzachis
    (cherry picked from commit f7d02555e806bc3f53920b702a73e655182816ec)

---
+++ tests/integration/targets/event_source_aws_cloudtrail/runme.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Beware: most of our tests here are run in parallel.
+# To add new tests you'll need to add a new host to the inventory and a matching
+# '{{ inventory_hostname }}'.yml file in roles/ec2_instance/tasks/
+
+
+set -eux
+
+export ANSIBLE_ROLES_PATH=../
+
+CMD_ARGS=("$@")
+# For ansible-rulebook --vars: pass only the var file path (e.g. after -e), not "-e $'@...'"
+if [[ ${#CMD_ARGS[@]} -ge 2 && "${CMD_ARGS[0]}" == "-e" ]]; then
+    VAR_FILE="${CMD_ARGS[1]}"
+    # Strip $'@ prefix and trailing ' (e.g. $'@/path/to/file.json' -> /path/to/file.json)
+    VAR_FILE="${VAR_FILE#@}"
+fi
+
+# Create the temp directory
+TEMP_DIR=$(mktemp -d -t event_source_aws_cloudtrail_vars)
+
+# Set a 'trap' to remove the directory when the script exits
+trap 'rm -rf "$TEMP_DIR"' EXIT
+
+# join the var file and the vars.json file
+jq -s 'add' "${VAR_FILE}" vars.json > "$TEMP_DIR/vars.json"
+
+ansible-rulebook --inventory inventory --rulebook rulebooks/aws_manage_cloudtrail_ec2_keypair.yml --vars "$TEMP_DIR/vars.json" &
+
+# sleep 10 seconds for make sure ansible-rulebook had no errors
+sleep 10
+
+ansible-playbook ec2_keypair.yml -i inventory -e "@$TEMP_DIR/vars.json"
